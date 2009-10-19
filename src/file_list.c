4a75ec8effe3e4878e7710a1322d8ed86f059c08
Christophe Grenier <grenier@cgsecurity.org>
2009-10-19 22:28:30 +0200

    PhotoRec: recover Nintendo DS Game ROM Image .nds

--- src/file_list.c
+++ src/file_list.c
@@ -143,6 +143,7 @@ extern const file_hint_t file_hint_mrw;
 extern const file_hint_t file_hint_mus;
 extern const file_hint_t file_hint_mxf;
 extern const file_hint_t file_hint_mysql;
+extern const file_hint_t file_hint_nds;
 extern const file_hint_t file_hint_njx;
 extern const file_hint_t file_hint_nk2;
 extern const file_hint_t file_hint_ogg;
@@ -336,6 +337,7 @@ file_enable_t list_file_enable[]=
   { .enable=0, .file_hint=&file_hint_mus  },
   { .enable=0, .file_hint=&file_hint_mysql },
   { .enable=0, .file_hint=&file_hint_mxf  },
+  { .enable=0, .file_hint=&file_hint_nds  },
   { .enable=0, .file_hint=&file_hint_njx  },
   { .enable=0, .file_hint=&file_hint_nk2  },
   { .enable=0, .file_hint=&file_hint_ogg  },
