eab88caf92265cea557aaae24adb34dcd5f62ac8
Christophe Grenier <grenier@cgsecurity.org>
2010-01-24 13:21:03 +0100

    PhotoRec: Improve .dv Digital Video file recovery to get the filesize

--- src/file_dv.c
+++ src/file_dv.c
@@ -32,6 +32,7 @@
 
 static void register_header_check_dv(file_stat_t *file_stat);
 static int header_check_dv(const unsigned char *buffer, const unsigned int buffer_size, const unsigned int safe_header_only, const file_recovery_t *file_recovery, file_recovery_t *file_recovery_new);
+static int data_check_dv(const unsigned char *buffer, const unsigned int buffer_size, file_recovery_t *file_recovery);
 
 const file_hint_t file_hint_dv= {
   .extension="dv",
@@ -56,11 +57,28 @@ static int header_check_dv(const unsigned char *buffer, const unsigned int buffe
   /* The header may be only 3 bytes */
   if(buffer[0]==0x1f && buffer[1]==0x07 && buffer[2]==0x00 && buffer[5]==0x78 && buffer[6]==0x78 && buffer[7]==0x78)
   {
+    /* NTSC */
     reset_file_recovery(file_recovery_new);
     file_recovery_new->extension=file_hint_dv.extension;
+    file_recovery_new->data_check=data_check_dv;
+    file_recovery_new->file_check=&file_check_size;
     return 1;
   }
   return 0;
 }
 
+static int data_check_dv(const unsigned char *buffer, const unsigned int buffer_size, file_recovery_t *file_recovery)
+{
+  while(file_recovery->calculated_file_size + buffer_size/2  >= file_recovery->file_size &&
+      file_recovery->calculated_file_size + 8 < file_recovery->file_size + buffer_size/2)
+  {
+    const unsigned int i=file_recovery->calculated_file_size - file_recovery->file_size + buffer_size/2;
+    if(buffer[i]==0x1f && buffer[i+1]==0x07 && buffer[i+2]==0x00 &&
+	buffer[i+5]==0x78 && buffer[i+6]==0x78 && buffer[i+7]==0x78)
+      file_recovery->calculated_file_size+=120000;
+    else
+      return 2;
+  }
+  return 1;
+}
 
