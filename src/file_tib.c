3239824a326e9f8adda7370a161761c21ead1f4f
Christophe Grenier <grenier@cgsecurity.org>
2010-09-24 08:14:50 +0200

    PhotoRec: handle another version of Acronis True Image

--- src/file_tib.c
+++ src/file_tib.c
@@ -44,9 +44,17 @@ const file_hint_t file_hint_tib= {
 };
 
 static const unsigned char tib_header[4]= { 0xb4, 0x6e, 0x68, 0x44};
+static const unsigned char tib2_header[7]= { 0xce, 0x24, 0xb9, 0xa2, 0x20, 0x00, 0x00};
 static void register_header_check_tib(file_stat_t *file_stat)
 {
   register_header_check(0, tib_header,sizeof(tib_header), &header_check_tib, file_stat);
+  register_header_check(0, tib2_header,sizeof(tib2_header), &header_check_tib, file_stat);
+}
+
+static void file_check_tib2(file_recovery_t *file_recovery)
+{
+  const unsigned char tib2_footer[7]= {0x00, 0x00, 0x20, 0xa2, 0xb9, 0x24, 0xce};
+  file_search_footer(file_recovery, tib2_footer, sizeof(tib2_footer), 1);
 }
 
 static int header_check_tib(const unsigned char *buffer, const unsigned int buffer_size, const unsigned int safe_header_only, const file_recovery_t *file_recovery, file_recovery_t *file_recovery_new)
@@ -54,6 +62,13 @@ static int header_check_tib(const unsigned char *buffer, const unsigned int buff
   if(memcmp(buffer,tib_header, sizeof(tib_header))==0)
   {
     reset_file_recovery(file_recovery_new);
+    file_recovery_new->extension=file_hint_tib.extension;
+    return 1;
+  }
+  if(memcmp(buffer,tib2_header, sizeof(tib2_header))==0)
+  {
+    reset_file_recovery(file_recovery_new);
+    file_recovery_new->file_check=file_check_tib2;
     file_recovery_new->extension=file_hint_tib.extension;
     return 1;
   }
