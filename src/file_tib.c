2bca2d6e5370c5a402322049f90e9eb9152d81a2
Christophe Grenier <grenier@cgsecurity.org>
2011-01-05 07:57:16 +0100

    PhotoRec: recover recents version of Acronis .tib files

--- src/file_tib.c
+++ src/file_tib.c
@@ -2,7 +2,7 @@
 
     File: file_tib.c
 
-    Copyright (C) 2007 Christophe GRENIER <grenier@cgsecurity.org>
+    Copyright (C) 2007-2010 Christophe GRENIER <grenier@cgsecurity.org>
   
     This software is free software; you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
@@ -23,12 +23,16 @@
 #ifdef HAVE_CONFIG_H
 #include <config.h>
 #endif
+#ifdef HAVE_STDLIB_H
+#include <stdlib.h>
+#endif
 #ifdef HAVE_STRING_H
 #include <string.h>
 #endif
 #include <stdio.h>
 #include "types.h"
 #include "filegen.h"
+#include "common.h"
 
 static void register_header_check_tib(file_stat_t *file_stat);
 static int header_check_tib(const unsigned char *buffer, const unsigned int buffer_size, const unsigned int safe_header_only, const file_recovery_t *file_recovery, file_recovery_t *file_recovery_new);
@@ -45,16 +49,70 @@ const file_hint_t file_hint_tib= {
 
 static const unsigned char tib_header[4]= { 0xb4, 0x6e, 0x68, 0x44};
 static const unsigned char tib2_header[7]= { 0xce, 0x24, 0xb9, 0xa2, 0x20, 0x00, 0x00};
+static const unsigned char tib2_footer[7]= {0x00, 0x00, 0x20, 0xa2, 0xb9, 0x24, 0xce};
+
 static void register_header_check_tib(file_stat_t *file_stat)
 {
   register_header_check(0, tib_header,sizeof(tib_header), &header_check_tib, file_stat);
   register_header_check(0, tib2_header,sizeof(tib2_header), &header_check_tib, file_stat);
 }
 
-static void file_check_tib2(file_recovery_t *file_recovery)
+static int data_check_tib2(const unsigned char *buffer, const unsigned int buffer_size, file_recovery_t *file_recovery)
 {
-  const unsigned char tib2_footer[7]= {0x00, 0x00, 0x20, 0xa2, 0xb9, 0x24, 0xce};
-  file_search_footer(file_recovery, tib2_footer, sizeof(tib2_footer), 1);
+  while(file_recovery->calculated_file_size + buffer_size/2  >= file_recovery->file_size &&
+      file_recovery->calculated_file_size + 512 <= file_recovery->file_size + buffer_size/2)
+  {
+    const unsigned int i=file_recovery->calculated_file_size - file_recovery->file_size + buffer_size/2;
+    file_recovery->calculated_file_size+=512;
+    if(memcmp(&buffer[i + 512 - sizeof(tib2_footer)], tib2_footer, sizeof(tib2_footer))==0)
+      return 2;
+  }
+  return 1;
+}
+
+static void file_check_tib(file_recovery_t *file_recovery)
+{
+  unsigned char*buffer=(unsigned char*)MALLOC(512);
+  int64_t file_size=file_recovery->calculated_file_size-512;
+  file_recovery->file_size = file_recovery->calculated_file_size;
+  if(fseek(file_recovery->handle, file_size, SEEK_SET)<0)
+  {
+    free(buffer);
+    return;
+  }
+  if(fread(buffer, 1, 512, file_recovery->handle)<=0)
+  {
+    free(buffer);
+    return;
+  }
+  if(memcmp(&buffer[512 - sizeof(tib2_footer)], tib2_footer, sizeof(tib2_footer))==0)
+  {
+    free(buffer);
+    return;
+  }
+
+  for(; file_size>0; file_size-=512)
+  {
+    unsigned int i;
+    if(fseek(file_recovery->handle, file_size, SEEK_SET)<0)
+    {
+      free(buffer);
+      return;
+    }
+    if(fread(buffer, 1, 512, file_recovery->handle)<=0)
+    {
+      free(buffer);
+      return;
+    }
+    for(i=0; i<512 && buffer[i]==0; i++);
+    if(i!=512)
+    {
+      file_recovery->file_size=file_size + 512;
+      free(buffer);
+      return ;
+    }
+  }
+  free(buffer);
 }
 
 static int header_check_tib(const unsigned char *buffer, const unsigned int buffer_size, const unsigned int safe_header_only, const file_recovery_t *file_recovery, file_recovery_t *file_recovery_new)
@@ -68,7 +126,8 @@ static int header_check_tib(const unsigned char *buffer, const unsigned int buff
   if(memcmp(buffer,tib2_header, sizeof(tib2_header))==0)
   {
     reset_file_recovery(file_recovery_new);
-    file_recovery_new->file_check=file_check_tib2;
+    file_recovery_new->file_check=&file_check_tib;
+    file_recovery_new->data_check=data_check_tib2;
     file_recovery_new->extension=file_hint_tib.extension;
     return 1;
   }
