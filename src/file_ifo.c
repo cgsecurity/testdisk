d275abb229920f26fcef7929af83bd04b949f8a4
Christophe Grenier <grenier@cgsecurity.org>
2011-08-28 10:02:04 +0200

    PhotoRec: detect IFO file size

--- src/file_ifo.c
+++ src/file_ifo.c
@@ -32,6 +32,7 @@
 #include <stdio.h>
 #include "types.h"
 #include "filegen.h"
+#include "common.h"
 
 static void register_header_check_ifo(file_stat_t *file_stat);
 static int header_check_ifo(const unsigned char *buffer, const unsigned int buffer_size, const unsigned int safe_header_only, const file_recovery_t *file_recovery, file_recovery_t *file_recovery_new);
@@ -55,14 +56,24 @@ static void register_header_check_ifo(file_stat_t *file_stat)
   register_header_check(0, ifo_header_vts, sizeof(ifo_header_vts), &header_check_ifo, file_stat);
 }
 
+struct ifo_hdr
+{
+  char 	   name[12];
+  uint32_t ls_BUP[4];
+  uint32_t ls_IFO;
+} __attribute__ ((__packed__));
 
 static int header_check_ifo(const unsigned char *buffer, const unsigned int buffer_size, const unsigned int safe_header_only, const file_recovery_t *file_recovery, file_recovery_t *file_recovery_new)
 {
   if(memcmp(buffer, ifo_header_vmg, sizeof(ifo_header_vmg))==0 ||
     memcmp(buffer, ifo_header_vts, sizeof(ifo_header_vts))==0)
   {
+    const struct ifo_hdr *hdr=(const struct ifo_hdr *)buffer;
     reset_file_recovery(file_recovery_new);
     file_recovery_new->extension=file_hint_ifo.extension;
+    file_recovery_new->calculated_file_size=((uint64_t)be32(hdr->ls_IFO)+1)*2048;
+    file_recovery_new->data_check=&data_check_size;
+    file_recovery_new->file_check=&file_check_size;
     return 1;
   }
   return 0;
