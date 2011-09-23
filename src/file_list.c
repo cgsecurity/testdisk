34d2b85749511199dd8fd34d530f6b8e5daa2e06
Christophe Grenier <grenier@cgsecurity.org>
2011-09-23 20:14:48 +0200

    PhotoRec: recover lxo/lwo 3d model files

--- src/file_list.c
+++ src/file_list.c
@@ -143,6 +143,7 @@ extern const file_hint_t file_hint_ldf;
 extern const file_hint_t file_hint_logic;
 extern const file_hint_t file_hint_lnk;
 extern const file_hint_t file_hint_lso;
+extern const file_hint_t file_hint_lxo;
 extern const file_hint_t file_hint_lzo;
 extern const file_hint_t file_hint_m2ts;
 extern const file_hint_t file_hint_mat;
@@ -381,6 +382,7 @@ file_enable_t list_file_enable[]=
   { .enable=0, .file_hint=&file_hint_logic},
   { .enable=0, .file_hint=&file_hint_lnk  },
   { .enable=0, .file_hint=&file_hint_lso  },
+  { .enable=0, .file_hint=&file_hint_lxo  },
   { .enable=0, .file_hint=&file_hint_lzo  },
   { .enable=0, .file_hint=&file_hint_m2ts },
   { .enable=0, .file_hint=&file_hint_mat  },
---
+++ src/file_lxo.c
@@ -0,0 +1,76 @@
+/*
+
+    File: file_lxo.c
+
+    Copyright (C) 2011 Christophe GRENIER <grenier@cgsecurity.org>
+  
+    This software is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+  
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+  
+    You should have received a copy of the GNU General Public License along
+    with this program; if not, write the Free Software Foundation, Inc., 51
+    Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+ */
+
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
+#ifdef HAVE_STRING_H
+#include <string.h>
+#endif
+#include <stdio.h>
+#include "types.h"
+#include "filegen.h"
+
+static void register_header_check_lxo(file_stat_t *file_stat);
+
+const file_hint_t file_hint_lxo= {
+  .extension="lxo",
+  .description="lxo/lwo 3d model",
+  .min_header_distance=0,
+  .max_filesize=PHOTOREC_MAX_FILE_SIZE,
+  .recover=1,
+  .enable_by_default=1,
+  .register_header_check=&register_header_check_lxo
+};
+
+static const unsigned char lxo_header[4]=  {
+  'F' , 'O' , 'R' , 'M' 
+};
+
+static int header_check_lxo(const unsigned char *buffer, const unsigned int buffer_size, const unsigned int safe_header_only, const file_recovery_t *file_recovery, file_recovery_t *file_recovery_new)
+{
+  if(memcmp(&buffer[0], lxo_header, sizeof(lxo_header))!=0)
+    return 0;
+  if(buffer[8]=='L' && buffer[9]=='X' && buffer[10]=='O')
+  {
+    reset_file_recovery(file_recovery_new);
+    file_recovery_new->extension=file_hint_lxo.extension;
+    file_recovery_new->calculated_file_size=(buffer[4]<<24)+(buffer[5]<<16)+(buffer[6]<<8)+buffer[7]+8;
+    file_recovery_new->file_check=&file_check_size;
+    file_recovery_new->data_check=&data_check_size;
+    return 1;
+  }
+  if(buffer[8]=='L' && buffer[9]=='W' && buffer[10]=='O')
+  {
+    reset_file_recovery(file_recovery_new);
+    file_recovery_new->extension="lwo";
+    file_recovery_new->calculated_file_size=(buffer[4]<<24)+(buffer[5]<<16)+(buffer[6]<<8)+buffer[7]+8;
+    file_recovery_new->file_check=&file_check_size;
+    file_recovery_new->data_check=&data_check_size;
+    return 1;
+  }
+}
+
+static void register_header_check_lxo(file_stat_t *file_stat)
+{
+  register_header_check(0, lxo_header, sizeof(lxo_header), &header_check_lxo, file_stat);
+}
