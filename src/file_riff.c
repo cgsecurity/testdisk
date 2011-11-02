8469f754c730ebe6e09cfe7ec9910d92e242a566
Christophe Grenier <grenier@cgsecurity.org>
2011-11-02 08:06:38 +0100

    file_riff.c: Fix regression introduced in #bd92f8847e15aadfe676e7e56e3f934547dd762b

--- src/file_riff.c
+++ src/file_riff.c
@@ -222,7 +222,9 @@ static int header_check_riff(const unsigned char *buffer, const unsigned int buf
     {
       /* Cubase Project File */
       file_recovery_new->extension="cpr";
-      file_recovery_new->calculated_file_size+=4;
+      file_recovery_new->calculated_file_size=(((uint64_t)buffer[4])<<24) +
+	(((uint64_t)buffer[5])<<16) + (((uint64_t)buffer[6])<<8) +
+	(uint64_t)buffer[7] + 12;
       return 1;
     }
     if(memcmp(&buffer[8],"AVI ",4)==0)
