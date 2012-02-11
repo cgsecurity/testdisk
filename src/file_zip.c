3b81092d6af28b8fcc8525b717210b7fd94a122a
Christophe Grenier <grenier@cgsecurity.org>
2012-02-11 19:31:15 +0100

    PhotoRec: identify MindManager as .mmap files instead of .zip

--- src/file_zip.c
+++ src/file_zip.c
@@ -230,6 +230,8 @@ static int zip_parse_file_entry(file_recovery_t *fr, const char **ext, const uns
 	  msoffice=1;
 	else if(len==4 && memcmp(filename, "Home", 4)==0)
 	  sh3d=1;
+	else if(len==30 && memcmp(filename, "xsd/MindManagerApplication.xsd", 30)==0)
+	  *ext="mmap";
       }
       else if(file_nbr==1 && sh3d==1)
       {
