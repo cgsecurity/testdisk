a91738704b52e595dc7f1b6c413717fd6c271022
Christophe Grenier <grenier@cgsecurity.org>
2012-05-01 19:12:42 +0200

    PhotoRec: recover Mobi e-book .mobi

--- src/file_list.c
+++ src/file_list.c
@@ -161,6 +161,7 @@ extern const file_hint_t file_hint_mfg;
 extern const file_hint_t file_hint_mid;
 extern const file_hint_t file_hint_mk5;
 extern const file_hint_t file_hint_mkv;
+extern const file_hint_t file_hint_mobi;
 extern const file_hint_t file_hint_mov;
 extern const file_hint_t file_hint_mp3;
 extern const file_hint_t file_hint_mpg;
@@ -407,6 +408,7 @@ file_enable_t list_file_enable[]=
   { .enable=0, .file_hint=&file_hint_mid  },
   { .enable=0, .file_hint=&file_hint_mk5  },
   { .enable=0, .file_hint=&file_hint_mkv  },
+  { .enable=0, .file_hint=&file_hint_mobi },
   { .enable=0, .file_hint=&file_hint_mov  },
   { .enable=0, .file_hint=&file_hint_mp3  },
   { .enable=0, .file_hint=&file_hint_mpg  },
