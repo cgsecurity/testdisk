24c08099101d600ffd06ddcad43db0285585d6e4
Christophe Grenier <grenier@cgsecurity.org>
2014-06-14 12:07:48 +0200

    PhotoRec: raise .flac filesize limit

--- src/file_flac.c
+++ src/file_flac.c
@@ -30,6 +30,7 @@
 #include "types.h"
 #include "filegen.h"
 #include "common.h"
+#include "log.h"
 
 static void register_header_check_flac(file_stat_t *file_stat);
 
@@ -37,11 +38,46 @@ const file_hint_t file_hint_flac= {
   .extension="flac",
   .description="FLAC audio",
   .min_header_distance=0,
-  .max_filesize=100*1024*1024,
+  .max_filesize=(uint64_t)1200*1024*1024,
   .recover=1,
   .enable_by_default=1,
-	.register_header_check=&register_header_check_flac
+  .register_header_check=&register_header_check_flac
 };
+
+/* https://xiph.org/flac/format.html */
+
+#if 0
+static data_check_t data_check_flac_frame(const unsigned char *buffer, const unsigned int buffer_size, file_recovery_t *file_recovery)
+{
+  return DC_CONTINUE;
+}
+
+static data_check_t data_check_flac_metadata(const unsigned char *buffer, const unsigned int buffer_size, file_recovery_t *file_recovery)
+{
+  while(file_recovery->calculated_file_size + buffer_size/2  >= file_recovery->file_size &&
+      file_recovery->calculated_file_size + 4 < file_recovery->file_size + buffer_size/2)
+  {
+    const unsigned int i=file_recovery->calculated_file_size - file_recovery->file_size + buffer_size/2;
+    const uint32_t *p32=(const uint32_t *)&buffer[i];
+    const uint32_t size=be32(*p32)&0x00ffffff;
+#ifdef DEBUG_FLAC
+    log_info("data_check_flac_metadata calculated_file_size=0x%llx: 0x%02x\n",
+	(long long unsigned)file_recovery->calculated_file_size, buffer[i]);
+#endif
+    if((buffer[i]&0x7f)==0x7f)
+	return DC_ERROR;
+    file_recovery->calculated_file_size+=4+size;
+    if((buffer[i]&0x80)==0x80)
+    {
+      file_recovery->data_check=&data_check_flac_frame;
+      log_info("data_check_flac_frame    calculated_file_size=0x%llx\n",
+	  (long long unsigned)file_recovery->calculated_file_size);
+      return data_check_flac_frame(buffer, buffer_size, file_recovery);
+    }
+  }
+  return DC_CONTINUE;
+}
+#endif
 
 static int header_check_flac(const unsigned char *buffer, const unsigned int buffer_size, const unsigned int safe_header_only, const file_recovery_t *file_recovery, file_recovery_t *file_recovery_new)
 {
@@ -50,6 +86,10 @@ static int header_check_flac(const unsigned char *buffer, const unsigned int buf
   file_recovery_new->extension="flc";
 #else
   file_recovery_new->extension=file_hint_flac.extension;
+#endif
+#if 0
+  file_recovery_new->calculated_file_size=4;
+  file_recovery_new->data_check=&data_check_flac_metadata;
 #endif
   return 1;
 }
