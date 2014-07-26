90dcbb01f2ea3ca8e65e81418a7648abb6cb189f
Christophe Grenier <grenier@cgsecurity.org>
2014-07-26 15:51:16 +0200

    PhotoRec: code cleanup for .fob recovery

--- src/file_fob.c
+++ src/file_fob.c
@@ -44,42 +44,26 @@ const file_hint_t file_hint_fob= {
   .register_header_check=&register_header_check_fob
 };
 
-static const unsigned char sign_navnl[5]	= {'N','A','V','N','L'};
-static const unsigned char sign_navw[4]		= {'N','A','V','W'};
-static const unsigned char magic_codeunit[9]	= {'C','o','d','e','u','n','i','t',' '};
-static const unsigned char magic_dataport[9]	= {'D','a','t','a','p','o','r','t',' '};
-static const unsigned char magic_form[5]	= {'F','o','r','m',' '};
-static const unsigned char magic_menusuite[10]	= {'M','e','n','u','S','u','i','t','e',' '};
-static const unsigned char magic_report[7]	= {'R','e','p','o','r','t',' '};
-static const unsigned char magic_table[6]	= {'T','a','b','l','e',' '};
-static const unsigned char magic_xmlport[8]	= {'X','M','L','p','o','r','t',' '};
+static int header_check_fob(const unsigned char *buffer, const unsigned int buffer_size, const unsigned int safe_header_only, const file_recovery_t *file_recovery, file_recovery_t *file_recovery_new)
+{
+  static const unsigned char sign_navnl[5]	= {'N','A','V','N','L'};
+  static const unsigned char sign_navw[4]	= {'N','A','V','W'};
+  if(td_memmem(buffer, buffer_size, sign_navnl, sizeof(sign_navnl))==NULL &&
+      td_memmem(buffer, buffer_size, sign_navw, sizeof(sign_navw))==NULL)
+    return 0;
+  reset_file_recovery(file_recovery_new);
+  file_recovery_new->extension=file_hint_fob.extension;
+  file_recovery_new->min_filesize=10;
+  return 1;
+}
 
 static void register_header_check_fob(file_stat_t *file_stat)
 {
-  register_header_check(0, magic_codeunit,  sizeof(magic_codeunit), 	&header_check_fob, file_stat);
-  register_header_check(0, magic_dataport,  sizeof(magic_dataport), 	&header_check_fob, file_stat);
-  register_header_check(0, magic_form,      sizeof(magic_form), 	&header_check_fob, file_stat);
-  register_header_check(0, magic_menusuite, sizeof(magic_menusuite), 	&header_check_fob, file_stat);
-  register_header_check(0, magic_report,    sizeof(magic_report), 	&header_check_fob, file_stat);
-  register_header_check(0, magic_table,     sizeof(magic_table), 	&header_check_fob, file_stat);
-  register_header_check(0, magic_xmlport,   sizeof(magic_xmlport), 	&header_check_fob, file_stat);
-}
-
-static int header_check_fob(const unsigned char *buffer, const unsigned int buffer_size, const unsigned int safe_header_only, const file_recovery_t *file_recovery, file_recovery_t *file_recovery_new)
-{
-  if((memcmp(buffer, magic_codeunit, sizeof(magic_codeunit))==0 ||
-      memcmp(buffer, magic_dataport, sizeof(magic_dataport))==0 ||
-      memcmp(buffer, magic_form, sizeof(magic_form))==0 ||
-      memcmp(buffer, magic_menusuite, sizeof(magic_menusuite))==0 ||
-      memcmp(buffer, magic_report, sizeof(magic_report))==0 ||
-      memcmp(buffer, magic_table, sizeof(magic_table))==0 ||
-      memcmp(buffer, magic_xmlport, sizeof(magic_xmlport))==0) &&
-      (td_memmem(buffer, buffer_size, sign_navnl, sizeof(sign_navnl))!=NULL ||
-       td_memmem(buffer, buffer_size, sign_navw, sizeof(sign_navw))!=NULL))
-  {
-    reset_file_recovery(file_recovery_new);
-    file_recovery_new->extension=file_hint_fob.extension;
-    return 1;
-  }
-  return 0;
+  register_header_check(0, "Codeunit ",  	 9, &header_check_fob, file_stat);
+  register_header_check(0, "Dataport ",  	 9, &header_check_fob, file_stat);
+  register_header_check(0, "Form ",		 5, &header_check_fob, file_stat);
+  register_header_check(0, "MenuSuite ",	10, &header_check_fob, file_stat);
+  register_header_check(0, "Report ",		 7, &header_check_fob, file_stat);
+  register_header_check(0, "Table ",		 6, &header_check_fob, file_stat);
+  register_header_check(0, "XMLport ",		 8, &header_check_fob, file_stat);
 }
