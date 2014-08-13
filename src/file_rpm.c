33cb72cab3c27c1add99b0d8b78c8f82b545f1da
Christophe Grenier <grenier@cgsecurity.org>
2014-08-13 08:36:02 +0200

    PhotoRec: stricter check for .rpm, get the package name

--- src/file_rpm.c
+++ src/file_rpm.c
@@ -45,22 +45,58 @@ const file_hint_t file_hint_rpm= {
   .register_header_check=&register_header_check_rpm
 };
 
-static const unsigned char rpm_header[5]= {0xed, 0xab, 0xee, 0xdb, 0x3};
+struct rpmlead {
+  unsigned char magic[4];
+  unsigned char major, minor;
+  uint16_t type;
+  uint16_t archnum;
+  char name[66];
+  uint16_t osnum;
+  uint16_t signature_type;
+  char reserved[16];
+} __attribute__ ((__packed__));
 
-static void register_header_check_rpm(file_stat_t *file_stat)
+static void file_rename_rpm(const char *old_filename)
 {
-  register_header_check(0, rpm_header,sizeof(rpm_header), &header_check_rpm, file_stat);
+  FILE *file;
+  struct rpmlead hdr;
+  if((file=fopen(old_filename, "rb"))==NULL)
+    return;
+  if(fread(&hdr, sizeof(hdr), 1, file)!=1)
+  {
+    fclose(file);
+    return ;
+  }
+  fclose(file);
+  file_rename(old_filename, &hdr.name, 66, 0, "rpm", 0);
 }
 
 static int header_check_rpm(const unsigned char *buffer, const unsigned int buffer_size, const unsigned int safe_header_only, const file_recovery_t *file_recovery, file_recovery_t *file_recovery_new)
 {
-  /* RPM v3 */
-  if(memcmp(buffer, rpm_header, sizeof(rpm_header))==0)
+  const struct rpmlead *hdr=(const struct rpmlead *)buffer;
+  if(be16(hdr->type)>1)	/* 0=bin 1=src */
+    return 0;
+  switch(be16(hdr->signature_type))
   {
-    reset_file_recovery(file_recovery_new);
-    file_recovery_new->min_filesize=(96 + 16 + 16); /* file header + checksum + content header */
-    file_recovery_new->extension=file_hint_rpm.extension;
-    return 1;
+    case 0:	/* RPMSIG_NONE */
+    case 1:	/* RPMSIG_PGP262_1024 */
+    case 5:	/* RPMSIG_HEADERSIG */
+      break;
+    default:
+    return 0;
   }
-  return 0;
+  if(hdr->name[0]=='\0')
+    return 0;
+  reset_file_recovery(file_recovery_new);
+  file_recovery_new->min_filesize=(96 + 16 + 16); /* file header + checksum + content header */
+  file_recovery_new->extension=file_hint_rpm.extension;
+  file_recovery_new->file_rename=&file_rename_rpm;
+  return 1;
+}
+
+static void register_header_check_rpm(file_stat_t *file_stat)
+{
+  /* RPM v3 */
+  static const unsigned char rpm_header[5]= {0xed, 0xab, 0xee, 0xdb, 0x3};
+  register_header_check(0, rpm_header,sizeof(rpm_header), &header_check_rpm, file_stat);
 }
