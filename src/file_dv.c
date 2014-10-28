785805156445ff209ac22eccadccdcdde7bd740c
Christophe Grenier <grenier@cgsecurity.org>
2014-10-28 11:11:49 +0100

    PhotoRec: recover .dv using PAL format

--- src/file_dv.c
+++ src/file_dv.c
@@ -42,7 +42,7 @@ const file_hint_t file_hint_dv= {
   .register_header_check=&register_header_check_dv
 };
 
-static data_check_t data_check_dv(const unsigned char *buffer, const unsigned int buffer_size, file_recovery_t *file_recovery)
+static data_check_t data_check_NTSC(const unsigned char *buffer, const unsigned int buffer_size, file_recovery_t *file_recovery)
 {
   while(file_recovery->calculated_file_size + buffer_size/2  >= file_recovery->file_size &&
       file_recovery->calculated_file_size + 8 < file_recovery->file_size + buffer_size/2)
@@ -57,25 +57,117 @@ static data_check_t data_check_dv(const unsigned char *buffer, const unsigned in
   return DC_CONTINUE;
 }
 
+static void file_check_dv_NTSC(file_recovery_t *fr)
+{
+  unsigned char buffer_header[512];
+  unsigned char buffer[120000];
+  uint64_t fs=fr->file_size/120000*120000;
+  if(
+#ifdef HAVE_FSEEKO
+      fseeko(fr->handle, 0, SEEK_SET) < 0 ||
+#else
+      fseek(fr->handle, 0, SEEK_SET) < 0 ||
+#endif
+      fread(&buffer_header, sizeof(buffer_header), 1, fr->handle) != 1)
+    return ;
+  if(fs > 0)
+    fs-=120000;
+  if(fs > 0)
+    fs-=120000;
+  while(fs < fr->file_size &&
+#ifdef HAVE_FSEEKO
+      fseeko(fr->handle, fs, SEEK_SET) >= 0 &&
+#else
+      fseek(fr->handle, fs, SEEK_SET) >= 0 &&
+#endif
+      fread(&buffer, sizeof(buffer), 1, fr->handle) == 1)
+  {
+    unsigned int i;
+    for(i=1; i<sizeof(buffer); i+=0x50)
+      if((buffer[i]&0x0f)!=(buffer_header[1]&0x0f))
+      {
+	fr->file_size=fs;
+	return;
+      }
+    fs+=sizeof(buffer);
+  }
+  fr->file_size=fs;
+}
+
+static data_check_t data_check_PAL(const unsigned char *buffer, const unsigned int buffer_size, file_recovery_t *file_recovery)
+{
+  while(file_recovery->calculated_file_size + buffer_size/2  >= file_recovery->file_size &&
+      file_recovery->calculated_file_size + 8 < file_recovery->file_size + buffer_size/2)
+  {
+    const unsigned int i=file_recovery->calculated_file_size - file_recovery->file_size + buffer_size/2;
+    if(buffer[i]==0x1f && buffer[i+1]==0x07 && buffer[i+2]==0x00 &&
+	buffer[i+5]==0x78 && buffer[i+6]==0x78 && buffer[i+7]==0x78)
+      file_recovery->calculated_file_size+=144000;
+    else
+      return DC_STOP;
+  }
+  return DC_CONTINUE;
+}
+
+static void file_check_dv_PAL(file_recovery_t *fr)
+{
+  unsigned char buffer_header[512];
+  unsigned char buffer[144000];
+  uint64_t fs=fr->file_size/144000*144000;
+  if(
+#ifdef HAVE_FSEEKO
+      fseeko(fr->handle, 0, SEEK_SET) < 0 ||
+#else
+      fseek(fr->handle, 0, SEEK_SET) < 0 ||
+#endif
+      fread(&buffer_header, sizeof(buffer_header), 1, fr->handle) != 1)
+    return ;
+  if(fs > 0)
+    fs-=144000;
+  if(fs > 0)
+    fs-=144000;
+  while(fs < fr->file_size &&
+#ifdef HAVE_FSEEKO
+      fseeko(fr->handle, fs, SEEK_SET) >= 0 &&
+#else
+      fseek(fr->handle, fs, SEEK_SET) >= 0 &&
+#endif
+      fread(&buffer, sizeof(buffer), 1, fr->handle) == 1)
+  {
+    unsigned int i;
+    for(i=1; i<sizeof(buffer); i+=0x50)
+      if((buffer[i]&0x0f)!=(buffer_header[1]&0x0f))
+      {
+	fr->file_size=fs;
+	return;
+      }
+    fs+=sizeof(buffer);
+  }
+  fr->file_size=fs;
+}
+
+
 static int header_check_dv(const unsigned char *buffer, const unsigned int buffer_size, const unsigned int safe_header_only, const file_recovery_t *file_recovery, file_recovery_t *file_recovery_new)
 {
   if(file_recovery->file_stat!=NULL &&
       file_recovery->file_stat->file_hint==&file_hint_dv)
     return 0;
-  /* The header may be only 3 bytes */
-  if(buffer[0]==0x1f && buffer[1]==0x07 && buffer[2]==0x00 && buffer[5]==0x78 && buffer[6]==0x78 && buffer[7]==0x78)
-  {
-    /* NTSC */
-    reset_file_recovery(file_recovery_new);
-    file_recovery_new->extension=file_hint_dv.extension;
-    if(file_recovery_new->blocksize >= 8)
-    {
-      file_recovery_new->data_check=&data_check_dv;
-      file_recovery_new->file_check=&file_check_size;
-    }
+  if(buffer[0]!=0x1f || buffer[1]!=0x07 || buffer[2]!=0x00 || buffer[5]!=0x78 || buffer[6]!=0x78 || buffer[7]!=0x78)
+    return 0;
+  reset_file_recovery(file_recovery_new);
+  file_recovery_new->extension=file_hint_dv.extension;
+  if((buffer[3]&0x80)==0)
+    file_recovery_new->file_check=&file_check_dv_NTSC;
+  else
+    file_recovery_new->file_check=&file_check_dv_PAL;
+  if(file_recovery_new->blocksize < 8)
     return 1;
-  }
-  return 0;
+  // Each frame contains exactly 120000 bytes in NTSC, 144000 in PAL.
+  if((buffer[3]&0x80)==0)
+    file_recovery_new->data_check=&data_check_NTSC;
+  else
+    file_recovery_new->data_check=&data_check_PAL;
+  return 1;
 }
 
 static void register_header_check_dv(file_stat_t *file_stat)
