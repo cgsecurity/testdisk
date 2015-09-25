cgsecurity/testdisk

commit 00557961744f0a88000678db179a84a4a0e4fdfd

PhotoRec: reduce the number of backward search

src/filegen.c
@@ -582,3 +582,45 @@ int file_rename_unicode(file_recovery_t *file_recovery, const void *buffer, cons
   free(new_filename);
   return 0;
 }
+
+static uint64_t offset_skipped_header=0;
+
+void header_ignored(const file_recovery_t *file_recovery_new)
+{
+  if(file_recovery_new==NULL)
+  {
+    offset_skipped_header=0;
+    return ;
+  }
+  if(file_recovery_new->location.start==0 || offset_skipped_header==0)
+    offset_skipped_header=file_recovery_new->location.start;
+}
+
+void get_prev_location_smart(alloc_data_t *list_search_space, alloc_data_t **current_search_space, uint64_t *offset, const uint64_t prev_location)
+{
+  alloc_data_t *file_space=*current_search_space;
+  uint64_t size=0;
+  int nbr;
+  if(offset_skipped_header==0)
+    return ;
+  offset_skipped_header=0;
+  /* Search backward the first fragment of a file not successfully recovered
+   * Limit the search to 3 fragments or 200 MB */
+  for(nbr=0; nbr<3 && size < (uint64_t)200*1024*1024; nbr++)
+  {
+    file_space=td_list_entry(file_space->list.prev, alloc_data_t, list);
+    if(file_space==list_search_space)
+      return;
+    if(file_space->start <= offset_skipped_header && offset_skipped_header < file_space->end)
+    {
+      *current_search_space=file_space;
+      *offset=offset_skipped_header;
+      return ;
+    }
+    if(file_space->start < prev_location)
+      return ;
+    size+=file_space->end - file_space->start + 1;
+    *current_search_space=file_space;
+    *offset=file_space->start;
+  }
+}

src/file_dad.c
@@ -73,13 +73,16 @@ static data_check_t data_check_dad(const unsigned char *buffer, const unsigned i
 static int header_check_dad(const unsigned char *buffer, const unsigned int buffer_size, const unsigned int safe_header_only, const file_recovery_t *file_recovery, file_recovery_t *file_recovery_new)
 {
   const struct dad_header *dad=(const struct dad_header *)buffer;
+  if(le32(dad->size)<16)
+    return 0;
   if(file_recovery->file_stat!=NULL &&
       file_recovery->file_stat->file_hint==&file_hint_dad &&
       (file_recovery->calculated_file_size==file_recovery->file_size ||
        file_recovery->blocksize < 16))
+  {
+    header_ignored(file_recovery_new);
     return 0;
-  if(le32(dad->size)<16)
-    return 0;
+  }
   reset_file_recovery(file_recovery_new);
   file_recovery_new->extension=file_hint_dad.extension;
   file_recovery_new->min_filesize=le32(dad->size);

src/file_dv.c
@@ -136,11 +136,14 @@ static void file_check_dv_PAL(file_recovery_t *fr)
 
 static int header_check_dv(const unsigned char *buffer, const unsigned int buffer_size, const unsigned int safe_header_only, const file_recovery_t *file_recovery, file_recovery_t *file_recovery_new)
 {
-  if(file_recovery->file_stat!=NULL &&
-      file_recovery->file_stat->file_hint==&file_hint_dv)
-    return 0;
   if(buffer[0]!=0x1f || buffer[1]!=0x07 || buffer[2]!=0x00 || buffer[5]!=0x78 || buffer[6]!=0x78 || buffer[7]!=0x78)
     return 0;
+  if(file_recovery->file_stat!=NULL &&
+      file_recovery->file_stat->file_hint==&file_hint_dv)
+  {
+    header_ignored(file_recovery_new);
+    return 0;
+  }
   reset_file_recovery(file_recovery_new);
   file_recovery_new->extension=file_hint_dv.extension;
   if((buffer[3]&0x80)==0)

src/file_dvr.c
@@ -46,7 +46,10 @@ static int header_check_dvr(const unsigned char *buffer, const unsigned int buff
   if(memcmp(&buffer[8], "XVID", 4)!=0)
     return 0;
   if(file_recovery->file_stat!=NULL && file_recovery->file_stat->file_hint==&file_hint_dvr)
+  {
+    header_ignored(file_recovery_new);
     return 0;
+  }
   reset_file_recovery(file_recovery_new);
   file_recovery_new->extension=file_hint_dvr.extension;
   file_recovery_new->min_filesize=0x10;

src/file_edb.c
@@ -49,7 +49,10 @@ static int header_check_edb(const unsigned char *buffer, const unsigned int buff
   if(file_recovery->file_stat!=NULL &&
       file_recovery->file_stat->file_hint==&file_hint_edb &&
       file_recovery->file_size==4096)
+  {
+    header_ignored(file_recovery_new);
     return 0;
+  }
   reset_file_recovery(file_recovery_new);
   file_recovery_new->extension=file_hint_edb.extension;
   return 1;

src/file_ext2.c
@@ -62,7 +62,10 @@ static int header_check_ext2_fs(const unsigned char *buffer, const unsigned int 
   if(file_recovery->file_stat!=NULL &&
        file_recovery->file_stat->file_hint==&file_hint_ext2_fs &&
        file_recovery->calculated_file_size==(uint64_t)le32(sb->s_blocks_count)*(EXT2_MIN_BLOCK_SIZE<<le32(sb->s_log_block_size)))
+  {
+    header_ignored(file_recovery_new);
     return 0;
+  }
   reset_file_recovery(file_recovery_new);
   file_recovery_new->extension=file_hint_ext2_fs.extension;
   file_recovery_new->calculated_file_size=(uint64_t)le32(sb->s_blocks_count)*(EXT2_MIN_BLOCK_SIZE<<le32(sb->s_log_block_size));

src/file_fdb.c
@@ -45,7 +45,10 @@ static int header_check_fdb(const unsigned char *buffer, const unsigned int buff
 {
   if(file_recovery->file_stat!=NULL &&
       file_recovery->file_stat->file_hint==&file_hint_fdb)
+  {
+    header_ignored(file_recovery_new);
     return 0;
+  }
   reset_file_recovery(file_recovery_new);
   file_recovery_new->extension=file_hint_fdb.extension;
   return 1;

src/file_gsm.c
@@ -68,9 +68,6 @@ static int header_check_gsm(const unsigned char *buffer, const unsigned int buff
 {
   const struct block_header *hdr;
   unsigned int i=0;
-  if(file_recovery->file_stat!=NULL
-      &&  file_recovery->file_stat->file_hint==&file_hint_gsm)
-    return 0;
   for(i=0, hdr=(const struct block_header *)buffer;
       i * sizeof(struct block_header) < file_recovery_new->blocksize;
       i++, hdr++)
@@ -80,6 +77,12 @@ static int header_check_gsm(const unsigned char *buffer, const unsigned int buff
   }
   if(i<3)
     return 0;
+  if(file_recovery->file_stat!=NULL
+      &&  file_recovery->file_stat->file_hint==&file_hint_gsm)
+  {
+    header_ignored(file_recovery_new);
+    return 0;
+  }
   reset_file_recovery(file_recovery_new);
   file_recovery_new->extension=file_hint_gsm.extension;
   file_recovery_new->min_filesize=sizeof(struct block_header);

commit 21827baef53aaa4a8c97f35850722b93c42f5914

Fix regression introduced in commit 00557961744f0a88000678db179a84a4a0e4fdfd

src/filegen.c
@@ -604,7 +604,6 @@ void get_prev_location_smart(alloc_data_t *list_search_space, alloc_data_t **cur
   int nbr;
   if(offset_skipped_header==0)
     return ;
-  offset_skipped_header=0;
   /* Search backward the first fragment of a file not successfully recovered
    * Limit the search to 3 fragments or 200 MB */
   for(nbr=0; nbr<3 && size < (uint64_t)200*1024*1024; nbr++)
@@ -616,6 +615,7 @@ void get_prev_location_smart(alloc_data_t *list_search_space, alloc_data_t **cur
     {
       *current_search_space=file_space;
       *offset=offset_skipped_header;
+      offset_skipped_header=0;
       return ;
     }
     if(file_space->start < prev_location)
@@ -624,4 +624,5 @@ void get_prev_location_smart(alloc_data_t *list_search_space, alloc_data_t **cur
     *current_search_space=file_space;
     *offset=file_space->start;
   }
+  offset_skipped_header=0;
 }