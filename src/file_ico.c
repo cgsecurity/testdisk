558b101896d01d7b5ba185e1161b6b3fc3a542b7
Christophe Grenier <grenier@cgsecurity.org>
2015-10-11 10:28:25 +0200

    PhotoRec: recover .ico containing up to 9 icones instead of only 1

--- src/file_ico.c
+++ src/file_ico.c
@@ -30,6 +30,7 @@
 #include "types.h"
 #include "common.h"
 #include "filegen.h"
+#include "log.h"
 
 static void register_header_check_ico(file_stat_t *file_stat);
 static int header_check_ico(const unsigned char *buffer, const unsigned int buffer_size, const unsigned int safe_header_only, const file_recovery_t *file_recovery, file_recovery_t *file_recovery_new);
@@ -43,11 +44,28 @@ const file_hint_t file_hint_ico= {
   .register_header_check=&register_header_check_ico
 };
 
-static const unsigned char header_ico[6]= 	{0x00 , 0x00, 0x01, 0x00, 0x01, 0x00};
+static const unsigned char header_ico1[6]=	{0x00 , 0x00, 0x01, 0x00, 0x01, 0x00};
+static const unsigned char header_ico2[6]=	{0x00 , 0x00, 0x01, 0x00, 0x02, 0x00};
+static const unsigned char header_ico3[6]=	{0x00 , 0x00, 0x01, 0x00, 0x03, 0x00};
+static const unsigned char header_ico4[6]=	{0x00 , 0x00, 0x01, 0x00, 0x04, 0x00};
+static const unsigned char header_ico5[6]=	{0x00 , 0x00, 0x01, 0x00, 0x05, 0x00};
+static const unsigned char header_ico6[6]=	{0x00 , 0x00, 0x01, 0x00, 0x06, 0x00};
+static const unsigned char header_ico7[6]=	{0x00 , 0x00, 0x01, 0x00, 0x07, 0x00};
+static const unsigned char header_ico8[6]=	{0x00 , 0x00, 0x01, 0x00, 0x08, 0x00};
+static const unsigned char header_ico9[6]=	{0x00 , 0x00, 0x01, 0x00, 0x09, 0x00};
+
 
 static void register_header_check_ico(file_stat_t *file_stat)
 {
-  register_header_check(0, header_ico, sizeof(header_ico), &header_check_ico, file_stat);
+  register_header_check(0, header_ico1, sizeof(header_ico1), &header_check_ico, file_stat);
+  register_header_check(0, header_ico2, sizeof(header_ico2), &header_check_ico, file_stat);
+  register_header_check(0, header_ico3, sizeof(header_ico3), &header_check_ico, file_stat);
+  register_header_check(0, header_ico4, sizeof(header_ico4), &header_check_ico, file_stat);
+  register_header_check(0, header_ico5, sizeof(header_ico5), &header_check_ico, file_stat);
+  register_header_check(0, header_ico6, sizeof(header_ico6), &header_check_ico, file_stat);
+  register_header_check(0, header_ico7, sizeof(header_ico7), &header_check_ico, file_stat);
+  register_header_check(0, header_ico8, sizeof(header_ico8), &header_check_ico, file_stat);
+  register_header_check(0, header_ico9, sizeof(header_ico9), &header_check_ico, file_stat);
 }
 
 /*
@@ -77,26 +95,65 @@ static int header_check_ico(const unsigned char *buffer, const unsigned int buff
 {
   const struct ico_header *ico=(const struct ico_header*)buffer;
   const struct ico_directory *ico_dir=(const struct ico_directory*)(ico+1);
-  /* Recover square icon with a single image */
-  if(le16(ico->reserved)==0 && le16(ico->type)==1 && le16(ico->count)==1 &&
-      (ico_dir->reserved==0 || ico_dir->reserved==255) &&
-      (ico_dir->color_planes==0 || ico_dir->color_planes==1) &&
-      ico_dir->width==ico_dir->heigth &&
-      ico_dir->width>=16 &&
-      (le16(ico_dir->bits_per_pixel)==1 ||
-       le16(ico_dir->bits_per_pixel)==4 ||
-       le16(ico_dir->bits_per_pixel)==8 ||
-       le16(ico_dir->bits_per_pixel)==16 ||
-       le16(ico_dir->bits_per_pixel)==32) &&
-       le32(ico_dir->bitmap_offset) >= sizeof(struct ico_header)+le16(ico->count)*sizeof(struct ico_directory) &&
-       le32(ico_dir->bitmap_size) > 0)
+  unsigned int i;
+  uint64_t fs=0;
+#ifdef DEBUG_ICO
+  log_info("ICO: reserved=%u type=%u count=%u\n", le16(ico->reserved), le16(ico->type), le16(ico->count));
+#endif
+  if(le16(ico->reserved)!=0 || le16(ico->type)!=1 || le16(ico->count)==0)
+    return 0;
+  for(i=0, ico_dir=(const struct ico_directory*)(ico+1);
+      i<le16(ico->count);
+      i++, ico_dir++)
   {
-    reset_file_recovery(file_recovery_new);
-    file_recovery_new->extension=file_hint_ico.extension;
-    file_recovery_new->calculated_file_size=(uint64_t)le32(ico_dir->bitmap_size) + le32(ico_dir->bitmap_offset);
-    file_recovery_new->data_check=&data_check_size;
-    file_recovery_new->file_check=&file_check_size;
-    return 1;
+#ifdef DEBUG_ICO
+    log_info("ICO%u: reserved=%u color_planes=%u width=%u heigth=%u bps=%u offset=%u size=%u\n",
+	i, ico_dir->reserved, le16(ico_dir->color_planes), ico_dir->width, ico_dir->heigth, le16(ico_dir->bits_per_pixel),
+	le32(ico_dir->bitmap_offset), le32(ico_dir->bitmap_size));
+#endif
+    if(ico_dir->reserved!=0 && ico_dir->reserved!=255)
+      return 0;
+    if(le16(ico_dir->color_planes)>1)
+      return 0;
+    if(ico_dir->width!=ico_dir->heigth)
+      return 0; /* Reject non square icon */
+    switch(ico_dir->width)
+    {
+      case 16:
+      case 24:
+      case 32:
+      case 48:
+      case 64:
+      case 128:
+      case 0:	/* 256 */
+	break;
+      default:
+	return 0;
+    }
+    switch(le16(ico_dir->bits_per_pixel))
+    {
+      case 0:
+      case 1:
+      case 4:
+      case 8:
+      case 16:
+      case 24:
+      case 32:
+	break;
+      default:
+	return 0;
+    }
+    if(le32(ico_dir->bitmap_size)==0)
+      return 0;
+    if(le32(ico_dir->bitmap_offset) < sizeof(struct ico_header)+le16(ico->count)*sizeof(struct ico_directory))
+      return 0;
+    if(fs < (uint64_t)le32(ico_dir->bitmap_size) + le32(ico_dir->bitmap_offset))
+      fs=(uint64_t)le32(ico_dir->bitmap_size) + le32(ico_dir->bitmap_offset);
   }
-  return 0;
+  reset_file_recovery(file_recovery_new);
+  file_recovery_new->extension=file_hint_ico.extension;
+  file_recovery_new->calculated_file_size=fs;
+  file_recovery_new->data_check=&data_check_size;
+  file_recovery_new->file_check=&file_check_size;
+  return 1;
 }
