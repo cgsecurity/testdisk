588fa1e76e632b8a43f1af424e9d47c7e99348a5
François Revol <revol@free.fr>
2016-07-24 19:42:12 +0200

    Haiku: widen globbing on device names
    We also need to match:
    /dev/disk/ata/0/slave/raw
    /dev/disk/usb/0/0/raw

--- src/hdaccess.c
+++ src/hdaccess.c
@@ -384,7 +384,7 @@ list_disk_t *hd_parse(list_disk_t *list_disk, const int verbose, const int testd
 #elif defined(__HAIKU__)
   {
 #ifdef HAVE_GLOB_H
-    list_disk=hd_glob_parse("/dev/disk/*/*/master/raw", list_disk, verbose, testdisk_mode);
+    list_disk=hd_glob_parse("/dev/disk/*/*/*/raw", list_disk, verbose, testdisk_mode);
 #endif
   }
 #else
