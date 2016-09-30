e50fae5d6b1b0d83a5c351ab80d0ce661136ecec
Christophe Grenier <grenier@cgsecurity.org>
2016-09-30 16:14:45 +0200

    PhotoRec: remove a useless call to header_ignored() in src/file_fdb.c

--- src/file_fdb.c
+++ src/file_fdb.c
@@ -46,7 +46,6 @@ static int header_check_fdb(const unsigned char *buffer, const unsigned int buff
   if(file_recovery->file_stat!=NULL &&
       file_recovery->file_stat->file_hint==&file_hint_fdb)
   {
-    header_ignored(file_recovery_new);
     return 0;
   }
   reset_file_recovery(file_recovery_new);
