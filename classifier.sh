33f5239767ab4adf63fff1126fed083bfa6bffbe
Pablo Valdés <p.valdes334@gmail.com>
2017-02-04 14:09:21 +0100

    Update classifier.sh
    Name of the directories improved. Support for further classification inside each group.

--- classifier.sh
+++ classifier.sh
@@ -3,21 +3,31 @@
 ## with other files of the same type, making much easier to find what we are looking for. 
 ## It the dir does not exist will be created automatically in the current working directory.
 
-## i.e. All Phyton scripts in recup_dirXXX will be automatically moved to a new dir named Python-script/, 
-## all makefiles to makefile/ all chunks of C++ files to C++-source-ascii-text, etc... 
+## i.e. All Phyton scripts in recup_dirXXX will be automatically moved to subdirectories in a new dir named Python/, 
+## all makefiles to makefile/ all chunks of C++ files to C++/, etc... 
 
-## 2017-02-04 name of the directories improved. Maybe too much verbose now?
+## 2017-02-04 name of the directories improved. Support for further classification inside each group.
 
 for i in `find . -type f`; do
-     typedir=`file $i | awk 'BEGIN {OFS="-";} {gsub("/" , "-"); gsub("," , ""); if($6) print $2,$3,$4,$5,$6; else if($5) print $2,$3,$4,$5; else if($4) print $2,$3,$4; else if($3) print $2,$3; else print $2}'`;
+    parentdir=`file $i | awk '{gsub("/" , "-"); gsub("," , ""); print $2}'`;
+    typedir=`file $i | awk 'BEGIN {OFS="-";} {gsub("/" , "-"); gsub("," , ""); if($6) print $2,$3,$4,$5,$6; else if($5) print $2,$3,$4,$5; else if($4) print $2,$3,$4; else if($3) print $2,$3; else print $2}'`;
 
-     if [ ! -d "../$typedir" ]
+    if [ ! -d "../$parentdir" ]
      then
-	 mkdir "../$typedir"
-     fi
-     mv -i $i ../$typedir/
-     typedir=""
-     
+	 mkdir "../$parentdir"
+       fi
+
+    if [[ $typedir == "$parentdir"* ]]
+    then
+	if [ ! -d "../$parentdir/$typedir" ]
+	then
+	    mkdir "../$parentdir/$typedir"
+	fi
+	mv -i $i ../$parentdir/$typedir/
+    fi
+    typedir=""
+    parentdir=""
+    
 done
 
 exit 0
