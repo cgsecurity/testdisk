4bb987a56966fabe951963469ad72fa45376d3cf
Pablo Valdés <p.valdes334@gmail.com>
2017-02-04 13:31:37 +0100

    Update classifier.sh
    Working in the name of directories. Can specify more types of directories now.

--- classifier.sh
+++ classifier.sh
@@ -3,18 +3,21 @@
 ## with other files of the same type, making much easier to find what we are looking for. 
 ## It the dir does not exist will be created automatically in the current working directory.
 
-## i.e. All Phyton scripts in recup_dirXXX will be automatically moved to a new dir named Python/, 
-## all makefiles to makefile/ all chunks of C++ files to C++, etc... 
+## i.e. All Phyton scripts in recup_dirXXX will be automatically moved to a new dir named Python-script/, 
+## all makefiles to makefile/ all chunks of C++ files to C++-source-ascii-text, etc... 
+
+## 2017-02-04 name of the directories improved. Maybe too much verbose now?
 
 for i in `find . -type f`; do
-     tipo=`file $i | awk '{gsub("/","-"); print $2}'`
+     typedir=`file $i | awk 'BEGIN {OFS="-";} {gsub("/" , "-"); gsub("," , ""); if($6) print $2,$3,$4,$5,$6; else if($5) print $2,$3,$4,$5; else if($4) print $2,$3,$4; else if($3) print $2,$3; else print $2}'`;
 
-     if [ ! -d "../$tipo" ]
+     if [ ! -d "../$typedir" ]
      then
-	 mkdir "../$tipo"
+	 mkdir "../$typedir"
      fi
-     mv -i $i ../$tipo/
-     tipo=""
+     mv -i $i ../$typedir/
+     typedir=""
+     
 done
 
 exit 0
