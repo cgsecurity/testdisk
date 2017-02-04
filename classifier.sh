936a9b7b5df4c2174aac6e9c5d4846030d7403b6
Pablo Valdés <p.valdes334@gmail.com>
2017-02-04 14:35:52 +0100

    Update classifier.sh
    Adding some notes explaining code

--- classifier.sh
+++ classifier.sh
@@ -1,7 +1,6 @@
 ## pvaldes 2017-01-27
 ## This script examine the content of each file recovered by photorec and will put it in a directory 
 ## with other files of the same type, making much easier to find what we are looking for. 
-## It the dir does not exist will be created automatically in the current working directory.
 
 ## i.e. All Phyton scripts in recup_dirXXX will be automatically moved to subdirectories in a new dir named Python/, 
 ## all makefiles to makefile/ all chunks of C++ files to C++/, etc... 
@@ -9,29 +8,34 @@
 ## 2017-02-04 name of the directories improved. Support for further classification inside each group.
 
 for i in `find . -type f`; do
-    parentdir=`file $i | awk '{gsub("/" , "-"); gsub("," , ""); print $2}'`;
+    classdir=`file $i | awk '{gsub("/" , "-"); gsub("," , ""); print $2}'`;
     typedir=`file $i | awk 'BEGIN {OFS="-";} {gsub("/" , "-"); gsub("," , ""); if($6) print $2,$3,$4,$5,$6; else if($5) print $2,$3,$4,$5; else if($4) print $2,$3,$4; else if($3) print $2,$3; else print $2}'`;
 
-    if [ ! -d "../$parentdir" ]
+## It the dir does not exist, create it in ../
+    if [ ! -d "../$classdir" ]
      then
-	 mkdir "../$parentdir"
+	 mkdir "../$classtdir"
     fi
        
-   if [ "$typedir" == "$parentdir"]
+## If the names of dir class (parent) and type (subdir) are the same, do not create subdirs 
+## and move files directly to the directory class
+   if [ "$typedir" == "$classdir"]
     then 
-   	mv -i $i ../$parentdir/
+   	mv -i $i ../$classdir/
     fi
-
-   if [[ $typedir == "$parentdir"* ]]
+## Create subdirs and and move all archives of its appropriate class and type.
+   if [[ $typedir == "$classdir"* ]]
     then
-	if [ ! -d "../$parentdir/$typedir" ]
+	if [ ! -d "../$classdir/$typedir" ]
 	then
-	    mkdir "../$parentdir/$typedir"
+	    mkdir "../$classdir/$typedir"
 	fi
-	mv -i $i ../$parentdir/$typedir/
+	mv -i $i ../$classdir/$typedir/
     fi
+    
+## reset variables
     typedir=""
-    parentdir=""
+    classdir=""
     
 done
 
