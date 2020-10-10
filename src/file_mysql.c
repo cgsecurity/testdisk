370d333d782287b34de396a1296837306a51760f
Christophe Grenier <grenier@cgsecurity.org>
2020-10-10 10:13:00 +0200

    src/file_mysql.c: add Frama-C annotations

--- src/file_mysql.c
+++ src/file_mysql.c
@@ -31,9 +31,10 @@
 #include "types.h"
 #include "filegen.h"
 
-
+/*@
+  @ requires \valid(file_stat);
+  @*/
 static void register_header_check_mysql(file_stat_t *file_stat);
-static int header_check_mysql(const unsigned char *buffer, const unsigned int buffer_size, const unsigned int safe_header_only, const file_recovery_t *file_recovery, file_recovery_t *file_recovery_new);
 
 const file_hint_t file_hint_mysql= {
   .extension="MYI",
@@ -44,24 +45,34 @@ const file_hint_t file_hint_mysql= {
   .register_header_check=&register_header_check_mysql
 };
 
-static const unsigned char mysql_header[4]= {0xfe, 0xfe, 0x07, 0x01};
-static const unsigned char mysql_header_def[2]= {0xfe, 0x01};
-
-static void register_header_check_mysql(file_stat_t *file_stat)
-{
-  register_header_check(0, mysql_header,sizeof(mysql_header), &header_check_mysql, file_stat);
-  register_header_check(0, mysql_header_def,sizeof(mysql_header_def), &header_check_mysql, file_stat);
-}
-
-static int header_check_mysql(const unsigned char *buffer, const unsigned int buffer_size, const unsigned int safe_header_only, const file_recovery_t *file_recovery, file_recovery_t *file_recovery_new)
+/*@
+  @ requires buffer_size > 0;
+  @ requires \valid_read(buffer+(0..buffer_size-1));
+  @ requires \valid_read(file_recovery);
+  @ requires file_recovery->file_stat==\null || valid_read_string((char*)file_recovery->filename);
+  @ requires \valid(file_recovery_new);
+  @ requires file_recovery_new->blocksize > 0;
+  @ requires separation: \separated(&file_hint_mysql, buffer+(..), file_recovery, file_recovery_new);
+  @*/
+static int header_check_myisam(const unsigned char *buffer, const unsigned int buffer_size, const unsigned int safe_header_only, const file_recovery_t *file_recovery, file_recovery_t *file_recovery_new)
 {
   /* MySQL MYISAM compressed data file Version 1 */
-  if(buffer[0]==0xfe && buffer[1]==0xfe && buffer[2]==0x07 && buffer[3]==0x01)
-  {
-    reset_file_recovery(file_recovery_new);
-    file_recovery_new->extension="MYI";
-    return 1;
-  }
+  reset_file_recovery(file_recovery_new);
+  file_recovery_new->extension="MYI";
+  return 1;
+}
+
+/*@
+  @ requires buffer_size >= 6;
+  @ requires \valid_read(buffer+(0..buffer_size-1));
+  @ requires \valid_read(file_recovery);
+  @ requires file_recovery->file_stat==\null || valid_read_string((char*)file_recovery->filename);
+  @ requires \valid(file_recovery_new);
+  @ requires file_recovery_new->blocksize > 0;
+  @ requires separation: \separated(&file_hint_mysql, buffer+(..), file_recovery, file_recovery_new);
+  @*/
+static int header_check_mysql(const unsigned char *buffer, const unsigned int buffer_size, const unsigned int safe_header_only, const file_recovery_t *file_recovery, file_recovery_t *file_recovery_new)
+{
   /* MySQL table definition file Version 7 up to 10 */
   if(buffer[0]==0xfe && buffer[1]==0x01 && (buffer[2]>=0x07 && buffer[2]<=0x0A) && buffer[3]==0x09 && buffer[5]==0)
   {
@@ -70,5 +81,13 @@ static int header_check_mysql(const unsigned char *buffer, const unsigned int bu
     return 1;
   }
   return 0;
+}
+
+static void register_header_check_mysql(file_stat_t *file_stat)
+{
+  static const unsigned char mysql_header[4]= {0xfe, 0xfe, 0x07, 0x01};
+  static const unsigned char mysql_header_def[2]= {0xfe, 0x01};
+  register_header_check(0, mysql_header,sizeof(mysql_header), &header_check_myisam, file_stat);
+  register_header_check(0, mysql_header_def,sizeof(mysql_header_def), &header_check_mysql, file_stat);
 }
 #endif
