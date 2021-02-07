59cbf8c83935563abd6b9086efb2bb333371a3aa
Christophe Grenier <grenier@cgsecurity.org>
2021-02-07 17:31:14 +0100

    src/file_1cd.c: add more frama-c annotations

--- src/file_1cd.c
+++ src/file_1cd.c
@@ -32,6 +32,7 @@
 #include "filegen.h"
 #include "common.h"
 
+/*@ requires \valid(file_stat); */
 static void register_header_check_1cd(file_stat_t *file_stat);
 
 const file_hint_t file_hint_1cd= {
@@ -50,6 +51,17 @@ struct header_1cd
   uint32_t size;
 } __attribute__ ((gcc_struct, __packed__));
 
+/*@
+  @ requires buffer_size >= sizeof(struct header_1cd);
+  @ requires \valid_read(buffer+(0..buffer_size-1));
+  @ requires valid_file_recovery(file_recovery);
+  @ requires \valid(file_recovery_new);
+  @ requires file_recovery_new->blocksize > 0;
+  @ requires separation: \separated(&file_hint_1cd, buffer+(..), file_recovery, file_recovery_new);
+  @ assigns  *file_recovery_new;
+  @ ensures \result == 0 || \result == 1;
+  @ ensures  \result!=0 ==> valid_file_recovery(file_recovery_new);
+  @*/
 static int header_check_1cd(const unsigned char *buffer, const unsigned int buffer_size, const unsigned int safe_header_only, const file_recovery_t *file_recovery, file_recovery_t *file_recovery_new)
 {
   const struct header_1cd *hdr=(const struct header_1cd *)buffer;
