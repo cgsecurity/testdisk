2e7dfd0ce4568fecf48dd3a1cf8018234ad10263
Christophe Grenier <grenier@cgsecurity.org>
2021-02-11 18:57:14 +0100

    src/file_3ds.c: add frama-c annotations

--- src/file_3ds.c
+++ src/file_3ds.c
@@ -32,6 +32,7 @@
 #include "filegen.h"
 #include "common.h"
 
+/*@ requires \valid(file_stat); */
 static void register_header_check_3ds(file_stat_t *file_stat);
 
 const file_hint_t file_hint_3ds= {
@@ -49,6 +50,17 @@ struct chunk_3ds
   uint32_t next_chunk;
 } __attribute__ ((gcc_struct, __packed__));
 
+/*@
+  @ requires buffer_size >= sizeof(struct chunk_3ds);
+  @ requires \valid_read(buffer+(0..buffer_size-1));
+  @ requires valid_file_recovery(file_recovery);
+  @ requires \valid(file_recovery_new);
+  @ requires file_recovery_new->blocksize > 0;
+  @ requires separation: \separated(&file_hint_3ds, buffer+(..), file_recovery, file_recovery_new);
+  @ assigns  *file_recovery_new;
+  @ ensures \result == 0 || \result == 1;
+  @ ensures  \result!=0 ==> valid_file_recovery(file_recovery_new);
+  @*/
 static int header_check_3ds(const unsigned char *buffer, const unsigned int buffer_size, const unsigned int safe_header_only, const file_recovery_t *file_recovery, file_recovery_t *file_recovery_new)
 {
   uint64_t fs;
