28df67cbb0152c43e0e4991143444d7919a72dae
Christophe Grenier <grenier@cgsecurity.org>
2021-02-13 10:09:44 +0100

    src/file_ape.c: add frama-c annotations

--- src/file_ape.c
+++ src/file_ape.c
@@ -31,6 +31,7 @@
 #include "filegen.h"
 #include "common.h"
 
+/*@ requires \valid(file_stat); */
 static void register_header_check_ape(file_stat_t *file_stat);
 
 const file_hint_t file_hint_ape= {
@@ -99,23 +100,36 @@ struct APE_HEADER
   uint32_t nSampleRate;		// the sample rate (typically 44100)
 };
 
-static int header_check_ape(const unsigned char *buffer, const unsigned int buffer_size, const unsigned int safe_header_only, const file_recovery_t *file_recovery, file_recovery_t *file_recovery_new)
+/*@
+  @ requires buffer_size >= sizeof(struct APE_HEADER_OLD);
+  @ requires \valid_read(buffer+(0..buffer_size-1));
+  @ requires \valid_read(file_recovery);
+  @ requires file_recovery->file_stat==\null || valid_read_string((char*)file_recovery->filename);
+  @ requires \valid(file_recovery_new);
+  @ requires file_recovery_new->blocksize > 0;
+  @ requires separation: \separated(&file_hint_ape, buffer, file_recovery, file_recovery_new);
+  @ ensures \result == 0 || \result == 1;
+  @ ensures  \result!=0 ==> valid_file_recovery(file_recovery_new);
+  @ assigns  *file_recovery_new;
+  @*/static int header_check_ape(const unsigned char *buffer, const unsigned int buffer_size, const unsigned int safe_header_only, const file_recovery_t *file_recovery, file_recovery_t *file_recovery_new)
 {
   const struct APE_HEADER_OLD *ape=(const struct APE_HEADER_OLD*)buffer;
   /* Version 3.96 released April 7, 2002, Version 4.06 March 17, 2009 */
   if(le16(ape->nVersion)>=3980)
   {
     const struct APE_DESCRIPTOR *descr=(const struct APE_DESCRIPTOR*)buffer;
-    if(le32(descr->nDescriptorBytes) < sizeof(struct APE_DESCRIPTOR))
+    const unsigned int nDescriptorBytes=le32(descr->nDescriptorBytes);
+    if(nDescriptorBytes < sizeof(struct APE_DESCRIPTOR))
       return 0;
     if(le32(descr->nHeaderDataBytes) > 0 && le32(descr->nHeaderDataBytes) < sizeof(struct APE_HEADER))
       return 0;
-    if(le32(descr->nDescriptorBytes) >= buffer_size)
+    if(nDescriptorBytes >= buffer_size)
       return 0;
-    if(le32(descr->nDescriptorBytes) + sizeof(struct APE_HEADER) >= buffer_size)
+    if(nDescriptorBytes + sizeof(struct APE_HEADER) >= buffer_size)
       return 0;
     {
-      const struct APE_HEADER *apeh=(const struct APE_HEADER*)&buffer[le32(descr->nDescriptorBytes)];
+      const struct APE_HEADER *apeh=(const struct APE_HEADER*)&buffer[nDescriptorBytes];
+      /*@ assert \valid_read(apeh); */
       if(le16(apeh->nChannels)<1 || le16(apeh->nChannels)>2)
 	return 0;
     }
