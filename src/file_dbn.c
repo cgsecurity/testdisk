2aadf95c9f4eaef849d75ea145bd02449f4c374d
Christophe Grenier <grenier@cgsecurity.org>
2021-02-14 18:18:57 +0100

    src/file_dbn.c: add frama-c annotations

--- src/file_dbn.c
+++ src/file_dbn.c
@@ -32,6 +32,7 @@
 #include "filegen.h"
 #include "memmem.h"
 
+/*@ requires \valid(file_stat); */
 static void register_header_check_dbn(file_stat_t *file_stat);
 
 const file_hint_t file_hint_dbn= {
@@ -43,6 +44,17 @@ const file_hint_t file_hint_dbn= {
   .register_header_check=&register_header_check_dbn
 };
 
+/*@
+  @ requires buffer_size >= 512;
+  @ requires \valid_read(buffer+(0..buffer_size-1));
+  @ requires valid_file_recovery(file_recovery);
+  @ requires \valid(file_recovery_new);
+  @ requires file_recovery_new->blocksize > 0;
+  @ requires separation: \separated(&file_hint_dbn, buffer+(..), file_recovery, file_recovery_new);
+  @ assigns  *file_recovery_new;
+  @ ensures \result == 0 || \result == 1;
+  @ ensures  \result!=0 ==> valid_file_recovery(file_recovery_new);
+  @*/
 static int header_check_dbn(const unsigned char *buffer, const unsigned int buffer_size, const unsigned int safe_header_only, const file_recovery_t *file_recovery, file_recovery_t *file_recovery_new)
 {
   if(td_memmem(buffer, 512, "[HEADER]", 8)==NULL)
