7562c6d422e16cc23f1490e3d4d89c3f3876ead6
Christophe Grenier <grenier@cgsecurity.org>
2021-02-14 18:32:05 +0100

    src/file_ddf.c: add frama-c annotations

--- src/file_ddf.c
+++ src/file_ddf.c
@@ -35,6 +35,7 @@
 #include "filegen.h"
 #include "common.h"
 
+/*@ requires \valid(file_stat); */
 static void register_header_check_ddf(file_stat_t *file_stat);
 
 const file_hint_t file_hint_ddf= {
@@ -80,6 +81,14 @@ struct MasterHeader
   char	   m_cRsvdData[120]; // (120) pad to 512 bytes
 } __attribute__ ((gcc_struct, __packed__));
 
+/*@
+  @ requires \valid_read(buffer+(0..sizeof(struct MasterHeader)-1));
+  @ requires \valid(file_recovery_new);
+  @ requires separation: \separated(&file_hint_ddf, buffer+(..), file_recovery_new);
+  @ assigns  *file_recovery_new;
+  @ ensures  \result == 1;
+  @ ensures  valid_file_recovery(file_recovery_new);
+  @*/
 static int header_check_aux(const unsigned char *buffer, file_recovery_t *file_recovery_new)
 {
   const char *date_asc=(const char *)&buffer[0x3f];
@@ -92,6 +101,17 @@ static int header_check_aux(const unsigned char *buffer, file_recovery_t *file_r
   return 1;
 }
 
+/*@
+  @ requires buffer_size >= sizeof(struct MasterHeader);
+  @ requires \valid_read(buffer+(0..buffer_size-1));
+  @ requires valid_file_recovery(file_recovery);
+  @ requires \valid(file_recovery_new);
+  @ requires file_recovery_new->blocksize > 0;
+  @ requires separation: \separated(&file_hint_ddf, buffer+(..), file_recovery, file_recovery_new);
+  @ assigns  *file_recovery_new;
+  @ ensures \result == 0 || \result == 1;
+  @ ensures  \result!=0 ==> valid_file_recovery(file_recovery_new);
+  @*/
 static int header_check_ddf3(const unsigned char *buffer, const unsigned int buffer_size, const unsigned int safe_header_only, const file_recovery_t *file_recovery, file_recovery_t *file_recovery_new)
 {
   const struct MasterHeader *h=(const struct MasterHeader *)buffer;
@@ -102,6 +122,17 @@ static int header_check_ddf3(const unsigned char *buffer, const unsigned int buf
   return header_check_aux(buffer, file_recovery_new);
 }
 
+/*@
+  @ requires buffer_size >= sizeof(struct MasterHeader);
+  @ requires \valid_read(buffer+(0..buffer_size-1));
+  @ requires valid_file_recovery(file_recovery);
+  @ requires \valid(file_recovery_new);
+  @ requires file_recovery_new->blocksize > 0;
+  @ requires separation: \separated(&file_hint_ddf, buffer+(..), file_recovery, file_recovery_new);
+  @ assigns  *file_recovery_new;
+  @ ensures \result == 0 || \result == 1;
+  @ ensures  \result!=0 ==> valid_file_recovery(file_recovery_new);
+  @*/
 static int header_check_ddf4(const unsigned char *buffer, const unsigned int buffer_size, const unsigned int safe_header_only, const file_recovery_t *file_recovery, file_recovery_t *file_recovery_new)
 {
   const struct MasterHeader *h=(const struct MasterHeader *)buffer;
@@ -112,6 +143,17 @@ static int header_check_ddf4(const unsigned char *buffer, const unsigned int buf
   return header_check_aux(buffer, file_recovery_new);
 }
 
+/*@
+  @ requires buffer_size >= sizeof(struct MasterHeader);
+  @ requires \valid_read(buffer+(0..buffer_size-1));
+  @ requires valid_file_recovery(file_recovery);
+  @ requires \valid(file_recovery_new);
+  @ requires file_recovery_new->blocksize > 0;
+  @ requires separation: \separated(&file_hint_ddf, buffer+(..), file_recovery, file_recovery_new);
+  @ assigns  *file_recovery_new;
+  @ ensures \result == 0 || \result == 1;
+  @ ensures  \result!=0 ==> valid_file_recovery(file_recovery_new);
+  @*/
 static int header_check_ddf5(const unsigned char *buffer, const unsigned int buffer_size, const unsigned int safe_header_only, const file_recovery_t *file_recovery, file_recovery_t *file_recovery_new)
 {
   const struct MasterHeader *h=(const struct MasterHeader *)buffer;
