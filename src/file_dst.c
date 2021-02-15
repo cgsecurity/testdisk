66f19119e5d15ab3e242720fb3e05c34f33f1a3f
Christophe Grenier <grenier@cgsecurity.org>
2021-02-15 09:46:55 +0100

    src/file_dst.c: fix frama-c warnings

--- src/file_dst.c
+++ src/file_dst.c
@@ -31,6 +31,7 @@
 #include "types.h"
 #include "filegen.h"
 
+/*@ requires \valid(file_stat); */
 static void register_header_check_dst(file_stat_t *file_stat);
 
 const file_hint_t file_hint_dst= {
@@ -42,18 +43,32 @@ const file_hint_t file_hint_dst= {
   .register_header_check=&register_header_check_dst
 };
 
+/*@
+  @ requires buffer_size >= 512;
+  @ requires \valid_read(buffer+(0..buffer_size-1));
+  @ requires valid_file_recovery(file_recovery);
+  @ requires \valid(file_recovery_new);
+  @ requires file_recovery_new->blocksize > 0;
+  @ requires separation: \separated(&file_hint_dst, buffer+(..), file_recovery, file_recovery_new);
+  @ assigns  *file_recovery_new;
+  @ ensures \result == 0 || \result == 1;
+  @ ensures  \result!=0 ==> valid_file_recovery(file_recovery_new);
+  @*/
 static int header_check_dst(const unsigned char *buffer, const unsigned int buffer_size, const unsigned int safe_header_only, const file_recovery_t *file_recovery, file_recovery_t *file_recovery_new)
 {
   unsigned int stitch_count=0;
+  char buf[8];
+  memcpy(&buf, &buffer[23], 7);
+  buf[7]='\0';
   if(memcmp(buffer, "LA:",3)!=0 || memcmp(&buffer[30], "\rCO:", 4)!=0)
     return 0;
-  if(sscanf((const char *) &buffer[23], "%u", &stitch_count) < 0)
+  if(sscanf(&buf, "%u", &stitch_count) < 0)
     return 0;
   reset_file_recovery(file_recovery_new);
   file_recovery_new->extension=file_hint_dst.extension;
   /* https://community.kde.org/Projects/Liberty/File_Formats/Tajima_Ternary */
   /* The header is 512 bytes */
-  file_recovery_new->calculated_file_size=512 + 3*stitch_count;
+  file_recovery_new->calculated_file_size=(uint64_t)512 + (uint64_t)3*stitch_count;
   file_recovery_new->data_check=&data_check_size;
   file_recovery_new->file_check=&file_check_size;
   return 1;
