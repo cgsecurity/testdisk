21e89f31e8617eba9b9978d760daa2ab89a8fea8
Christophe Grenier <grenier@cgsecurity.org>
2021-02-19 18:50:26 +0100

    src/file_exs.c: add frama-c annotations

--- src/file_exs.c
+++ src/file_exs.c
@@ -31,6 +31,7 @@
 #include "types.h"
 #include "filegen.h"
 
+/*@ requires \valid(file_stat); */
 static void register_header_check_exs(file_stat_t *file_stat);
 
 const file_hint_t file_hint_exs= {
@@ -42,6 +43,11 @@ const file_hint_t file_hint_exs= {
   .register_header_check=&register_header_check_exs
 };
 
+/*@
+  @ requires \valid(file_recovery);
+  @ requires valid_read_string((char*)&file_recovery->filename);
+  @ requires file_recovery->file_rename==&file_rename_exs;
+  @*/
 static void file_rename_exs(file_recovery_t *file_recovery)
 {
   unsigned char buffer[512];
@@ -54,6 +60,24 @@ static void file_rename_exs(file_recovery_t *file_recovery)
   file_rename(file_recovery, buffer, buffer_size, 0x14, "exs", 0);
 }
 
+
+/*@
+  @ requires buffer_size >= 0x14;
+  @ requires \valid_read(buffer+(0..buffer_size-1));
+  @ requires \valid_read(file_recovery);
+  @ requires file_recovery->file_stat==\null || valid_read_string((char*)file_recovery->filename);
+  @ requires \valid(file_recovery_new);
+  @ requires file_recovery_new->blocksize > 0;
+  @ requires separation: \separated(&file_hint_exs, buffer+(..), file_recovery, file_recovery_new);
+  @ ensures \result == 0 || \result == 1;
+  @ ensures (\result == 1) ==> (file_recovery_new->file_stat == \null);
+  @ ensures (\result == 1) ==> (file_recovery_new->handle == \null);
+  @ ensures (\result == 1) ==> (file_recovery_new->file_rename == &file_rename_exs);
+  @ ensures (\result == 1) ==> (valid_read_string(file_recovery_new->extension));
+  @ ensures (\result == 1) ==> \separated(file_recovery_new, file_recovery_new->extension);
+  @ ensures  \result!=0 ==> valid_file_recovery(file_recovery_new);
+  @ assigns  *file_recovery_new;
+  @*/
 static int header_check_exs(const unsigned char *buffer, const unsigned int buffer_size, const unsigned int safe_header_only, const file_recovery_t *file_recovery, file_recovery_t *file_recovery_new)
 {
   if(memcmp(&buffer[0x10], "TBOS", 4)!=0)
