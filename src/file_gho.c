da183d2a869e75a50b9dac39ab0b3b53dc0c4048
Christophe Grenier <grenier@cgsecurity.org>
2021-02-21 17:14:18 +0100

    src/file_gho.c: add frama-c annotations

--- src/file_gho.c
+++ src/file_gho.c
@@ -31,6 +31,7 @@
 #include "types.h"
 #include "filegen.h"
 
+/*@ requires \valid(file_stat); */
 static void register_header_check_gho(file_stat_t *file_stat);
 
 const file_hint_t file_hint_gho= {
@@ -42,7 +43,18 @@ const file_hint_t file_hint_gho= {
   .register_header_check=&register_header_check_gho
 };
 
-static int header_check_db(const unsigned char *buffer, const unsigned int buffer_size, const unsigned int safe_header_only, const file_recovery_t *file_recovery, file_recovery_t *file_recovery_new)
+/*@
+  @ requires buffer_size >= 16;
+  @ requires \valid_read(buffer+(0..buffer_size-1));
+  @ requires valid_file_recovery(file_recovery);
+  @ requires \valid(file_recovery_new);
+  @ requires file_recovery_new->blocksize > 0;
+  @ requires separation: \separated(&file_hint_gho, buffer+(..), file_recovery, file_recovery_new);
+  @ ensures \result == 0 || \result == 1;
+  @ ensures  \result!=0 ==> valid_file_recovery(file_recovery_new);
+  @ assigns  *file_recovery_new;
+  @*/
+static int header_check_ghost(const unsigned char *buffer, const unsigned int buffer_size, const unsigned int safe_header_only, const file_recovery_t *file_recovery, file_recovery_t *file_recovery_new)
 {
   static const unsigned char gho_header_next[8]= { 0x01, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 };
   if(memcmp (buffer+8, gho_header_next, sizeof(gho_header_next))!=0)
@@ -55,6 +67,6 @@ static int header_check_db(const unsigned char *buffer, const unsigned int buffe
 static void register_header_check_gho(file_stat_t *file_stat)
 {
   static const unsigned char gho_header[3]= { 0xfe, 0xef, 0x01 };
-  register_header_check(0, gho_header,sizeof(gho_header), &header_check_db, file_stat);
+  register_header_check(0, gho_header, sizeof(gho_header), &header_check_ghost, file_stat);
 }
 #endif
