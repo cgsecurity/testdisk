075108bb497196693999e047524b969d0b0818b4
Christophe Grenier <grenier@cgsecurity.org>
2021-02-24 19:32:22 +0100

    src/file_mk5.c: add frama-c annotations

--- src/file_mk5.c
+++ src/file_mk5.c
@@ -30,6 +30,7 @@
 #include "types.h"
 #include "filegen.h"
 
+/*@ requires \valid(file_stat); */
 static void register_header_check_mk5(file_stat_t *file_stat);
 
 const file_hint_t file_hint_mk5= {
@@ -41,6 +42,17 @@ const file_hint_t file_hint_mk5= {
   .register_header_check=&register_header_check_mk5
 };
 
+/*@
+  @ requires buffer_size >= 0x20;
+  @ requires \valid_read(buffer+(0..buffer_size-1));
+  @ requires valid_file_recovery(file_recovery);
+  @ requires \valid(file_recovery_new);
+  @ requires file_recovery_new->blocksize > 0;
+  @ requires separation: \separated(&file_hint_mk5, buffer+(..), file_recovery, file_recovery_new);
+  @ assigns  *file_recovery_new;
+  @ ensures \result == 0 || \result == 1;
+  @ ensures  \result!=0 ==> valid_file_recovery(file_recovery_new);
+  @*/
 static int header_check_mk5(const unsigned char *buffer, const unsigned int buffer_size, const unsigned int safe_header_only, const file_recovery_t *file_recovery, file_recovery_t *file_recovery_new)
 {
   static const unsigned char mk5_header2[4]= { 0x00, 0x40, 0x1c, 0x46 };
