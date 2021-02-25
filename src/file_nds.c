b2eb3b103d7db6aa16ac9f8c997a574afd80ea22
Christophe Grenier <grenier@cgsecurity.org>
2021-02-25 17:21:07 +0100

    src/file_nds.c: add frama-c annotations

--- src/file_nds.c
+++ src/file_nds.c
@@ -31,6 +31,7 @@
 #include "types.h"
 #include "filegen.h"
 
+/*@ requires \valid(file_stat); */
 static void register_header_check_nds(file_stat_t *file_stat);
 
 const file_hint_t file_hint_nds= {
@@ -42,6 +43,11 @@ const file_hint_t file_hint_nds= {
   .register_header_check=&register_header_check_nds
 };
 
+/*@
+  @ requires \valid(file_recovery);
+  @ requires valid_file_recovery(file_recovery);
+  @ requires file_recovery->file_rename==&file_rename_nds;
+  @*/
 static void file_rename_nds(file_recovery_t *file_recovery)
 {
   FILE *file;
@@ -57,6 +63,17 @@ static void file_rename_nds(file_recovery_t *file_recovery)
   file_rename(file_recovery, &buffer, 12, 0, file_hint_nds.extension, 0);
 }
 
+/*@
+  @ requires buffer_size >= 0x10;
+  @ requires \valid_read(buffer+(0..buffer_size-1));
+  @ requires valid_file_recovery(file_recovery);
+  @ requires \valid(file_recovery_new);
+  @ requires file_recovery_new->blocksize > 0;
+  @ requires separation: \separated(&file_hint_nds, buffer+(..), file_recovery, file_recovery_new);
+  @ assigns  *file_recovery_new;
+  @ ensures \result == 0 || \result == 1;
+  @ ensures  \result!=0 ==> valid_file_recovery(file_recovery_new);
+  @*/
 static int header_check_nds(const unsigned char *buffer, const unsigned int buffer_size, const unsigned int safe_header_only, const file_recovery_t *file_recovery, file_recovery_t *file_recovery_new)
 {
   if(memcmp(&buffer[0x0c], "NTRJ", 4)==0)
