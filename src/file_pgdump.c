b90dee6d5f238a6d56af069239189eea541c6c48
Christophe Grenier <grenier@cgsecurity.org>
2021-02-27 12:41:37 +0100

    src/file_pgdump.c: add Frama-C annotations

--- src/file_pgdump.c
+++ src/file_pgdump.c
@@ -83,6 +83,7 @@ struct pgdmp_hdr
   /* PG_VERSION */
 } __attribute__ ((gcc_struct, __packed__));
 
+/*@ requires \valid(file_stat); */
 static void register_header_check_pgdump(file_stat_t *file_stat);
 
 const file_hint_t file_hint_pgdump= {
@@ -94,6 +95,17 @@ const file_hint_t file_hint_pgdump= {
   .register_header_check=&register_header_check_pgdump
 };
 
+/*@
+  @ requires buffer_size >= sizeof(struct pgdmp_hdr);
+  @ requires \valid_read(buffer+(0..buffer_size-1));
+  @ requires valid_file_recovery(file_recovery);
+  @ requires \valid(file_recovery_new);
+  @ requires file_recovery_new->blocksize > 0;
+  @ requires separation: \separated(&file_hint_pgdump, buffer+(..), file_recovery, file_recovery_new);
+  @ ensures \result == 0 || \result == 1;
+  @ ensures  \result!=0 ==> valid_file_recovery(file_recovery_new);
+  @ assigns  *file_recovery_new;
+  @*/
 static int header_check_pgdump(const unsigned char *buffer, const unsigned int buffer_size, const unsigned int safe_header_only, const file_recovery_t *file_recovery, file_recovery_t *file_recovery_new)
 {
   const struct pgdmp_hdr *hdr=(const struct pgdmp_hdr *)buffer;
