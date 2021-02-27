db23e68526165ebe1fbbee6a159af01ab70d4397
Christophe Grenier <grenier@cgsecurity.org>
2021-02-27 10:55:53 +0100

    src/file_pfx.c: add Frama-C annotations

--- src/file_pfx.c
+++ src/file_pfx.c
@@ -31,6 +31,7 @@
 #include "types.h"
 #include "filegen.h"
 
+/*@ requires \valid(file_stat); */
 static void register_header_check_pfx(file_stat_t *file_stat);
 
 const file_hint_t file_hint_pfx= {
@@ -64,6 +65,17 @@ const file_hint_t file_hint_pfx= {
  *    Here, contentType=data
  */
 
+/*@
+  @ requires buffer_size >= 9;
+  @ requires \valid_read(buffer+(0..buffer_size-1));
+  @ requires valid_file_recovery(file_recovery);
+  @ requires \valid(file_recovery_new);
+  @ requires file_recovery_new->blocksize > 0;
+  @ requires separation: \separated(&file_hint_pfx, buffer+(..), file_recovery, file_recovery_new);
+  @ ensures \result == 0 || \result == 1;
+  @ ensures  \result!=0 ==> valid_file_recovery(file_recovery_new);
+  @ assigns  *file_recovery_new;
+  @*/
 static int header_check_pfx(const unsigned char *buffer, const unsigned int buffer_size, const unsigned int safe_header_only, const file_recovery_t *file_recovery, file_recovery_t *file_recovery_new)
 {
   if(buffer[0]==0x30 && buffer[1]==0x82 &&
