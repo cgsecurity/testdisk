0d50de3d04cc8cabc248e93205eb7b97517520cb
Christophe Grenier <grenier@cgsecurity.org>
2021-03-05 13:31:55 +0100

    src/file_sgcta.c: add Frama-C annotations

--- src/file_sgcta.c
+++ src/file_sgcta.c
@@ -31,6 +31,7 @@
 #include "types.h"
 #include "filegen.h"
 
+/*@ requires \valid(file_stat); */
 static void register_header_check_sgcta(file_stat_t *file_stat);
 
 const file_hint_t file_hint_sgcta= {
@@ -42,6 +43,16 @@ const file_hint_t file_hint_sgcta= {
   .register_header_check=&register_header_check_sgcta
 };
 
+/*@
+  @ requires buffer_size > 0;
+  @ requires \valid_read(buffer+(0..buffer_size-1));
+  @ requires valid_file_recovery(file_recovery);
+  @ requires \valid(file_recovery_new);
+  @ requires file_recovery_new->blocksize > 0;
+  @ requires separation: \separated(&file_hint_sgcta, buffer+(..), file_recovery, file_recovery_new);
+  @ ensures  \result!=0 ==> valid_file_recovery(file_recovery_new);
+  @ assigns  *file_recovery_new;
+  @*/
 static int header_check_sgcta(const unsigned char *buffer, const unsigned int buffer_size, const unsigned int safe_header_only, const file_recovery_t *file_recovery, file_recovery_t *file_recovery_new)
 {
   reset_file_recovery(file_recovery_new);
