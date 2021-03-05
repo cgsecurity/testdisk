ea89721d3359ebd620aaadf274b1dd9166da6390
Christophe Grenier <grenier@cgsecurity.org>
2021-03-05 13:37:46 +0100

    src/file_sit.c: add Frama-C annotations

--- src/file_sit.c
+++ src/file_sit.c
@@ -32,8 +32,8 @@
 #include "filegen.h"
 
 
+/*@ requires \valid(file_stat); */
 static void register_header_check_sit(file_stat_t *file_stat);
-static int header_check_sit(const unsigned char *buffer, const unsigned int buffer_size, const unsigned int safe_header_only, const file_recovery_t *file_recovery, file_recovery_t *file_recovery_new);
 
 const file_hint_t file_hint_sit= {
   .extension="sit",
@@ -44,7 +44,16 @@ const file_hint_t file_hint_sit= {
   .register_header_check=&register_header_check_sit
 };
 
-
+/*@
+  @ requires buffer_size > 0;
+  @ requires \valid_read(buffer+(0..buffer_size-1));
+  @ requires valid_file_recovery(file_recovery);
+  @ requires \valid(file_recovery_new);
+  @ requires file_recovery_new->blocksize > 0;
+  @ requires separation: \separated(&file_hint_sit, buffer+(..), file_recovery, file_recovery_new);
+  @ ensures  \result!=0 ==> valid_file_recovery(file_recovery_new);
+  @ assigns  *file_recovery_new;
+  @*/
 static int header_check_sit(const unsigned char *buffer, const unsigned int buffer_size, const unsigned int safe_header_only, const file_recovery_t *file_recovery, file_recovery_t *file_recovery_new)
 {
   reset_file_recovery(file_recovery_new);
