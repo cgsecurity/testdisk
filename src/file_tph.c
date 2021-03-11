d88b5571081688cec6e5b553e914e3c5bc3314dd
Christophe Grenier <grenier@cgsecurity.org>
2021-03-11 14:09:42 +0100

    src/file_tph.c: add Frama-C annotations

--- src/file_tph.c
+++ src/file_tph.c
@@ -32,44 +32,64 @@
 #include "types.h"
 #include "filegen.h"
 
-
+/*@ requires \valid(file_stat); */
 static void register_header_check_tph(file_stat_t *file_stat);
-static int header_check_tph(const unsigned char *buffer, const unsigned int buffer_size, const unsigned int safe_header_only, const file_recovery_t *file_recovery, file_recovery_t *file_recovery_new);
-static void file_check_tph(file_recovery_t *file_recovery);
 
-const file_hint_t file_hint_tph= {
-  .extension="tph",
-  .description="Pro/ENGINEER ToolPath",
-  .max_filesize=PHOTOREC_MAX_FILE_SIZE,
-  .recover=1,
-  .enable_by_default=1,
-  .register_header_check=&register_header_check_tph
+const file_hint_t file_hint_tph = {
+  .extension = "tph",
+  .description = "Pro/ENGINEER ToolPath",
+  .max_filesize = PHOTOREC_MAX_FILE_SIZE,
+  .recover = 1,
+  .enable_by_default = 1,
+  .register_header_check = &register_header_check_tph
 };
 
+/*@
+  @ requires \valid(file_recovery);
+  @ requires valid_file_recovery(file_recovery);
+  @ requires \separated(file_recovery, file_recovery->handle, file_recovery->extension, &errno, &Frama_C_entropy_source);
+  @ requires file_recovery->file_check == &file_check_tph;
+  @ ensures \valid(file_recovery->handle);
+  @ assigns *file_recovery->handle, errno, file_recovery->file_size;
+  @ assigns Frama_C_entropy_source;
+  @*/
 static void file_check_tph(file_recovery_t *file_recovery)
 {
-  const unsigned char tph_footer[11]= {
+  const unsigned char tph_footer[11] = {
     '#', 'E', 'N', 'D', '_', 'O', 'F', '_',
-    'U', 'G', 'C'};
+    'U', 'G', 'C'
+  };
   file_search_footer(file_recovery, tph_footer, sizeof(tph_footer), 1);
 }
 
+/*@
+  @ requires buffer_size >= 24;
+  @ requires \valid_read(buffer+(0..buffer_size-1));
+  @ requires valid_file_recovery(file_recovery);
+  @ requires \valid(file_recovery_new);
+  @ requires file_recovery_new->blocksize > 0;
+  @ requires separation: \separated(&file_hint_tph, buffer+(..), file_recovery, file_recovery_new);
+  @ ensures  \result == 0 || \result == 1;
+  @ ensures  \result!=0 ==> valid_file_recovery(file_recovery_new);
+  @ assigns  *file_recovery_new;
+  @*/
 static int header_check_tph(const unsigned char *buffer, const unsigned int buffer_size, const unsigned int safe_header_only, const file_recovery_t *file_recovery, file_recovery_t *file_recovery_new)
 {
   if(!isprint(buffer[20]) || !isprint(buffer[21]) || !isprint(buffer[22]) || !isprint(buffer[23]))
     return 0;
   reset_file_recovery(file_recovery_new);
-  file_recovery_new->file_check=&file_check_tph;
-  file_recovery_new->extension=file_hint_tph.extension;
+  file_recovery_new->file_check = &file_check_tph;
+  file_recovery_new->extension = file_hint_tph.extension;
   return 1;
 }
 
 static void register_header_check_tph(file_stat_t *file_stat)
 {
-  static const unsigned char tph_header[20]= {
+  static const unsigned char tph_header[20] = {
     '#', 'U', 'G', 'C', ':', '2', ' ', 'M',
     'F', 'G', '_', 'T', 'O', 'O', 'L', '_',
-    'P', 'A', 'T', 'H'};
-  register_header_check(0, tph_header,sizeof(tph_header), &header_check_tph, file_stat);
+    'P', 'A', 'T', 'H'
+  };
+  register_header_check(0, tph_header, sizeof(tph_header), &header_check_tph, file_stat);
 }
 #endif
