57cf2e0f19e8390b5c92ba9c50e1663c240b413d
Christophe Grenier <grenier@cgsecurity.org>
2021-03-12 17:33:50 +0100

    src/file_tpl.c: add Frama-C annotations

--- src/file_tpl.c
+++ src/file_tpl.c
@@ -31,28 +31,40 @@
 #include "types.h"
 #include "filegen.h"
 
+/*@ requires \valid(file_stat); */
 static void register_header_check_tpl(file_stat_t *file_stat);
 
-const file_hint_t file_hint_tpl= {
-  .extension="tpl",
-  .description="Adobe Tool Preset",
-  .max_filesize=PHOTOREC_MAX_FILE_SIZE,
-  .recover=1,
-  .enable_by_default=1,
-  .register_header_check=&register_header_check_tpl
+const file_hint_t file_hint_tpl = {
+  .extension = "tpl",
+  .description = "Adobe Tool Preset",
+  .max_filesize = PHOTOREC_MAX_FILE_SIZE,
+  .recover = 1,
+  .enable_by_default = 1,
+  .register_header_check = &register_header_check_tpl
 };
 
+/*@
+  @ requires buffer_size > 0;
+  @ requires \valid_read(buffer+(0..buffer_size-1));
+  @ requires valid_file_recovery(file_recovery);
+  @ requires \valid(file_recovery_new);
+  @ requires file_recovery_new->blocksize > 0;
+  @ requires separation: \separated(&file_hint_tpl, buffer+(..), file_recovery, file_recovery_new);
+  @ ensures  \result == 0 || \result == 1;
+  @ ensures  \result!=0 ==> valid_file_recovery(file_recovery_new);
+  @ assigns  *file_recovery_new;
+  @*/
 static int header_check_tpl(const unsigned char *buffer, const unsigned int buffer_size, const unsigned int safe_header_only, const file_recovery_t *file_recovery, file_recovery_t *file_recovery_new)
 {
   reset_file_recovery(file_recovery_new);
-  file_recovery_new->extension=file_hint_tpl.extension;
+  file_recovery_new->extension = file_hint_tpl.extension;
   return 1;
 }
 
 static void register_header_check_tpl(file_stat_t *file_stat)
 {
-  static const unsigned char tpl_header[7]=  {
-    '8' , 'B' , 'T' , 'P' , 0x00, 0x00, 0x00
+  static const unsigned char tpl_header[7] = {
+    '8', 'B', 'T', 'P', 0x00, 0x00, 0x00
   };
   register_header_check(0, tpl_header, sizeof(tpl_header), &header_check_tpl, file_stat);
 }
