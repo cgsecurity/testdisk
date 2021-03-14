a090d30f5badfe2daa815c6dc79493de52a315e3
Christophe Grenier <grenier@cgsecurity.org>
2021-03-14 14:05:19 +0100

    src/file_wnk.c: add Frama-C annotations

--- src/file_wnk.c
+++ src/file_wnk.c
@@ -31,27 +31,38 @@
 #include "types.h"
 #include "filegen.h"
 
+/*@ requires \valid(file_stat); */
 static void register_header_check_wnk(file_stat_t *file_stat);
 
-const file_hint_t file_hint_wnk= {
-  .extension="wnk",
-  .description="Wink",
-  .max_filesize=PHOTOREC_MAX_FILE_SIZE,
-  .recover=1,
-  .enable_by_default=1,
-  .register_header_check=&register_header_check_wnk
+const file_hint_t file_hint_wnk = {
+  .extension = "wnk",
+  .description = "Wink",
+  .max_filesize = PHOTOREC_MAX_FILE_SIZE,
+  .recover = 1,
+  .enable_by_default = 1,
+  .register_header_check = &register_header_check_wnk
 };
 
+/*@
+  @ requires buffer_size > 0;
+  @ requires \valid_read(buffer+(0..buffer_size-1));
+  @ requires valid_file_recovery(file_recovery);
+  @ requires \valid(file_recovery_new);
+  @ requires file_recovery_new->blocksize > 0;
+  @ requires separation: \separated(&file_hint_wnk, buffer+(..), file_recovery, file_recovery_new);
+  @ ensures  \result!=0 ==> valid_file_recovery(file_recovery_new);
+  @ assigns  *file_recovery_new;
+  @*/
 static int header_check_wnk(const unsigned char *buffer, const unsigned int buffer_size, const unsigned int safe_header_only, const file_recovery_t *file_recovery, file_recovery_t *file_recovery_new)
 {
   reset_file_recovery(file_recovery_new);
-  file_recovery_new->extension=file_hint_wnk.extension;
+  file_recovery_new->extension = file_hint_wnk.extension;
   return 1;
 }
 
 static void register_header_check_wnk(file_stat_t *file_stat)
 {
-  static const unsigned char wnk_header[6]= {'R','P','K','W','I',0x02};
-  register_header_check(0, wnk_header,sizeof(wnk_header), &header_check_wnk, file_stat);
+  static const unsigned char wnk_header[6] = { 'R', 'P', 'K', 'W', 'I', 0x02 };
+  register_header_check(0, wnk_header, sizeof(wnk_header), &header_check_wnk, file_stat);
 }
 #endif
