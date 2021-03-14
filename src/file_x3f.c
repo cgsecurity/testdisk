3532fb711d17fc270785654ac9edbe96d73ca2b8
Christophe Grenier <grenier@cgsecurity.org>
2021-03-14 15:04:03 +0100

    src/file_x3f.c: add Frama-C annotations

--- src/file_x3f.c
+++ src/file_x3f.c
@@ -32,54 +32,65 @@
 #include "filegen.h"
 #include "common.h"
 
+#if !defined(SINGLE_FORMAT)
 extern const file_hint_t file_hint_x3i;
-static void register_header_check_x3f(file_stat_t *file_stat);
-static int header_check_x3f(const unsigned char *buffer, const unsigned int buffer_size, const unsigned int safe_header_only, const file_recovery_t *file_recovery, file_recovery_t *file_recovery_new);
+#endif
 
-const file_hint_t file_hint_x3f= {
-  .extension="x3f",
-  .description="Sigma/Foveon X3 raw picture",
-  .max_filesize=PHOTOREC_MAX_FILE_SIZE,
-  .recover=1,
-  .enable_by_default=1,
-  .register_header_check=&register_header_check_x3f
+/*@ requires \valid(file_stat); */
+static void register_header_check_x3f(file_stat_t *file_stat);
+
+const file_hint_t file_hint_x3f = {
+  .extension = "x3f",
+  .description = "Sigma/Foveon X3 raw picture",
+  .max_filesize = PHOTOREC_MAX_FILE_SIZE,
+  .recover = 1,
+  .enable_by_default = 1,
+  .register_header_check = &register_header_check_x3f
 };
 
 struct x3f_header
 {
-  uint32_t	id;
-  uint32_t	version;
+  uint32_t id;
+  uint32_t version;
   unsigned char uuid[16];
-  uint32_t	mark;
-  uint32_t	columns;
-  uint32_t	rows;
-  uint32_t	rotation;
+  uint32_t mark;
+  uint32_t columns;
+  uint32_t rows;
+  uint32_t rotation;
   /* version 2.1 and later have additional fields */
-} __attribute__ ((gcc_struct, __packed__));
+} __attribute__((gcc_struct, __packed__));
 
+/*@
+  @ requires buffer_size >= sizeof(struct x3f_header);
+  @ requires \valid_read(buffer+(0..buffer_size-1));
+  @ requires valid_file_recovery(file_recovery);
+  @ requires \valid(file_recovery_new);
+  @ requires file_recovery_new->blocksize > 0;
+  @ requires separation: \separated(&file_hint_x3f, buffer+(..), file_recovery, file_recovery_new);
+  @ ensures  \result!=0 ==> valid_file_recovery(file_recovery_new);
+  @ assigns  *file_recovery_new;
+  @*/
 static int header_check_x3f(const unsigned char *buffer, const unsigned int buffer_size, const unsigned int safe_header_only, const file_recovery_t *file_recovery, file_recovery_t *file_recovery_new)
 {
-  const struct x3f_header *h=(const struct x3f_header *)buffer;
-  const unsigned int rotation=le32(h->rotation);
-  if(le32(h->rows)==0 || le32(h->columns)==0)
+  const struct x3f_header *h = (const struct x3f_header *)buffer;
+  const unsigned int rotation = le32(h->rotation);
+  if(le32(h->rows) == 0 || le32(h->columns) == 0)
     return 0;
-  if(rotation!=0 && rotation!=90 && rotation!=180 && rotation!=270)
+  if(rotation != 0 && rotation != 90 && rotation != 180 && rotation != 270)
     return 0;
-  if(file_recovery->file_stat!=NULL &&
-      file_recovery->file_stat->file_hint==&file_hint_x3i &&
-      safe_header_only==0)
-  {
+#if !defined(SINGLE_FORMAT)
+  if(file_recovery->file_stat != NULL && file_recovery->file_stat->file_hint == &file_hint_x3i && safe_header_only == 0)
     return 0;
-  }
+#endif
   reset_file_recovery(file_recovery_new);
-  file_recovery_new->extension=file_hint_x3f.extension;
-  file_recovery_new->min_filesize=1024;
+  file_recovery_new->extension = file_hint_x3f.extension;
+  file_recovery_new->min_filesize = 1024;
   return 1;
 }
 
 static void register_header_check_x3f(file_stat_t *file_stat)
 {
-  static const unsigned char x3f_header[4]= {'F','O','V','b'};
-  register_header_check(0, x3f_header,sizeof(x3f_header), &header_check_x3f, file_stat);
+  static const unsigned char x3f_header[4] = { 'F', 'O', 'V', 'b' };
+  register_header_check(0, x3f_header, sizeof(x3f_header), &header_check_x3f, file_stat);
 }
 #endif
