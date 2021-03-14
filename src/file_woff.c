ea4a84e4b463f33de039f49dd4012773c2df5ace
Christophe Grenier <grenier@cgsecurity.org>
2021-03-14 14:13:51 +0100

    src/file_woff.c: add Frama-C annotations

--- src/file_woff.c
+++ src/file_woff.c
@@ -32,54 +32,68 @@
 #include "filegen.h"
 #include "common.h"
 
+/*@ requires \valid(file_stat); */
 static void register_header_check_woff(file_stat_t *file_stat);
 
-const file_hint_t file_hint_woff= {
-  .extension="woff",
-  .description="Web Open Font Format",
-  .max_filesize=PHOTOREC_MAX_FILE_SIZE,
-  .recover=1,
-  .enable_by_default=1,
-  .register_header_check=&register_header_check_woff
+const file_hint_t file_hint_woff = {
+  .extension = "woff",
+  .description = "Web Open Font Format",
+  .max_filesize = PHOTOREC_MAX_FILE_SIZE,
+  .recover = 1,
+  .enable_by_default = 1,
+  .register_header_check = &register_header_check_woff
 };
 
 struct WOFFHeader
 {
-  uint32_t	signature;
-  uint32_t	flavor;
-  uint32_t	length;
-  uint16_t	numTables;
-  uint16_t	reserved;
-  uint32_t	totalSfntSize;
-  uint16_t	majorVersion;
-  uint16_t	minorVersion;
-  uint32_t	metaOffset;
-  uint32_t	metaLength;
-  uint32_t	metaOrigLength;
-  uint32_t	privOffset;
-  uint32_t	privLength;
-} __attribute__ ((gcc_struct, __packed__));
+  uint32_t signature;
+  uint32_t flavor;
+  uint32_t length;
+  uint16_t numTables;
+  uint16_t reserved;
+  uint32_t totalSfntSize;
+  uint16_t majorVersion;
+  uint16_t minorVersion;
+  uint32_t metaOffset;
+  uint32_t metaLength;
+  uint32_t metaOrigLength;
+  uint32_t privOffset;
+  uint32_t privLength;
+} __attribute__((gcc_struct, __packed__));
 
+/*@
+  @ requires buffer_size >= sizeof(struct WOFFHeader);
+  @ requires \valid_read(buffer+(0..buffer_size-1));
+  @ requires valid_file_recovery(file_recovery);
+  @ requires \valid(file_recovery_new);
+  @ requires file_recovery_new->blocksize > 0;
+  @ requires separation: \separated(&file_hint_woff, buffer+(..), file_recovery, file_recovery_new);
+  @ ensures  \result!=0 ==> valid_file_recovery(file_recovery_new);
+  @ assigns  *file_recovery_new;
+  @*/
 static int header_check_woff(const unsigned char *buffer, const unsigned int buffer_size, const unsigned int safe_header_only, const file_recovery_t *file_recovery, file_recovery_t *file_recovery_new)
 {
-  const struct WOFFHeader *woff=(const struct WOFFHeader *)buffer;
-  const uint64_t length=(uint64_t)be32(woff->length);
-  if((uint32_t)be32(woff->length) < sizeof(struct WOFFHeader))
+  const struct WOFFHeader *woff = (const struct WOFFHeader *)buffer;
+  const uint32_t length = be32(woff->length);
+  const uint32_t metaLength = be32(woff->metaLength);
+  const uint32_t metaOffset = be32(woff->metaOffset);
+  const uint32_t privLength = be32(woff->privLength);
+  const uint32_t privOffset = be32(woff->privOffset);
+  if(length < sizeof(struct WOFFHeader))
     return 0;
-  if((uint32_t)be32(woff->metaOffset) > 0 && (uint32_t)be32(woff->metaOffset) < sizeof(struct WOFFHeader))
+  if(metaOffset > 0 && metaOffset < sizeof(struct WOFFHeader))
     return 0;
-  if((uint32_t)be32(woff->privOffset) > 0 && (uint32_t)be32(woff->privOffset) < sizeof(struct WOFFHeader))
+  if(privOffset > 0 && privOffset < sizeof(struct WOFFHeader))
     return 0;
-  if((uint64_t)be32(woff->metaOffset) + (uint64_t)be32(woff->metaLength) > length ||
-      (uint64_t)be32(woff->privOffset) + (uint64_t)be32(woff->privLength) > length)
+  if((uint64_t)metaOffset + metaLength > length || (uint64_t)privOffset + privLength > length)
     return 0;
-  if(woff->reserved!=0)
+  if(woff->reserved != 0)
     return 0;
   reset_file_recovery(file_recovery_new);
-  file_recovery_new->extension=file_hint_woff.extension;
-  file_recovery_new->calculated_file_size=length;
-  file_recovery_new->data_check=&data_check_size;
-  file_recovery_new->file_check=&file_check_size;
+  file_recovery_new->extension = file_hint_woff.extension;
+  file_recovery_new->calculated_file_size = length;
+  file_recovery_new->data_check = &data_check_size;
+  file_recovery_new->file_check = &file_check_size;
   return 1;
 }
 
