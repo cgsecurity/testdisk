7a3674eb7d3bf13d90e16bf703026f1e50569dbe
Christophe Grenier <grenier@cgsecurity.org>
2021-03-14 19:18:33 +0100

    src/file_xpt.c: add Frama-C annotations

--- src/file_xpt.c
+++ src/file_xpt.c
@@ -32,48 +32,60 @@
 #include "filegen.h"
 #include "common.h"
 
+/*@ requires \valid(file_stat); */
 static void register_header_check_xpt(file_stat_t *file_stat);
 
-const file_hint_t file_hint_xpt= {
-  .extension="xpt",
-  .description="Mozilla XPCOM Type Library",
-  .max_filesize=PHOTOREC_MAX_FILE_SIZE,
-  .recover=1,
-  .enable_by_default=1,
-  .register_header_check=&register_header_check_xpt
+const file_hint_t file_hint_xpt = {
+  .extension = "xpt",
+  .description = "Mozilla XPCOM Type Library",
+  .max_filesize = PHOTOREC_MAX_FILE_SIZE,
+  .recover = 1,
+  .enable_by_default = 1,
+  .register_header_check = &register_header_check_xpt
 };
-
 
 /* Read http://www.mozilla.org/scriptable/typelib_file.html
  * for more information about this file format */
-struct TypeLibHeader {
-  char                     magic[16];
-  uint8_t                  major_version;
-  uint8_t                  minor_version;
-  uint16_t                 num_interfaces;
-  uint32_t                 file_length;
-} __attribute__ ((gcc_struct, __packed__));
+struct TypeLibHeader
+{
+  char magic[16];
+  uint8_t major_version;
+  uint8_t minor_version;
+  uint16_t num_interfaces;
+  uint32_t file_length;
+} __attribute__((gcc_struct, __packed__));
 
+/*@
+  @ requires buffer_size >= sizeof(struct TypeLibHeader);
+  @ requires \valid_read(buffer+(0..buffer_size-1));
+  @ requires valid_file_recovery(file_recovery);
+  @ requires \valid(file_recovery_new);
+  @ requires file_recovery_new->blocksize > 0;
+  @ requires separation: \separated(&file_hint_xpt, buffer+(..), file_recovery, file_recovery_new);
+  @ ensures  \result!=0 ==> valid_file_recovery(file_recovery_new);
+  @ assigns  *file_recovery_new;
+  @*/
 static int header_check_xpt(const unsigned char *buffer, const unsigned int buffer_size, const unsigned int safe_header_only, const file_recovery_t *file_recovery, file_recovery_t *file_recovery_new)
 {
-  const struct TypeLibHeader *xpt=(const struct TypeLibHeader*)buffer;
-  if(be32(xpt->file_length) < 65)
+  const struct TypeLibHeader *xpt = (const struct TypeLibHeader *)buffer;
+  const unsigned int file_length = be32(xpt->file_length);
+  if(file_length < 65)
     return 0;
   reset_file_recovery(file_recovery_new);
-  file_recovery_new->extension=file_hint_xpt.extension;
-  file_recovery_new->min_filesize=65;
-  file_recovery_new->calculated_file_size=be32(xpt->file_length);
-  file_recovery_new->data_check=&data_check_size;
-  file_recovery_new->file_check=&file_check_size;
+  file_recovery_new->extension = file_hint_xpt.extension;
+  file_recovery_new->min_filesize = 65;
+  file_recovery_new->calculated_file_size = file_length;
+  file_recovery_new->data_check = &data_check_size;
+  file_recovery_new->file_check = &file_check_size;
   return 1;
 }
 
 static void register_header_check_xpt(file_stat_t *file_stat)
 {
-  static const unsigned char xpt_header[16]={
+  static const unsigned char xpt_header[16] = {
     'X', 'P', 'C', 'O', 'M', '\n', 'T', 'y',
     'p', 'e', 'L', 'i', 'b', '\r', '\n', 0x1a
   };
-  register_header_check(0, xpt_header,sizeof(xpt_header), &header_check_xpt, file_stat);
+  register_header_check(0, xpt_header, sizeof(xpt_header), &header_check_xpt, file_stat);
 }
 #endif
