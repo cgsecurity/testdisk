6360b01e5cf5740344d47f6f3428338fa733cc3b
Christophe Grenier <grenier@cgsecurity.org>
2021-03-15 17:34:22 +0100

    src/file_xz.c: add Frama-C annotations

--- src/file_xz.c
+++ src/file_xz.c
@@ -31,38 +31,58 @@
 #include "types.h"
 #include "filegen.h"
 
+/*@ requires \valid(file_stat); */
 static void register_header_check_xz(file_stat_t *file_stat);
 
-const file_hint_t file_hint_xz= {
-  .extension="xz",
-  .description="xz Archive",
-  .max_filesize=PHOTOREC_MAX_FILE_SIZE,
-  .recover=1,
-  .enable_by_default=1,
-  .register_header_check=&register_header_check_xz
+const file_hint_t file_hint_xz = {
+  .extension = "xz",
+  .description = "xz Archive",
+  .max_filesize = PHOTOREC_MAX_FILE_SIZE,
+  .recover = 1,
+  .enable_by_default = 1,
+  .register_header_check = &register_header_check_xz
 };
 
+/*@
+  @ requires \valid(file_recovery);
+  @ requires valid_file_recovery(file_recovery);
+  @ requires \separated(file_recovery, file_recovery->handle, file_recovery->extension, &errno, &Frama_C_entropy_source);
+  @ requires file_recovery->file_check == &file_check_xz;
+  @ ensures \valid(file_recovery->handle);
+  @ assigns *file_recovery->handle, errno, file_recovery->file_size;
+  @ assigns Frama_C_entropy_source;
+  @*/
 static void file_check_xz(file_recovery_t *file_recovery)
 {
-  const unsigned char xz_footer[2]= { 'Y' , 'Z' };
+  const unsigned char xz_footer[2] = { 'Y', 'Z' };
   file_search_footer(file_recovery, xz_footer, sizeof(xz_footer), 0);
 }
 
+/*@
+  @ requires buffer_size > 0;
+  @ requires \valid_read(buffer+(0..buffer_size-1));
+  @ requires valid_file_recovery(file_recovery);
+  @ requires \valid(file_recovery_new);
+  @ requires file_recovery_new->blocksize > 0;
+  @ requires separation: \separated(&file_hint_xz, buffer+(..), file_recovery, file_recovery_new);
+  @ ensures  \result!=0 ==> valid_file_recovery(file_recovery_new);
+  @ assigns  *file_recovery_new;
+  @*/
 static int header_check_xz(const unsigned char *buffer, const unsigned int buffer_size, const unsigned int safe_header_only, const file_recovery_t *file_recovery, file_recovery_t *file_recovery_new)
 {
   reset_file_recovery(file_recovery_new);
-  file_recovery_new->extension=file_hint_xz.extension;
-  file_recovery_new->file_check=&file_check_xz;
-  file_recovery_new->min_filesize=9;
+  file_recovery_new->extension = file_hint_xz.extension;
+  file_recovery_new->file_check = &file_check_xz;
+  file_recovery_new->min_filesize = 9;
   return 1;
 }
 
 static void register_header_check_xz(file_stat_t *file_stat)
 {
-  /* http://tukaani.org/xz/xz-file-format.txt */
+  /* https://tukaani.org/xz/xz-file-format.txt */
   /* HEADER_MAGIC + Stream Flag[0] */
-  static const unsigned char xz_header[7]=  {
-    0xfd, '7' , 'z' , 'X' , 'Z' , 0x00, 0x00
+  static const unsigned char xz_header[7] = {
+    0xfd, '7', 'z', 'X', 'Z', 0x00, 0x00
   };
   register_header_check(0, xz_header, sizeof(xz_header), &header_check_xz, file_stat);
 }
