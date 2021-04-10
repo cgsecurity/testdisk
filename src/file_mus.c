db677e3b322bef7b77978857ef6a31e639bb508e
Christophe Grenier <grenier@cgsecurity.org>
2021-04-10 10:48:37 +0200

    src/file_mus.c: add Frama-C annotations

--- src/file_mus.c
+++ src/file_mus.c
@@ -31,35 +31,57 @@
 #include "types.h"
 #include "filegen.h"
 
+/*@ requires \valid(file_stat); */
 static void register_header_check_mus(file_stat_t *file_stat);
 
-const file_hint_t file_hint_mus= {
-  .extension="mus",
-  .description="Finale Music Score",
-  .max_filesize=PHOTOREC_MAX_FILE_SIZE,
-  .recover=1,
-  .enable_by_default=1,
-  .register_header_check=&register_header_check_mus
+const file_hint_t file_hint_mus = {
+  .extension = "mus",
+  .description = "Finale Music Score",
+  .max_filesize = PHOTOREC_MAX_FILE_SIZE,
+  .recover = 1,
+  .enable_by_default = 1,
+  .register_header_check = &register_header_check_mus
 };
 
+/*@
+  @ requires valid_file_recovery(file_recovery);
+  @ requires \valid(file_recovery->handle);
+  @ requires \separated(file_recovery, file_recovery->handle, file_recovery->extension, &errno, &Frama_C_entropy_source);
+  @ requires \initialized(&file_recovery->time);
+  @ requires file_recovery->file_check == &file_check_mus;
+  @ ensures \valid(file_recovery->handle);
+  @ assigns *file_recovery->handle, errno, file_recovery->file_size;
+  @ assigns Frama_C_entropy_source;
+  @*/
 static void file_check_mus(file_recovery_t *file_recovery)
 {
-  const unsigned char mus_footer[5]= {'-','^','e','n','d'};
+  const unsigned char mus_footer[5] = { '-', '^', 'e', 'n', 'd' };
   file_search_footer(file_recovery, mus_footer, sizeof(mus_footer), 0);
 }
 
+/*@
+  @ requires buffer_size > 0;
+  @ requires \valid_read(buffer+(0..buffer_size-1));
+  @ requires valid_file_recovery(file_recovery);
+  @ requires \valid(file_recovery_new);
+  @ requires file_recovery_new->blocksize > 0;
+  @ requires separation: \separated(&file_hint_mus, buffer+(..), file_recovery, file_recovery_new);
+  @ ensures \result == 0 || \result == 1;
+  @ ensures  \result!=0 ==> valid_file_recovery(file_recovery_new);
+  @ assigns  *file_recovery_new;
+  @*/
 static int header_check_mus(const unsigned char *buffer, const unsigned int buffer_size, const unsigned int safe_header_only, const file_recovery_t *file_recovery, file_recovery_t *file_recovery_new)
 {
   reset_file_recovery(file_recovery_new);
-  file_recovery_new->min_filesize=18;
-  file_recovery_new->file_check=&file_check_mus;
-  file_recovery_new->extension=file_hint_mus.extension;
+  file_recovery_new->min_filesize = 18;
+  file_recovery_new->file_check = &file_check_mus;
+  file_recovery_new->extension = file_hint_mus.extension;
   return 1;
 }
 
 static void register_header_check_mus(file_stat_t *file_stat)
 {
-  static const unsigned char mus_header[18]  = { 'E','N','I','G','M','A',' ','B','I','N','A','R','Y',' ','F','I','L','E' };
-  register_header_check(0, mus_header,sizeof(mus_header), &header_check_mus, file_stat);
+  static const unsigned char mus_header[18] = { 'E', 'N', 'I', 'G', 'M', 'A', ' ', 'B', 'I', 'N', 'A', 'R', 'Y', ' ', 'F', 'I', 'L', 'E' };
+  register_header_check(0, mus_header, sizeof(mus_header), &header_check_mus, file_stat);
 }
 #endif
