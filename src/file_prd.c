6e7f734674b01894a51ccdd789fec8023a3dc859
Christophe Grenier <grenier@cgsecurity.org>
2021-04-10 10:56:22 +0200

    src/file_prd.c: add Frama-C annotations

--- src/file_prd.c
+++ src/file_prd.c
@@ -31,30 +31,40 @@
 #include "types.h"
 #include "filegen.h"
 
+/*@ requires \valid(file_stat); */
 static void register_header_check_prd(file_stat_t *file_stat);
 
-const file_hint_t file_hint_prd= {
-  .extension="prd",
-  .description="Paessler PRTG",
-  .max_filesize=PHOTOREC_MAX_FILE_SIZE,
-  .recover=1,
-  .enable_by_default=1,
-  .register_header_check=&register_header_check_prd
+const file_hint_t file_hint_prd = {
+  .extension = "prd",
+  .description = "Paessler PRTG",
+  .max_filesize = PHOTOREC_MAX_FILE_SIZE,
+  .recover = 1,
+  .enable_by_default = 1,
+  .register_header_check = &register_header_check_prd
 };
 
+/*@
+  @ requires buffer_size >= 0x18;
+  @ requires \valid_read(buffer+(0..buffer_size-1));
+  @ requires valid_file_recovery(file_recovery);
+  @ requires \valid(file_recovery_new);
+  @ requires file_recovery_new->blocksize > 0;
+  @ requires separation: \separated(&file_hint_prd, buffer+(..), file_recovery, file_recovery_new);
+  @ ensures  \result!=0 ==> valid_file_recovery(file_recovery_new);
+  @ assigns  *file_recovery_new;
+  @*/
 static int header_check_prd(const unsigned char *buffer, const unsigned int buffer_size, const unsigned int safe_header_only, const file_recovery_t *file_recovery, file_recovery_t *file_recovery_new)
 {
-  if( buffer[0x0d]!=0xdb || buffer[0x0e]!=0xe4 || buffer[0x0f]!=0x40 ||
-      buffer[0x15]!=0xdb || buffer[0x16]!=0xe4 || buffer[0x17]!=0x40)
+  if(buffer[0x0d] != 0xdb || buffer[0x0e] != 0xe4 || buffer[0x0f] != 0x40 || buffer[0x15] != 0xdb || buffer[0x16] != 0xe4 || buffer[0x17] != 0x40)
     return 0;
   reset_file_recovery(file_recovery_new);
-  file_recovery_new->extension=file_hint_prd.extension;
+  file_recovery_new->extension = file_hint_prd.extension;
   return 1;
 }
 
 static void register_header_check_prd(file_stat_t *file_stat)
 {
-  static const unsigned char prd_header[8]=  {
+  static const unsigned char prd_header[8] = {
     0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00
   };
   register_header_check(0, prd_header, sizeof(prd_header), &header_check_prd, file_stat);
