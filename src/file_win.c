e0a96e40d91176fafd1ea7a6c33e356eba0834e7
Christophe Grenier <grenier@cgsecurity.org>
2021-04-16 16:57:21 +0200

    src/file_win.c: add Frama-C annotations

--- src/file_win.c
+++ src/file_win.c
@@ -48,6 +48,15 @@ const file_hint_t file_hint_win = {
   .register_header_check = &register_header_check_win
 };
 
+/*@
+  @ requires buffer_size > 0;
+  @ requires (buffer_size&1)==0;
+  @ requires \valid_read(buffer+(0..buffer_size-1));
+  @ requires \valid(file_recovery);
+  @ requires file_recovery->data_check==&data_check_win;
+  @ ensures  \result == DC_CONTINUE || \result == DC_STOP;
+  @ assigns  file_recovery->calculated_file_size;
+  @*/
 static data_check_t data_check_win(const unsigned char *buffer, const unsigned int buffer_size, file_recovery_t *file_recovery)
 {
   unsigned int i;
@@ -65,12 +74,23 @@ static data_check_t data_check_win(const unsigned char *buffer, const unsigned i
   return DC_CONTINUE;
 }
 
+/*@
+  @ requires buffer_size > 0;
+  @ requires \valid_read(buffer+(0..buffer_size-1));
+  @ requires valid_file_recovery(file_recovery);
+  @ requires \valid(file_recovery_new);
+  @ requires file_recovery_new->blocksize > 0;
+  @ requires separation: \separated(&file_hint_win, buffer+(..), file_recovery, file_recovery_new);
+  @ ensures  \result!=0 ==> valid_file_recovery(file_recovery_new);
+  @ assigns  *file_recovery_new;
+  @*/
 static int header_check_win(const unsigned char *buffer, const unsigned int buffer_size, const unsigned int safe_header_only, const file_recovery_t *file_recovery, file_recovery_t *file_recovery_new)
 {
   reset_file_recovery(file_recovery_new);
   file_recovery_new->extension = file_hint_win.extension;
   file_recovery_new->data_check = &data_check_win;
   file_recovery_new->file_check = &file_check_size;
+  /*@ assert valid_file_recovery(file_recovery_new); */
   return 1;
 }
 
