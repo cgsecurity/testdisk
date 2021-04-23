b19006725d18068da4327af5e1af1531c8a3746e
Christophe Grenier <grenier@cgsecurity.org>
2021-04-23 08:29:20 +0200

    qphotorec: add greek (el) language

---
+++ src/lang/qphotorec.el.ts
@@ -0,0 +1,257 @@
+<?xml version="1.0" ?><!DOCTYPE TS><TS language="el" version="2.1">
+<context>
+    <name>QPhotorec</name>
+    <message>
+        <location filename="src/qphotorec.cpp" line="125"/>
+        <source>QPhotoRec</source>
+        <translation>QPhotoRec</translation>
+    </message>
+    <message>
+        <location filename="src/qphotorec.cpp" line="156"/>
+        <location filename="src/qphotorec.cpp" line="521"/>
+        <source>Please select a destination to save the recovered files to.</source>
+        <translation>Παρακαλώ επιλέξτε έναν προορισμό για να αποθηκεύσετε τα ανακτημένα αρχεία.</translation>
+    </message>
+    <message>
+        <location filename="src/qphotorec.cpp" line="170"/>
+        <source>Please select a raw file</source>
+        <translation>Παρακαλώ επιλέξτε ένα ακατέργαστο αρχείο</translation>
+    </message>
+    <message>
+        <location filename="src/qphotorec.cpp" line="172"/>
+        <source>Raw Files (*.dd *.raw *.img)</source>
+        <translation>Ακατέργαστα Αρχεία (*.dd *.raw *.img)</translation>
+    </message>
+    <message>
+        <location filename="src/qphotorec.cpp" line="259"/>
+        <source>Sys=</source>
+        <translation>Sys=</translation>
+    </message>
+    <message>
+        <location filename="src/qphotorec.cpp" line="261"/>
+        <source>Unknown</source>
+        <translation>Άγνωστο</translation>
+    </message>
+    <message>
+        <location filename="src/qphotorec.cpp" line="377"/>
+        <source>No harddisk found</source>
+        <translation>Δεν βρέθηκε σκληρός δίσκος</translation>
+    </message>
+    <message>
+        <location filename="src/qphotorec.cpp" line="379"/>
+        <source>No harddisk found
+You need to be administrator to use this program.
+Under Win9x, use the DOS version instead.
+Under Vista or later, select this program, right-click and choose &quot;Run as administrator&quot;.</source>
+        <translation>Δεν βρέθηκε σκληρός δίσκος
+Πρέπει να είστε διαχειριστής ώστε να χρησιμοποιήσετε αυτό το πρόγραμμα.
+Για Win9x, χρησιμοποιήστε την έκδοση DOS.
+Για Vista ή νεότερα, επιλέξτε το πρόγραμμα, πατήστε δεξί-κλικ και την επιλογή &quot;Εκτέλεση ως διαχειριστής&quot;.</translation>
+    </message>
+    <message>
+        <location filename="src/qphotorec.cpp" line="388"/>
+        <source>No harddisk found
+You need to be root to use PhotoRec.</source>
+        <translation>Δεν βρέθηκε σκληρός δίσκος
+Πρέπει να είστε διαχειριστής root για να χρησιμοποιήσετε το PhotoRec.</translation>
+    </message>
+    <message>
+        <location filename="src/qphotorec.cpp" line="393"/>
+        <source>No Disk!</source>
+        <translation>Χωρίς δίσκο! </translation>
+    </message>
+    <message>
+        <location filename="src/qphotorec.cpp" line="455"/>
+        <source>Add a raw disk image...</source>
+        <translation>Προσθήκη μίας εικόνας ακατέργαστου δίσκου...</translation>
+    </message>
+    <message>
+        <location filename="src/qphotorec.cpp" line="461"/>
+        <source>PhotoRec is free software, and comes with ABSOLUTELY NO WARRANTY.</source>
+        <translation>Το PhotoRec είναι ελεύθερο λογισμικό και παρέχεται με ΚΑΜΙΑ ΑΠΟΛΥΤΩΣ ΕΓΓΥΗΣΗ.</translation>
+    </message>
+    <message>
+        <location filename="src/qphotorec.cpp" line="462"/>
+        <source>Please select a media to recover from</source>
+        <translation>Παρακαλώ επιλέξτε ένα μέσο για ανάκτηση</translation>
+    </message>
+    <message>
+        <location filename="src/qphotorec.cpp" line="465"/>
+        <source>Disk capacity must be correctly detected for a successful recovery.
+If a disk listed above has an incorrect size, check HD jumper settings and BIOS
+detection, and install the latest OS patches and disk drivers.</source>
+        <translation>Η χωρητικότητα του δίσκου πρέπει να εντοπιστεί σωστά για μια επιτυχή ανάκτηση.
+Εάν ένας δίσκος που εμφανίζεται παραπάνω έχει λανθασμένο μέγεθος, ελέγξτε την ρύθμιση jumper του δίσκου και
+το πως αναγνωρίζεται στο BIOS, προχωρήστε σε εγκατάσταση των πιο πρόσφατων ενημερώσεων του ΛΣ και προγραμμάτων οδήγησης δίσκων.</translation>
+    </message>
+    <message>
+        <location filename="src/qphotorec.cpp" line="472"/>
+        <source>Flags</source>
+        <translation>Σημαίες</translation>
+    </message>
+    <message>
+        <location filename="src/qphotorec.cpp" line="473"/>
+        <source>Type</source>
+        <translation>Τύπος</translation>
+    </message>
+    <message>
+        <location filename="src/qphotorec.cpp" line="474"/>
+        <source>File System</source>
+        <translation>Σύστημα αρχείων</translation>
+    </message>
+    <message>
+        <location filename="src/qphotorec.cpp" line="475"/>
+        <source>Size</source>
+        <translation>Μέγεθος</translation>
+    </message>
+    <message>
+        <location filename="src/qphotorec.cpp" line="476"/>
+        <source>Label</source>
+        <translation>Ετικέτα</translation>
+    </message>
+    <message>
+        <location filename="src/qphotorec.cpp" line="491"/>
+        <source>File System type</source>
+        <translation>Τύπος συστήματος αρχείων</translation>
+    </message>
+    <message>
+        <location filename="src/qphotorec.cpp" line="492"/>
+        <source>ext2/ext3/ext4 filesystem</source>
+        <translation>σύστημα αρχείων ext2/ext3/ext4</translation>
+    </message>
+    <message>
+        <location filename="src/qphotorec.cpp" line="493"/>
+        <source>FAT/NTFS/HFS+/ReiserFS/...</source>
+        <translation>FAT/NTFS/HFS+/ReiserFS/...</translation>
+    </message>
+    <message>
+        <location filename="src/qphotorec.cpp" line="497"/>
+        <source>Free: Scan for file from unallocated space only</source>
+        <translation>Ελεύθερος: Σάρωση για αρχείο μόνο από μη δεσμευμένο χώρο</translation>
+    </message>
+    <message>
+        <location filename="src/qphotorec.cpp" line="498"/>
+        <source>Whole: Extract files from whole partition</source>
+        <translation>Πλήρης: Εξαγωγή αρχείων από ολόκληρο το διαμέρισμα δίσκου</translation>
+    </message>
+    <message>
+        <location filename="src/qphotorec.cpp" line="525"/>
+        <source>&amp;Browse</source>
+        <translation>&amp;Περιήγηση</translation>
+    </message>
+    <message>
+        <location filename="src/qphotorec.cpp" line="541"/>
+        <source>&amp;Search</source>
+        <translation>&amp;Αναζήτηση</translation>
+    </message>
+    <message>
+        <location filename="src/qphotorec.cpp" line="544"/>
+        <source>&amp;About</source>
+        <translation>&amp;Περί</translation>
+    </message>
+    <message>
+        <location filename="src/qphotorec.cpp" line="545"/>
+        <source>&amp;File Formats</source>
+        <translation>&amp;Μορφές Αρχείου</translation>
+    </message>
+    <message>
+        <location filename="src/qphotorec.cpp" line="645"/>
+        <source>Destination:</source>
+        <translation>Προορισμός:</translation>
+    </message>
+    <message>
+        <location filename="src/qphotorec.cpp" line="650"/>
+        <source>Recovery completed</source>
+        <translation>Η ανάκτηση ολοκληρώθηκε</translation>
+    </message>
+    <message>
+        <location filename="src/qphotorec.cpp" line="655"/>
+        <source>Bruteforce %1 sectors remaining (test %2)</source>
+        <translation>Bruteforce απομένουν %1 τομείς (δοκιμή %2)</translation>
+    </message>
+    <message>
+        <location filename="src/qphotorec.cpp" line="663"/>
+        <source>Pass %1 - Reading sector %2/%3</source>
+        <translation>Πέρασμα %1 - Ανάγνωση τομέα %2/%3</translation>
+    </message>
+    <message>
+        <location filename="src/qphotorec.cpp" line="668"/>
+        <source>%1/10 headers found</source>
+        <translation>βρέθηκαν %1/10 κεφαλίδες</translation>
+    </message>
+    <message>
+        <location filename="src/qphotorec.cpp" line="670"/>
+        <source>%1 files found</source>
+        <translation>βρέθηκαν %1 αρχεία</translation>
+    </message>
+    <message>
+        <location filename="src/qphotorec.cpp" line="753"/>
+        <source>File family</source>
+        <translation>Οικογένεια αρχείου</translation>
+    </message>
+    <message>
+        <location filename="src/qphotorec.cpp" line="543"/>
+        <location filename="src/qphotorec.cpp" line="765"/>
+        <source>&amp;Quit</source>
+        <translation>&amp;Έξοδος</translation>
+    </message>
+    <message>
+        <location filename="src/qphotorec.cpp" line="754"/>
+        <source>Number of files recovered</source>
+        <translation>Αριθμός αρχείων που ανακτήθηκαν</translation>
+    </message>
+    <message>
+        <location filename="src/qphotorec.cpp" line="837"/>
+        <source>QPhotoRec: Failed to create file!</source>
+        <translation>QPhotoRec: Αποτυχία δημιουργίας αρχείου!</translation>
+    </message>
+    <message>
+        <location filename="src/qphotorec.cpp" line="838"/>
+        <source>Failed to create file! Please choose another destination</source>
+        <translation>Αποτυχία δημιουργίας αρχείου! Παρακαλώ επιλέξετε άλλο προορισμό</translation>
+    </message>
+    <message>
+        <location filename="src/qphotorec.cpp" line="857"/>
+        <source>QPhotoRec: Not enough space!</source>
+        <translation>QPhotoRec: Μη επαρκής χώρος!</translation>
+    </message>
+    <message>
+        <location filename="src/qphotorec.cpp" line="858"/>
+        <source>There is not enough space left! Please free disk space and/or choose another destination</source>
+        <translation>Δεν υπάρχει αρκετός ελεύθερος χώρος! Παρακαλώ ελευθερώστε χώρο στο δίσκο ή/και επιλέξτε άλλο προορισμό</translation>
+    </message>
+    <message>
+        <location filename="src/qphotorec.cpp" line="927"/>
+        <source>QPhotoRec is is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 2 of the License, or (at your option) any later version.
+
+QPhotoRec is is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with QPhotoRec.  If not, see &lt;http://www.gnu.org/licenses/&gt;.</source>
+        <translation>Το QPhotoRec είναι ελεύθερο λογισμικό: μπορείτε να το αναδιανείμετε ή/και να το τροποποιήσετε σύμφωνα με τους όρους της Γενικής Άδειας Δημόσιας Χρήσης GNU, όπως δημοσιεύθηκε από το Ίδρυμα Ελεύθερου Λογισμικού, είτε με βάση την έκδοση 2 της Άδειας, είτε (κατά την επιλογή σας) οποιαδήποτε νεότερη έκδοση.
+
+Το QPhotoRec διανέμεται με την ελπίδα ότι θα είναι χρήσιμο, αλλά ΧΩΡΙΣ ΚΑΜΙΑ ΕΓΓΥΗΣΗ, χωρίς ούτε τη σιωπηρή εγγύηση ΕΜΠΟΡΙΚΟΤΗΤΑΣ ή ΚΑΤΑΛΛΗΛΟΤΗΤΑΣ ΓΙΑ ΕΙΔΙΚΟ ΣΚΟΠΟ. Για περισσότερες λεπτομέρειες, ανατρέξτε στην άδεια Γενικής Άδειας Δημόσιας Χρήσης GNU.
+
+Θα πρέπει να έχετε λάβει ένα αντίγραφο της Γενικής Άδειας Δημόσιας Χρήσης GNU μαζί με το QPhotoRec.  Εάν όχι, ανατρέξτε στο &lt;http://www.gnu.org/licenses/&gt;.</translation>
+    </message>
+    <message>
+        <location filename="src/qphotorec.cpp" line="928"/>
+        <source>QPhotoRec: About</source>
+        <translation>QPhotoRec: Περί</translation>
+    </message>
+    <message>
+        <location filename="src/qphotorec.cpp" line="957"/>
+        <source>File Formats</source>
+        <translation>Μορφές Αρχείου</translation>
+    </message>
+    <message>
+        <location filename="src/qphotorec.cpp" line="960"/>
+        <source>&amp;Reset</source>
+        <translation>&amp;Επαναφορά</translation>
+    </message>
+    <message>
+        <location filename="src/qphotorec.cpp" line="961"/>
+        <source>Res&amp;tore</source>
+        <translation>Ανά&amp;κτηση</translation>
+    </message>
+</context>
+</TS>
