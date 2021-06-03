21bcf0d465fe0f525c16d371698302ea96417919
Christophe Grenier <grenier@cgsecurity.org>
2021-06-03 13:49:24 +0200

    src/file_arj.c: update Frama-C annotations

--- src/file_arj.c
+++ src/file_arj.c
@@ -32,7 +32,7 @@
 #include "filegen.h"
 #include "common.h"
 
-/*@ requires \valid(file_stat); */
+/*@ requires valid_register_header_check(file_stat); */
 static void register_header_check_arj(file_stat_t *file_stat);
 
 const file_hint_t file_hint_arj= {
@@ -119,13 +119,11 @@ struct arj_main_header {
 } __attribute__ ((gcc_struct, __packed__));
 
 /*@
-  @ requires \valid(file_recovery);
-  @ requires valid_file_recovery(file_recovery);
-  @ requires \separated(file_recovery, file_recovery->handle, file_recovery->extension, &errno, &Frama_C_entropy_source);
   @ requires file_recovery->file_check == &file_check_arj;
-  @ ensures \valid(file_recovery->handle);
-  @ assigns *file_recovery->handle, errno, file_recovery->file_size;
-  @ assigns Frama_C_entropy_source;
+  @ requires valid_file_check_param(file_recovery);
+  @ ensures  valid_file_check_result(file_recovery);
+  @ assigns  *file_recovery->handle, errno, file_recovery->file_size;
+  @ assigns  Frama_C_entropy_source;
   @*/
 static void file_check_arj(file_recovery_t *file_recovery)
 {
@@ -135,12 +133,9 @@ static void file_check_arj(file_recovery_t *file_recovery)
 
 /*@
   @ requires buffer_size >= sizeof(struct arj_main_header);
-  @ requires \valid_read(buffer+(0..buffer_size-1));
-  @ requires valid_file_recovery(file_recovery);
-  @ requires \valid(file_recovery_new);
-  @ requires file_recovery_new->blocksize > 0;
   @ requires separation: \separated(&file_hint_arj, buffer+(..), file_recovery, file_recovery_new);
-  @ ensures  \result!=0 ==> valid_file_recovery(file_recovery_new);
+  @ requires valid_header_check_param(buffer, buffer_size, safe_header_only, file_recovery, file_recovery_new);
+  @ ensures  valid_header_check_result(\result, file_recovery_new);
   @ assigns  *file_recovery_new;
   @*/
 static int header_check_arj(const unsigned char *buffer, const unsigned int buffer_size, const unsigned int safe_header_only, const file_recovery_t *file_recovery, file_recovery_t *file_recovery_new)
