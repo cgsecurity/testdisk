e961bfe234b89c2db1c733af5c26d1dfc23e2d85
Christophe Grenier <grenier@cgsecurity.org>
2021-06-06 16:37:19 +0200

    src/file_cwk.c: improve Frama-C annotations

--- src/file_cwk.c
+++ src/file_cwk.c
@@ -32,7 +32,7 @@
 #include "filegen.h"
 #include "common.h"
 
-/*@ requires \valid(file_stat); */
+/*@ requires valid_register_header_check(file_stat); */
 static void register_header_check_cwk(file_stat_t *file_stat);
 
 const file_hint_t file_hint_cwk= {
@@ -65,16 +65,12 @@ struct cwk_header
 } __attribute__ ((gcc_struct, __packed__));
 
 /*@
-  @ requires valid_file_recovery(file_recovery);
-  @ requires \valid(file_recovery->handle);
-  @ requires \separated(file_recovery, file_recovery->handle, file_recovery->extension, &errno, &Frama_C_entropy_source);
-  @ requires \initialized(&file_recovery->time);
-  @
   @ requires file_recovery->file_check == &file_check_cwk;
+  @ requires valid_file_check_param(file_recovery);
+  @ ensures  valid_file_check_result(file_recovery);
   @ assigns *file_recovery->handle, errno, file_recovery->file_size;
   @ assigns Frama_C_entropy_source;
   @
-  @ ensures \valid(file_recovery->handle);
   @*/
 static void file_check_cwk(file_recovery_t *file_recovery)
 {
@@ -84,14 +80,10 @@ static void file_check_cwk(file_recovery_t *file_recovery)
 
 /*@
   @ requires buffer_size >= sizeof(struct cwk_header);
-  @ requires \valid_read(buffer+(0..buffer_size-1));
-  @ requires valid_file_recovery(file_recovery);
-  @ requires \valid(file_recovery_new);
-  @ requires file_recovery_new->blocksize > 0;
   @ requires separation: \separated(&file_hint_cwk, buffer+(..), file_recovery, file_recovery_new);
+  @ requires valid_header_check_param(buffer, buffer_size, safe_header_only, file_recovery, file_recovery_new);
+  @ ensures  valid_header_check_result(\result, file_recovery_new);
   @ assigns  *file_recovery_new;
-  @ ensures \result == 0 || \result == 1;
-  @ ensures  \result!=0 ==> valid_file_recovery(file_recovery_new);
   @*/
 static int header_check_cwk(const unsigned char *buffer, const unsigned int buffer_size, const unsigned int safe_header_only, const file_recovery_t *file_recovery, file_recovery_t *file_recovery_new)
 {
