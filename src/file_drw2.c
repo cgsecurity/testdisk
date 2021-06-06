3c4ec8f41f697ec03c2674b79bb7f42f1f7f5f98
Christophe Grenier <grenier@cgsecurity.org>
2021-06-06 18:12:41 +0200

    src/file_drw2.c: add Frama-C annotations

--- src/file_drw2.c
+++ src/file_drw2.c
@@ -31,7 +31,7 @@
 #include "types.h"
 #include "filegen.h"
 
-/*@ requires \valid(file_stat); */
+/*@ requires valid_register_header_check(file_stat); */
 static void register_header_check_drw2(file_stat_t *file_stat);
 
 const file_hint_t file_hint_drw2= {
@@ -44,13 +44,10 @@ const file_hint_t file_hint_drw2= {
 };
 
 /*@
-  @ requires buffer_size > 0;
-  @ requires \valid_read(buffer+(0..buffer_size-1));
-  @ requires valid_file_recovery(file_recovery);
   @ requires \valid(file_recovery_new);
   @ requires separation: \separated(&file_hint_drw2, buffer+(..), file_recovery, file_recovery_new);
-  @ ensures  \result == 1;
-  @ ensures  valid_file_recovery(file_recovery_new);
+  @ requires valid_header_check_param(buffer, buffer_size, safe_header_only, file_recovery, file_recovery_new);
+  @ ensures  valid_header_check_result(\result, file_recovery_new);
   @ assigns  *file_recovery_new;
   @*/
 static int header_check_drw2(const unsigned char *buffer, const unsigned int buffer_size, const unsigned int safe_header_only, const file_recovery_t *file_recovery, file_recovery_t *file_recovery_new)
