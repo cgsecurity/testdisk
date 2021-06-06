810a9bc55cc6388147ae73af3c0ed7bd1ea4e957
Christophe Grenier <grenier@cgsecurity.org>
2021-06-06 15:17:47 +0200

    src/file_bz2.c: improve Frama-C annotations

--- src/file_bz2.c
+++ src/file_bz2.c
@@ -31,7 +31,7 @@
 #include "types.h"
 #include "filegen.h"
 
-/*@ requires \valid(file_stat); */
+/*@ requires valid_register_header_check(file_stat); */
 static void register_header_check_bz2(file_stat_t *file_stat);
 
 const file_hint_t file_hint_bz2= {
@@ -45,14 +45,10 @@ const file_hint_t file_hint_bz2= {
 
 /*@
   @ requires buffer_size >= 10;
-  @ requires \valid_read(buffer+(0..buffer_size-1));
-  @ requires valid_file_recovery(file_recovery);
-  @ requires \valid(file_recovery_new);
-  @ requires file_recovery_new->blocksize > 0;
   @ requires separation: \separated(&file_hint_bz2, buffer+(..), file_recovery, file_recovery_new);
+  @ requires valid_header_check_param(buffer, buffer_size, safe_header_only, file_recovery, file_recovery_new);
+  @ ensures  valid_header_check_result(\result, file_recovery_new);
   @ assigns  *file_recovery_new;
-  @ ensures \result == 0 || \result == 1;
-  @ ensures  \result!=0 ==> valid_file_recovery(file_recovery_new);
   @*/
 static int header_check_bz2(const unsigned char *buffer, const unsigned int buffer_size, const unsigned int safe_header_only, const file_recovery_t *file_recovery, file_recovery_t *file_recovery_new)
 {
