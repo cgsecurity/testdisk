7c242d56025788399c7f37334d7d4e835f664362
Christophe Grenier <grenier@cgsecurity.org>
2021-06-06 11:34:10 +0200

    src/file_ape.c: improve Frama-C annotations

--- src/file_ape.c
+++ src/file_ape.c
@@ -31,7 +31,7 @@
 #include "filegen.h"
 #include "common.h"
 
-/*@ requires \valid(file_stat); */
+/*@ requires valid_register_header_check(file_stat); */
 static void register_header_check_ape(file_stat_t *file_stat);
 
 const file_hint_t file_hint_ape= {
@@ -102,15 +102,12 @@ struct APE_HEADER
 
 /*@
   @ requires buffer_size >= sizeof(struct APE_HEADER_OLD);
-  @ requires \valid_read(buffer+(0..buffer_size-1));
-  @ requires valid_file_recovery(file_recovery);
-  @ requires \valid(file_recovery_new);
-  @ requires file_recovery_new->blocksize > 0;
   @ requires separation: \separated(&file_hint_ape, buffer, file_recovery, file_recovery_new);
-  @ ensures \result == 0 || \result == 1;
-  @ ensures  \result!=0 ==> valid_file_recovery(file_recovery_new);
+  @ requires valid_header_check_param(buffer, buffer_size, safe_header_only, file_recovery, file_recovery_new);
+  @ ensures  valid_header_check_result(\result, file_recovery_new);
   @ assigns  *file_recovery_new;
-  @*/static int header_check_ape(const unsigned char *buffer, const unsigned int buffer_size, const unsigned int safe_header_only, const file_recovery_t *file_recovery, file_recovery_t *file_recovery_new)
+  @*/
+static int header_check_ape(const unsigned char *buffer, const unsigned int buffer_size, const unsigned int safe_header_only, const file_recovery_t *file_recovery, file_recovery_t *file_recovery_new)
 {
   const struct APE_HEADER_OLD *ape=(const struct APE_HEADER_OLD*)buffer;
   /* Version 3.96 released April 7, 2002, Version 4.06 March 17, 2009 */
