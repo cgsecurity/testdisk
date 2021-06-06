46d107f4d393ab1e8f0d15343bff847e20cc875a
Christophe Grenier <grenier@cgsecurity.org>
2021-06-06 16:29:43 +0200

    src/file_crw.c: improve Frama-C annotations

--- src/file_crw.c
+++ src/file_crw.c
@@ -32,7 +32,7 @@
 #include "filegen.h"
 #include "log.h"
 
-/*@ requires \valid(file_stat); */
+/*@ requires valid_register_header_check(file_stat); */
 static void register_header_check_crw(file_stat_t *file_stat);
 
 const file_hint_t file_hint_crw= {
@@ -44,6 +44,12 @@ const file_hint_t file_hint_crw= {
   .register_header_check=&register_header_check_crw
 };
 
+/*@
+  @ requires valid_file_check_param(file_recovery);
+  @ ensures  valid_file_check_result(file_recovery);
+  @ assigns  *file_recovery->handle, errno, file_recovery->file_size;
+  @ assigns  Frama_C_entropy_source;
+  @*/
 static void file_check_crw(file_recovery_t *file_recovery)
 {
   const unsigned char crw_footer[2]= { 0x0A, 0x30};
@@ -52,14 +58,10 @@ static void file_check_crw(file_recovery_t *file_recovery)
 
 /*@
   @ requires buffer_size >= 6+8;
-  @ requires \valid_read(buffer+(0..buffer_size-1));
-  @ requires valid_file_recovery(file_recovery);
-  @ requires \valid(file_recovery_new);
-  @ requires file_recovery_new->blocksize > 0;
   @ requires separation: \separated(&file_hint_crw, buffer+(..), file_recovery, file_recovery_new);
+  @ requires valid_header_check_param(buffer, buffer_size, safe_header_only, file_recovery, file_recovery_new);
+  @ ensures  valid_header_check_result(\result, file_recovery_new);
   @ assigns  *file_recovery_new;
-  @ ensures \result == 0 || \result == 1;
-  @ ensures  \result!=0 ==> valid_file_recovery(file_recovery_new);
   @*/
 static int header_check_crw(const unsigned char *buffer, const unsigned int buffer_size, const unsigned int safe_header_only, const file_recovery_t *file_recovery, file_recovery_t *file_recovery_new)
 {
