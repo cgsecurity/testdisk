90a55cab1085fe5b83beae02b3c64f8f5a8d0d41
Christophe Grenier <grenier@cgsecurity.org>
2021-06-06 16:01:33 +0200

    src/file_che.c: improve Frama-C annotations

--- src/file_che.c
+++ src/file_che.c
@@ -33,7 +33,7 @@
 #include "common.h"
 #include "log.h"
 
-/*@ requires \valid(file_stat); */
+/*@ requires valid_register_header_check(file_stat); */
 static void register_header_check_che(file_stat_t *file_stat);
 
 const file_hint_t file_hint_che= {
@@ -55,16 +55,12 @@ struct che_block
 
 
 /*@
-  @ requires valid_file_recovery(file_recovery);
-  @ requires \valid(file_recovery->handle);
-  @ requires \separated(file_recovery, file_recovery->handle, file_recovery->extension, &errno, &Frama_C_entropy_source);
-  @ requires \initialized(&file_recovery->time);
-  @
   @ requires file_recovery->file_check == &file_check_che;
+  @ requires valid_file_check_param(file_recovery);
+  @ ensures  valid_file_check_result(file_recovery);
   @ assigns *file_recovery->handle, errno, file_recovery->file_size;
   @ assigns Frama_C_entropy_source;
   @
-  @ ensures \valid(file_recovery->handle);
   @*/
 static void file_check_che(file_recovery_t *file_recovery)
 {
@@ -103,15 +99,10 @@ static void file_check_che(file_recovery_t *file_recovery)
 }
 
 /*@
-  @ requires buffer_size > 0;
-  @ requires \valid_read(buffer+(0..buffer_size-1));
-  @ requires valid_file_recovery(file_recovery);
-  @ requires \valid(file_recovery_new);
-  @ requires file_recovery_new->blocksize > 0;
   @ requires separation: \separated(&file_hint_che, buffer+(..), file_recovery, file_recovery_new);
+  @ requires valid_header_check_param(buffer, buffer_size, safe_header_only, file_recovery, file_recovery_new);
+  @ ensures  valid_header_check_result(\result, file_recovery_new);
   @ assigns  *file_recovery_new;
-  @ ensures \result == 0 || \result == 1;
-  @ ensures  \result!=0 ==> valid_file_recovery(file_recovery_new);
   @*/
 static int header_check_che(const unsigned char *buffer, const unsigned int buffer_size, const unsigned int safe_header_only, const file_recovery_t *file_recovery, file_recovery_t *file_recovery_new)
 {
