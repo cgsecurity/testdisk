053fc5b5f970f69be6ed0398bef78542c55bd8a7
Christophe Grenier <grenier@cgsecurity.org>
2021-06-06 15:01:31 +0200

    src/file_bpg.c: improve Frama-C annotations

--- src/file_bpg.c
+++ src/file_bpg.c
@@ -37,7 +37,7 @@
 
 #define MAX_BPG_SIZE 0x800000
 
-/*@ requires \valid(file_stat); */
+/*@ requires valid_register_header_check(file_stat); */
 static void register_header_check_bpg(file_stat_t *file_stat);
 
 const file_hint_t file_hint_bpg= {
@@ -82,30 +82,15 @@ static unsigned int getue32(const unsigned char *buffer, const unsigned int buff
 }
 
 /*@
-  @ requires buffer_size > 0;
-  @ requires \valid_read(buffer+(0..buffer_size-1));
-  @ requires \valid_read(file_recovery);
-  @ requires file_recovery->file_stat==\null || valid_read_string((char*)file_recovery->filename);
-  @ requires \valid(file_recovery_new);
-  @ requires file_recovery_new->blocksize > 0;
-  @
   @ requires buffer_size >= 6+3*5;
   @ requires separation: \separated(&file_hint_bpg, buffer+(..), file_recovery, file_recovery_new);
-  @
-  @ ensures \result == 0 || \result == 1;
-  @ ensures (\result == 1) ==> (file_recovery_new->file_stat == \null);
-  @ ensures (\result == 1) ==> (file_recovery_new->handle == \null);
-  @ ensures (\result == 1) ==> \initialized(&file_recovery_new->time);
-  @ ensures (\result == 1) ==> \initialized(&file_recovery_new->calculated_file_size);
+  @ requires valid_header_check_param(buffer, buffer_size, safe_header_only, file_recovery, file_recovery_new);
+  @ ensures  valid_header_check_result(\result, file_recovery_new);
   @ ensures (\result == 1) ==> file_recovery_new->file_size == 0;
-  @ ensures (\result == 1) ==> \initialized(&file_recovery_new->min_filesize);
-  @ ensures (\result != 0) ==> file_recovery_new->extension != \null;
-  @
   @ ensures (\result == 1) ==> (file_recovery_new->time == 0);
   @ ensures (\result == 1) ==> (file_recovery_new->extension == file_hint_bpg.extension);
   @ ensures (\result == 1) ==> (file_recovery_new->data_check== &data_check_size);
   @ ensures (\result == 1) ==> (file_recovery_new->file_check == &file_check_size);
-  @ ensures  \result!=0 ==> valid_file_recovery(file_recovery_new);
   @ assigns  *file_recovery_new;
   @*/
 static int header_check_bpg(const unsigned char *buffer, const unsigned int buffer_size, const unsigned int safe_header_only, const file_recovery_t *file_recovery, file_recovery_t *file_recovery_new)
