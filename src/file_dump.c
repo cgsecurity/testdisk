12255e547f8eae451b70d86f7a1725fe0553f74f
Christophe Grenier <grenier@cgsecurity.org>
2021-06-07 13:27:00 +0200

    src/file_dump.c: improve Frama-C annotations

--- src/file_dump.c
+++ src/file_dump.c
@@ -33,7 +33,7 @@
 #include "filegen.h"
 #define TS_TAPE         1       /* dump tape header */
 
-/*@ requires \valid(file_stat); */
+/*@ requires valid_register_header_check(file_stat); */
 static void register_header_check_dump(file_stat_t *file_stat);
 
 const file_hint_t file_hint_dump= {
@@ -127,14 +127,10 @@ struct	dump_struct
 
 /*@
   @ requires buffer_size >= sizeof(struct dump_struct);
-  @ requires \valid_read(buffer+(0..buffer_size-1));
-  @ requires valid_file_recovery(file_recovery);
-  @ requires \valid(file_recovery_new);
-  @ requires file_recovery_new->blocksize > 0;
   @ requires separation: \separated(&file_hint_dump, buffer+(..), file_recovery, file_recovery_new);
+  @ requires valid_header_check_param(buffer, buffer_size, safe_header_only, file_recovery, file_recovery_new);
+  @ ensures  valid_header_check_result(\result, file_recovery_new);
   @ assigns  *file_recovery_new;
-  @ ensures \result == 0 || \result == 1;
-  @ ensures  \result!=0 ==> valid_file_recovery(file_recovery_new);
   @*/
 static int header_check_dump(const unsigned char *buffer, const unsigned int buffer_size, const unsigned int safe_header_only, const file_recovery_t *file_recovery, file_recovery_t *file_recovery_new)
 {
