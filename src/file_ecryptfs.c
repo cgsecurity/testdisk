2745d4c5c8576098d81f8ee8fb901d568cd5ee35
Christophe Grenier <grenier@cgsecurity.org>
2021-06-08 13:18:50 +0200

    src/file_ecryptfs.c: improve Frama-C annotations

--- src/file_ecryptfs.c
+++ src/file_ecryptfs.c
@@ -32,7 +32,7 @@
 #include "filegen.h"
 #include "common.h"
 
-/*@ requires \valid(file_stat); */
+/*@ requires valid_register_header_check(file_stat); */
 static void register_header_check_ecryptfs(file_stat_t *file_stat);
 
 const file_hint_t file_hint_ecryptfs= {
@@ -57,10 +57,9 @@ struct ecrypfs_header {
 } __attribute__ ((gcc_struct, __packed__));
 
 /*@
-  @ requires valid_file_recovery(file_recovery);
-  @ requires \valid(file_recovery->handle);
-  @ requires \separated(file_recovery, file_recovery->handle, file_recovery->extension, &errno, &Frama_C_entropy_source);
   @ requires file_recovery->file_check == &file_check_ecryptfs;
+  @ requires valid_file_check_param(file_recovery);
+  @ ensures  valid_file_check_result(file_recovery);
   @ assigns  file_recovery->file_size;
   @*/
 static void file_check_ecryptfs(file_recovery_t *file_recovery)
@@ -73,21 +72,18 @@ static void file_check_ecryptfs(file_recovery_t *file_recovery)
 
 /*@
   @ requires buffer_size >= sizeof(struct ecrypfs_header);
-  @ requires \valid_read(buffer+(0..buffer_size-1));
-  @ requires valid_file_recovery(file_recovery);
-  @ requires \valid(file_recovery_new);
-  @ requires file_recovery_new->blocksize > 0;
   @ requires separation: \separated(&file_hint_ecryptfs, buffer+(..), file_recovery, file_recovery_new);
+  @ requires valid_header_check_param(buffer, buffer_size, safe_header_only, file_recovery, file_recovery_new);
+  @ ensures  valid_header_check_result(\result, file_recovery_new);
   @ assigns  *file_recovery_new;
-  @ ensures \result == 0 || \result == 1;
-  @ ensures  \result!=0 ==> valid_file_recovery(file_recovery_new);
   @*/
 static int header_check_ecryptfs(const unsigned char *buffer, const unsigned int buffer_size, const unsigned int safe_header_only, const file_recovery_t *file_recovery, file_recovery_t *file_recovery_new)
 {
   const struct ecrypfs_header *e=(const struct ecrypfs_header *)buffer;
+  const uint64_t unencrypted_file_size=be64(e->unencrypted_file_size);
   if((be32(e->marker1) ^ be32(e->marker2)) != 0x3c81b7f5)
     return 0;
-  if(be64(e->unencrypted_file_size) < sizeof(struct ecrypfs_header))
+  if(unencrypted_file_size < sizeof(struct ecrypfs_header))
     return 0;
   reset_file_recovery(file_recovery_new);
 #ifdef DJGPP
@@ -95,8 +91,8 @@ static int header_check_ecryptfs(const unsigned char *buffer, const unsigned int
 #else
   file_recovery_new->extension=file_hint_ecryptfs.extension;
 #endif
-  file_recovery_new->min_filesize=be64(e->unencrypted_file_size);
-  file_recovery_new->calculated_file_size=be64(e->unencrypted_file_size);
+  file_recovery_new->min_filesize=unencrypted_file_size;
+  file_recovery_new->calculated_file_size=unencrypted_file_size;
   file_recovery_new->data_check=NULL;
   file_recovery_new->file_check=&file_check_ecryptfs;
   return 1;
