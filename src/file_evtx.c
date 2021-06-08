2a3f5aee88fb61ca2143a0d10a4141354a9ea25b
Christophe Grenier <grenier@cgsecurity.org>
2021-06-08 13:54:30 +0200

    src/file_evtx.c: improve Frama-C annotations

--- src/file_evtx.c
+++ src/file_evtx.c
@@ -48,7 +48,7 @@ struct evtx_header
   uint32_t Checksum;
 } __attribute__ ((gcc_struct, __packed__));
 
-/*@ requires \valid(file_stat); */
+/*@ requires valid_register_header_check(file_stat); */
 static void register_header_check_evtx(file_stat_t *file_stat);
 
 const file_hint_t file_hint_evtx= {
@@ -62,12 +62,9 @@ const file_hint_t file_hint_evtx= {
 
 /*@
   @ requires buffer_size >= sizeof(struct evtx_header);
-  @ requires \valid_read(buffer+(0..buffer_size-1));
-  @ requires valid_file_recovery(file_recovery);
-  @ requires \valid(file_recovery_new);
-  @ requires file_recovery_new->blocksize > 0;
   @ requires separation: \separated(&file_hint_evtx, buffer+(..), file_recovery, file_recovery_new);
-  @ ensures  \result!=0 ==> valid_file_recovery(file_recovery_new);
+  @ requires valid_header_check_param(buffer, buffer_size, safe_header_only, file_recovery, file_recovery_new);
+  @ ensures  valid_header_check_result(\result, file_recovery_new);
   @ assigns  *file_recovery_new;
   @*/
 static int header_check_evtx(const unsigned char *buffer, const unsigned int buffer_size, const unsigned int safe_header_only, const file_recovery_t *file_recovery, file_recovery_t *file_recovery_new)
