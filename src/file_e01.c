55fa7ed34e44988088a778f6b0bdb64185be7b65
Christophe Grenier <grenier@cgsecurity.org>
2021-06-08 12:28:44 +0200

    src/file_e01.c: improve Frama-C annotations

--- src/file_e01.c
+++ src/file_e01.c
@@ -32,7 +32,7 @@
 #include "filegen.h"
 #include "common.h"
 
-/*@ requires \valid(file_stat); */
+/*@ requires valid_register_header_check(file_stat); */
 static void register_header_check_e01(file_stat_t *file_stat);
 
 static char ext[10];
@@ -70,13 +70,11 @@ struct ewf_file_header
 } __attribute__ ((gcc_struct, __packed__));
 
 /*@
-  @ requires valid_file_recovery(file_recovery);
-  @ requires \valid(file_recovery->handle);
-  @ requires \separated(file_recovery, file_recovery->handle, file_recovery->extension, &errno, &Frama_C_entropy_source);
   @ requires file_recovery->file_check == &file_check_e01;
+  @ requires valid_file_check_param(file_recovery);
+  @ ensures  valid_file_check_result(file_recovery);
   @ assigns *file_recovery->handle, errno, file_recovery->file_size;
   @ assigns Frama_C_entropy_source;
-  @ ensures \valid(file_recovery->handle);
   @*/
 static void file_check_e01(file_recovery_t *file_recovery)
 {
@@ -98,14 +96,10 @@ static void file_check_e01(file_recovery_t *file_recovery)
 
 /*@
   @ requires buffer_size >= sizeof(struct ewf_file_header);
-  @ requires \valid_read(buffer+(0..buffer_size-1));
-  @ requires valid_file_recovery(file_recovery);
-  @ requires \valid(file_recovery_new);
-  @ requires file_recovery_new->blocksize > 0;
   @ requires separation: \separated(&file_hint_e01, buffer+(..), file_recovery, file_recovery_new);
+  @ requires valid_header_check_param(buffer, buffer_size, safe_header_only, file_recovery, file_recovery_new);
+  @ ensures  valid_header_check_result(\result, file_recovery_new);
   @ assigns  *file_recovery_new, *(ext + (0 .. sizeof(ext)-1));
-  @ ensures \result == 0 || \result == 1;
-  @ ensures  \result!=0 ==> valid_file_recovery(file_recovery_new);
   @*/
 static int header_check_e01(const unsigned char *buffer, const unsigned int buffer_size, const unsigned int safe_header_only, const file_recovery_t *file_recovery, file_recovery_t *file_recovery_new)
 {
