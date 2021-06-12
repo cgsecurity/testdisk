f5eab1d9ceee5437183be381ce114a3c109d1a57
Christophe Grenier <grenier@cgsecurity.org>
2021-06-12 21:44:28 +0200

    src/file_fh5.c: improve Frama-C annotations

--- src/file_fh5.c
+++ src/file_fh5.c
@@ -40,7 +40,7 @@ struct fh5_header_s
 } __attribute__ ((gcc_struct, __packed__));
 typedef struct fh5_header_s fh5_header_t;
 
-/*@ requires \valid(file_stat); */
+/*@ requires valid_register_header_check(file_stat); */
 static void register_header_check_fh5(file_stat_t *file_stat);
 
 const file_hint_t file_hint_fh5= {
@@ -53,10 +53,9 @@ const file_hint_t file_hint_fh5= {
 };
 
 /*@
-  @ requires \valid(file_recovery);
   @ requires file_recovery->file_check == &file_check_fh5;
-  @ requires valid_file_recovery(file_recovery);
-  @ ensures  valid_file_recovery(file_recovery);
+  @ requires valid_file_check_param(file_recovery);
+  @ ensures  valid_file_check_result(file_recovery);
   @ assigns  file_recovery->file_size;
   @*/
 static void file_check_fh5(file_recovery_t *file_recovery)
@@ -69,13 +68,9 @@ static void file_check_fh5(file_recovery_t *file_recovery)
 
 /*@
   @ requires buffer_size >= sizeof(fh5_header_t);
-  @ requires \valid_read(buffer+(0..buffer_size-1));
-  @ requires valid_file_recovery(file_recovery);
-  @ requires \valid(file_recovery_new);
-  @ requires file_recovery_new->blocksize > 0;
   @ requires separation: \separated(&file_hint_fh5, buffer+(..), file_recovery, file_recovery_new);
-  @ ensures \result == 0 || \result == 1;
-  @ ensures  \result!=0 ==> valid_file_recovery(file_recovery_new);
+  @ requires valid_header_check_param(buffer, buffer_size, safe_header_only, file_recovery, file_recovery_new);
+  @ ensures  valid_header_check_result(\result, file_recovery_new);
   @ assigns  *file_recovery_new;
   @*/
 static int header_check_fh5(const unsigned char *buffer, const unsigned int buffer_size, const unsigned int safe_header_only, const file_recovery_t *file_recovery, file_recovery_t *file_recovery_new)
