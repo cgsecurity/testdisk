8fa16f4a5046de8db3d952f1064f61efd4771188
Christophe Grenier <grenier@cgsecurity.org>
2021-06-13 18:49:19 +0200

    src/file_fp7.c: improve Frama-C annotations

--- src/file_fp7.c
+++ src/file_fp7.c
@@ -30,9 +30,8 @@
 #include "types.h"
 #include "filegen.h"
 
-/*@ requires \valid(file_stat); */
+/*@ requires valid_register_header_check(file_stat); */
 static void register_header_check_fp7(file_stat_t *file_stat);
-static int header_check_fp7(const unsigned char *buffer, const unsigned int buffer_size, const unsigned int safe_header_only, const file_recovery_t *file_recovery, file_recovery_t *file_recovery_new);
 
 const file_hint_t file_hint_fp7= {
   .extension="fp7",
@@ -43,20 +42,20 @@ const file_hint_t file_hint_fp7= {
   .register_header_check=&register_header_check_fp7
 };
 
+/*@
+  @ requires valid_file_check_param(file_recovery);
+  @ ensures  valid_file_check_result(file_recovery);
+  @ assigns  file_recovery->file_size;
+  @*/
 static void file_check_fp7(file_recovery_t *file_recovery)
 {
   file_recovery->file_size=file_recovery->file_size/4096*4096;
 }
 
 /*@
-  @ requires buffer_size > 0;
-  @ requires \valid_read(buffer+(0..buffer_size-1));
-  @ requires valid_file_recovery(file_recovery);
-  @ requires \valid(file_recovery_new);
-  @ requires file_recovery_new->blocksize > 0;
   @ requires separation: \separated(&file_hint_fp7, buffer+(..), file_recovery, file_recovery_new);
-  @ ensures \result == 0 || \result == 1;
-  @ ensures  \result!=0 ==> valid_file_recovery(file_recovery_new);
+  @ requires valid_header_check_param(buffer, buffer_size, safe_header_only, file_recovery, file_recovery_new);
+  @ ensures  valid_header_check_result(\result, file_recovery_new);
   @ assigns  *file_recovery_new;
   @*/
 static int header_check_fp7(const unsigned char *buffer, const unsigned int buffer_size, const unsigned int safe_header_only, const file_recovery_t *file_recovery, file_recovery_t *file_recovery_new)
