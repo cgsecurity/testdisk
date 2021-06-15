0a8556ece7b733049322c32756491a4e20b63be9
Christophe Grenier <grenier@cgsecurity.org>
2021-06-15 17:21:32 +0200

    src/file_hdf.c: improve Frama-C annotations

--- src/file_hdf.c
+++ src/file_hdf.c
@@ -38,7 +38,7 @@
 #include "log.h"
 #endif
 
-/*@ requires \valid(file_stat); */
+/*@ requires valid_register_header_check(file_stat); */
 static void register_header_check_hdf(file_stat_t *file_stat);
 
 const file_hint_t file_hint_hdf= {
@@ -65,11 +65,9 @@ struct dd_struct
 } __attribute__ ((gcc_struct, __packed__));
 
 /*@
-  @ requires \valid(file_recovery);
-  @ requires \valid(file_recovery->handle);
-  @ requires valid_file_recovery(file_recovery);
   @ requires \separated(file_recovery, file_recovery->handle, &errno, &Frama_C_entropy_source, &__fc_heap_status);
-  @ ensures \valid(file_recovery->handle);
+  @ requires valid_file_check_param(file_recovery);
+  @ ensures  valid_file_check_result(file_recovery);
   @*/
 static void file_check_hdf(file_recovery_t *file_recovery)
 {
@@ -140,13 +138,9 @@ static void file_check_hdf(file_recovery_t *file_recovery)
 
 /*@
   @ requires buffer_size >= sizeof(struct ddh_struct);
-  @ requires \valid_read(buffer+(0..buffer_size-1));
-  @ requires valid_file_recovery(file_recovery);
-  @ requires \valid(file_recovery_new);
-  @ requires file_recovery_new->blocksize > 0;
   @ requires separation: \separated(&file_hint_hdf, buffer+(..), file_recovery, file_recovery_new);
-  @ ensures \result == 0 || \result == 1;
-  @ ensures  \result!=0 ==> valid_file_recovery(file_recovery_new);
+  @ requires valid_header_check_param(buffer, buffer_size, safe_header_only, file_recovery, file_recovery_new);
+  @ ensures  valid_header_check_result(\result, file_recovery_new);
   @ assigns  *file_recovery_new;
   @*/
 static int header_check_hdf(const unsigned char *buffer, const unsigned int buffer_size, const unsigned int safe_header_only, const file_recovery_t *file_recovery, file_recovery_t *file_recovery_new)
