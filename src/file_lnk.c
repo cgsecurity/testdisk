3a995fc82c700dc803ded3008a9f6878799f08f9
Christophe Grenier <grenier@cgsecurity.org>
2021-06-30 13:49:31 +0200

    src/file_lnk.c: improve Frama-C annotations

--- src/file_lnk.c
+++ src/file_lnk.c
@@ -35,7 +35,7 @@
 #include "log.h"
 #endif
 
-/*@ requires \valid(file_stat); */
+/*@ requires valid_register_header_check(file_stat); */
 static void register_header_check_lnk(file_stat_t *file_stat);
 
 const file_hint_t file_hint_lnk= {
@@ -224,13 +224,10 @@ static unsigned int lnk_get_size(const unsigned char *buffer, const unsigned int
 
 /*@
   @ requires buffer_size >= 0x4c;
-  @ requires \valid_read(buffer+(0..buffer_size-1));
-  @ requires valid_file_recovery(file_recovery);
-  @ requires \valid(file_recovery_new);
-  @ requires file_recovery_new->blocksize > 0;
   @ requires separation: \separated(&file_hint_lnk, buffer+(..), file_recovery, file_recovery_new);
+  @ requires valid_header_check_param(buffer, buffer_size, safe_header_only, file_recovery, file_recovery_new);
+  @ ensures  valid_header_check_result(\result, file_recovery_new);
   @ assigns  *file_recovery_new;
-  @ ensures  \result!=0 ==> valid_file_recovery(file_recovery_new);
   @*/
 static int header_check_lnk(const unsigned char *buffer, const unsigned int buffer_size, const unsigned int safe_header_only, const file_recovery_t *file_recovery, file_recovery_t *file_recovery_new)
 {
