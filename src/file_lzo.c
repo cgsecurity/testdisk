f53cf3a49488765ea53cbd9fc20765ccd69cc6fc
Christophe Grenier <grenier@cgsecurity.org>
2021-07-02 12:21:52 +0200

    src/file_lzo.c: improve Frama-C annotations

--- src/file_lzo.c
+++ src/file_lzo.c
@@ -31,7 +31,7 @@
 #include "types.h"
 #include "filegen.h"
 
-/*@ requires \valid(file_stat); */
+/*@ requires valid_register_header_check(file_stat); */
 static void register_header_check_lzo(file_stat_t *file_stat);
 
 const file_hint_t file_hint_lzo= {
@@ -43,6 +43,12 @@ const file_hint_t file_hint_lzo= {
   .register_header_check=&register_header_check_lzo
 };
 
+/*@
+  @ requires separation: \separated(&file_hint_lzo, buffer+(..), file_recovery, file_recovery_new);
+  @ requires valid_header_check_param(buffer, buffer_size, safe_header_only, file_recovery, file_recovery_new);
+  @ ensures  valid_header_check_result(\result, file_recovery_new);
+  @ assigns  *file_recovery_new;
+  @*/
 static int header_check_lzo(const unsigned char *buffer, const unsigned int buffer_size, const unsigned int safe_header_only, const file_recovery_t *file_recovery, file_recovery_t *file_recovery_new)
 {
   reset_file_recovery(file_recovery_new);
