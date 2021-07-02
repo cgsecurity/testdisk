0ad0a6ec2938ca6f7cef893f069a663a328ff174
Christophe Grenier <grenier@cgsecurity.org>
2021-07-02 12:32:09 +0200

    src/file_max.c: improve Frama-C annotations

--- src/file_max.c
+++ src/file_max.c
@@ -33,7 +33,7 @@
 #include "filegen.h"
 
 
-/*@ requires \valid(file_stat); */
+/*@ requires valid_register_header_check(file_stat); */
 static void register_header_check_max(file_stat_t *file_stat);
 
 const file_hint_t file_hint_max= {
@@ -46,15 +46,10 @@ const file_hint_t file_hint_max= {
 };
 
 /*@
-  @ requires buffer_size > 0;
-  @ requires \valid_read(buffer+(0..buffer_size-1));
-  @ requires valid_file_recovery(file_recovery);
-  @ requires \valid(file_recovery_new);
-  @ requires file_recovery_new->blocksize > 0;
   @ requires separation: \separated(&file_hint_max, buffer+(..), file_recovery, file_recovery_new);
+  @ requires valid_header_check_param(buffer, buffer_size, safe_header_only, file_recovery, file_recovery_new);
+  @ ensures  valid_header_check_result(\result, file_recovery_new);
   @ assigns  *file_recovery_new;
-  @ ensures \result == 0 || \result == 1;
-  @ ensures  \result!=0 ==> valid_file_recovery(file_recovery_new);
   @*/
 static int header_check_max(const unsigned char *buffer, const unsigned int buffer_size, const unsigned int safe_header_only, const file_recovery_t *file_recovery, file_recovery_t *file_recovery_new)
 {
