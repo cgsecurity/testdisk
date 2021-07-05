9e4cf0aa03bdb9d06f169af6e11bf3ce19755e0b
Christophe Grenier <grenier@cgsecurity.org>
2021-07-05 13:50:14 +0200

    src/file_pcx.c: improve Frama-C annotations

--- src/file_pcx.c
+++ src/file_pcx.c
@@ -35,7 +35,7 @@
 #include "log.h"
 #endif
 
-/*@ requires \valid(file_stat); */
+/*@ requires valid_register_header_check(file_stat); */
 static void register_header_check_pcx(file_stat_t *file_stat);
 
 const file_hint_t file_hint_pcx= {
@@ -79,13 +79,9 @@ struct pcx_file_entry {
 
 /*@
   @ requires buffer_size >= sizeof(struct pcx_file_entry);
-  @ requires \valid_read(buffer+(0..buffer_size-1));
-  @ requires valid_file_recovery(file_recovery);
-  @ requires \valid(file_recovery_new);
-  @ requires file_recovery_new->blocksize > 0;
   @ requires separation: \separated(&file_hint_pcx, buffer+(..), file_recovery, file_recovery_new);
-  @ ensures \result == 0 || \result == 1;
-  @ ensures  \result!=0 ==> valid_file_recovery(file_recovery_new);
+  @ requires valid_header_check_param(buffer, buffer_size, safe_header_only, file_recovery, file_recovery_new);
+  @ ensures  valid_header_check_result(\result, file_recovery_new);
   @ assigns  *file_recovery_new;
   @*/
 static int header_check_pcx(const unsigned char *buffer, const unsigned int buffer_size, const unsigned int safe_header_only, const file_recovery_t *file_recovery, file_recovery_t *file_recovery_new)
