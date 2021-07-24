c9adff6a1f496fae1947193e25469c1b82c27317
Christophe Grenier <grenier@cgsecurity.org>
2021-07-24 14:58:19 +0200

    src/file_rm.c: improve Frama-C annotations

--- src/file_rm.c
+++ src/file_rm.c
@@ -32,7 +32,7 @@
 #include "filegen.h"
 #include "common.h"
 
-/*@ requires \valid(file_stat); */
+/*@ requires valid_register_header_check(file_stat); */
 static void register_header_check_rm(file_stat_t *file_stat);
 
 const file_hint_t file_hint_rm= {
@@ -55,12 +55,9 @@ struct rm_header
 
 /*@
   @ requires buffer_size >= sizeof(struct rm_header);
-  @ requires \valid_read(buffer+(0..buffer_size-1));
-  @ requires valid_file_recovery(file_recovery);
-  @ requires \valid(file_recovery_new);
-  @ requires file_recovery_new->blocksize > 0;
   @ requires separation: \separated(&file_hint_rm, buffer+(..), file_recovery, file_recovery_new);
-  @ ensures  \result!=0 ==> valid_file_recovery(file_recovery_new);
+  @ requires valid_header_check_param(buffer, buffer_size, safe_header_only, file_recovery, file_recovery_new);
+  @ ensures  valid_header_check_result(\result, file_recovery_new);
   @ assigns  *file_recovery_new;
   @*/
 static int header_check_rm(const unsigned char *buffer, const unsigned int buffer_size, const unsigned int safe_header_only, const file_recovery_t *file_recovery, file_recovery_t *file_recovery_new)
