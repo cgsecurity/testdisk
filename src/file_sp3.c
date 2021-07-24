dd4e040d3ad0976b817b9a0ac84666895c644bb7
Christophe Grenier <grenier@cgsecurity.org>
2021-07-24 15:53:36 +0200

    src/file_sp3.c: improve Frama-C annotations

--- src/file_sp3.c
+++ src/file_sp3.c
@@ -36,7 +36,7 @@
 #include "file_sp3.h"
 #include "common.h"
 
-/*@ requires \valid(file_stat); */
+/*@ requires valid_register_header_check(file_stat); */
 static void register_header_check_sp3(file_stat_t *file_stat);
 
 const file_hint_t file_hint_sp3= {
@@ -123,12 +123,9 @@ static uint64_t get_size_from_sp3(const struct SP3FileInfo *h)
 
 /*@
   @ requires buffer_size >= sizeof(struct SP3FileInfo);
-  @ requires \valid_read(buffer+(0..buffer_size-1));
-  @ requires valid_file_recovery(file_recovery);
-  @ requires \valid(file_recovery_new);
-  @ requires file_recovery_new->blocksize > 0;
   @ requires separation: \separated(&file_hint_sp3, buffer+(..), file_recovery, file_recovery_new);
-  @ ensures  \result!=0 ==> valid_file_recovery(file_recovery_new);
+  @ requires valid_header_check_param(buffer, buffer_size, safe_header_only, file_recovery, file_recovery_new);
+  @ ensures  valid_header_check_result(\result, file_recovery_new);
   @ assigns  *file_recovery_new;
   @*/
 static int header_check_sp3(const unsigned char *buffer, const unsigned int buffer_size, const unsigned int safe_header_only, const file_recovery_t *file_recovery, file_recovery_t *file_recovery_new)
