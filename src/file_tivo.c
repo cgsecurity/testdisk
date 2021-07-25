0877f7397f4f54b17181e15a0fa8758f9cf516a5
Christophe Grenier <grenier@cgsecurity.org>
2021-07-25 11:06:07 +0200

    src/file_tivo.c: improve Frama-C annotations

--- src/file_tivo.c
+++ src/file_tivo.c
@@ -31,7 +31,7 @@
 #include "types.h"
 #include "filegen.h"
 
-/*@ requires \valid(file_stat); */
+/*@ requires valid_register_header_check(file_stat); */
 static void register_header_check_tivo(file_stat_t *file_stat);
 
 const file_hint_t file_hint_tivo = {
@@ -44,11 +44,9 @@ const file_hint_t file_hint_tivo = {
 };
 
 /*@
-  @ requires \valid(file_recovery);
-  @ requires valid_file_recovery(file_recovery);
-  @ requires \separated(file_recovery, file_recovery->handle, file_recovery->extension, &errno, &Frama_C_entropy_source);
   @ requires file_recovery->file_check == &file_check_tivo;
-  @ ensures \valid(file_recovery->handle);
+  @ requires valid_file_check_param(file_recovery);
+  @ ensures  valid_file_check_result(file_recovery);
   @ assigns *file_recovery->handle, errno, file_recovery->file_size;
   @ assigns Frama_C_entropy_source;
   @*/
@@ -62,13 +60,9 @@ static void file_check_tivo(file_recovery_t *file_recovery)
 
 /*@
   @ requires buffer_size >= 0x1c+6;
-  @ requires \valid_read(buffer+(0..buffer_size-1));
-  @ requires valid_file_recovery(file_recovery);
-  @ requires \valid(file_recovery_new);
-  @ requires file_recovery_new->blocksize > 0;
   @ requires separation: \separated(&file_hint_tivo, buffer+(..), file_recovery, file_recovery_new);
-  @ ensures  \result == 0 || \result == 1;
-  @ ensures  \result!=0 ==> valid_file_recovery(file_recovery_new);
+  @ requires valid_header_check_param(buffer, buffer_size, safe_header_only, file_recovery, file_recovery_new);
+  @ ensures  valid_header_check_result(\result, file_recovery_new);
   @ assigns  *file_recovery_new;
   @*/
 static int header_check_tivo(const unsigned char *buffer, const unsigned int buffer_size, const unsigned int safe_header_only, const file_recovery_t *file_recovery, file_recovery_t *file_recovery_new)
