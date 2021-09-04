b394013916cf9102dc3e677bbdaff742c892e304
Christophe Grenier <grenier@cgsecurity.org>
2021-09-04 17:38:20 +0200

    src/file_mft.c: improve Frama-C annotations

--- src/file_mft.c
+++ src/file_mft.c
@@ -33,7 +33,7 @@
 #include "common.h"
 #include "ntfs_struct.h"
 
-/*@ requires \valid(file_stat); */
+/*@ requires valid_register_header_check(file_stat); */
 static void register_header_check_mft(file_stat_t *file_stat);
 
 const file_hint_t file_hint_mft= {
@@ -46,8 +46,8 @@ const file_hint_t file_hint_mft= {
 };
 
 /*@
-  @ requires \valid(file_recovery);
-  @ requires valid_file_recovery(file_recovery);
+  @ requires valid_file_rename_param(file_recovery);
+  @ ensures  valid_file_rename_result(file_recovery);
   @*/
 static void file_rename_mft(file_recovery_t *file_recovery)
 {
@@ -75,13 +75,9 @@ static void file_rename_mft(file_recovery_t *file_recovery)
 
 /*@
   @ requires buffer_size >= sizeof(struct ntfs_mft_record);
-  @ requires \valid_read(buffer+(0..buffer_size-1));
-  @ requires valid_file_recovery(file_recovery);
-  @ requires \valid(file_recovery_new);
-  @ requires file_recovery_new->blocksize > 0;
   @ requires separation: \separated(&file_hint_mft, buffer+(..), file_recovery, file_recovery_new);
-  @ ensures \result == 0 || \result == 1;
-  @ ensures  \result!=0 ==> valid_file_recovery(file_recovery_new);
+  @ requires valid_header_check_param(buffer, buffer_size, safe_header_only, file_recovery, file_recovery_new);
+  @ ensures  valid_header_check_result(\result, file_recovery_new);
   @ assigns  *file_recovery_new;
   @*/
 static int header_check_mft(const unsigned char *buffer, const unsigned int buffer_size, const unsigned int safe_header_only, const file_recovery_t *file_recovery, file_recovery_t *file_recovery_new)
