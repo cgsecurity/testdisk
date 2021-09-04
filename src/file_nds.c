5f1002487df106901714dadaceb7ef44b1942048
Christophe Grenier <grenier@cgsecurity.org>
2021-09-04 18:37:34 +0200

    src/file_nds.c: improve Frama-C annotations

--- src/file_nds.c
+++ src/file_nds.c
@@ -31,7 +31,7 @@
 #include "types.h"
 #include "filegen.h"
 
-/*@ requires \valid(file_stat); */
+/*@ requires valid_register_header_check(file_stat); */
 static void register_header_check_nds(file_stat_t *file_stat);
 
 const file_hint_t file_hint_nds= {
@@ -44,9 +44,9 @@ const file_hint_t file_hint_nds= {
 };
 
 /*@
-  @ requires \valid(file_recovery);
-  @ requires valid_file_recovery(file_recovery);
   @ requires file_recovery->file_rename==&file_rename_nds;
+  @ requires valid_file_rename_param(file_recovery);
+  @ ensures  valid_file_rename_result(file_recovery);
   @*/
 static void file_rename_nds(file_recovery_t *file_recovery)
 {
@@ -65,14 +65,10 @@ static void file_rename_nds(file_recovery_t *file_recovery)
 
 /*@
   @ requires buffer_size >= 0x10;
-  @ requires \valid_read(buffer+(0..buffer_size-1));
-  @ requires valid_file_recovery(file_recovery);
-  @ requires \valid(file_recovery_new);
-  @ requires file_recovery_new->blocksize > 0;
   @ requires separation: \separated(&file_hint_nds, buffer+(..), file_recovery, file_recovery_new);
+  @ requires valid_header_check_param(buffer, buffer_size, safe_header_only, file_recovery, file_recovery_new);
+  @ ensures  valid_header_check_result(\result, file_recovery_new);
   @ assigns  *file_recovery_new;
-  @ ensures \result == 0 || \result == 1;
-  @ ensures  \result!=0 ==> valid_file_recovery(file_recovery_new);
   @*/
 static int header_check_nds(const unsigned char *buffer, const unsigned int buffer_size, const unsigned int safe_header_only, const file_recovery_t *file_recovery, file_recovery_t *file_recovery_new)
 {
