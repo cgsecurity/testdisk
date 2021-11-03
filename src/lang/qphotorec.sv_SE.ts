d59dbf00879beafda8c615ac8b42ae1e7353bbbc
Christophe Grenier <grenier@cgsecurity.org>
2021-11-03 20:49:54 +0100

    QPhotoRec: add sv_SE and zh_CN translations

---
+++ src/lang/qphotorec.sv_SE.ts
@@ -0,0 +1,248 @@
+<?xml version="1.0" ?><!DOCTYPE TS><TS language="sv_SE" version="2.1">
+<context>
+    <name>QPhotorec</name>
+    <message>
+        <location filename="src/qphotorec.cpp" line="125"/>
+        <source>QPhotoRec</source>
+        <translation>QPhotoRec</translation>
+    </message>
+    <message>
+        <location filename="src/qphotorec.cpp" line="156"/>
+        <location filename="src/qphotorec.cpp" line="521"/>
+        <source>Please select a destination to save the recovered files to.</source>
+        <translation>Vänligen välj en plats att spara de återställda filerna till.</translation>
+    </message>
+    <message>
+        <location filename="src/qphotorec.cpp" line="170"/>
+        <source>Please select a raw file</source>
+        <translation>Vänligen välj en rå fil</translation>
+    </message>
+    <message>
+        <location filename="src/qphotorec.cpp" line="172"/>
+        <source>Raw Files (*.dd *.raw *.img)</source>
+        <translation>Råa filer (*.dd *.raw *.img)</translation>
+    </message>
+    <message>
+        <location filename="src/qphotorec.cpp" line="259"/>
+        <source>Sys=</source>
+        <translation>Sys=</translation>
+    </message>
+    <message>
+        <location filename="src/qphotorec.cpp" line="261"/>
+        <source>Unknown</source>
+        <translation>Okänd</translation>
+    </message>
+    <message>
+        <location filename="src/qphotorec.cpp" line="377"/>
+        <source>No harddisk found</source>
+        <translation>Ingen hårddisk hittades</translation>
+    </message>
+    <message>
+        <location filename="src/qphotorec.cpp" line="379"/>
+        <source>No harddisk found
+You need to be administrator to use this program.
+Under Win9x, use the DOS version instead.
+Under Vista or later, select this program, right-click and choose &quot;Run as administrator&quot;.</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location filename="src/qphotorec.cpp" line="388"/>
+        <source>No harddisk found
+You need to be root to use PhotoRec.</source>
+        <translation>Ingen hårddisk hittades
+Du måste vara root för att använda PhotoRec.</translation>
+    </message>
+    <message>
+        <location filename="src/qphotorec.cpp" line="393"/>
+        <source>No Disk!</source>
+        <translation>Ingen disk!</translation>
+    </message>
+    <message>
+        <location filename="src/qphotorec.cpp" line="455"/>
+        <source>Add a raw disk image...</source>
+        <translation>Lägg till en rå disk-avbild...</translation>
+    </message>
+    <message>
+        <location filename="src/qphotorec.cpp" line="461"/>
+        <source>PhotoRec is free software, and comes with ABSOLUTELY NO WARRANTY.</source>
+        <translation>PhotoRec är fri mjukvara och kommer med ABSOLUT INGEN SOM HELST GARANTI.</translation>
+    </message>
+    <message>
+        <location filename="src/qphotorec.cpp" line="462"/>
+        <source>Please select a media to recover from</source>
+        <translation>Vänligen välj ett media att återställa från</translation>
+    </message>
+    <message>
+        <location filename="src/qphotorec.cpp" line="465"/>
+        <source>Disk capacity must be correctly detected for a successful recovery.
+If a disk listed above has an incorrect size, check HD jumper settings and BIOS
+detection, and install the latest OS patches and disk drivers.</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location filename="src/qphotorec.cpp" line="472"/>
+        <source>Flags</source>
+        <translation>Flaggor</translation>
+    </message>
+    <message>
+        <location filename="src/qphotorec.cpp" line="473"/>
+        <source>Type</source>
+        <translation>Typ</translation>
+    </message>
+    <message>
+        <location filename="src/qphotorec.cpp" line="474"/>
+        <source>File System</source>
+        <translation>Filsystem</translation>
+    </message>
+    <message>
+        <location filename="src/qphotorec.cpp" line="475"/>
+        <source>Size</source>
+        <translation>Storlek</translation>
+    </message>
+    <message>
+        <location filename="src/qphotorec.cpp" line="476"/>
+        <source>Label</source>
+        <translation>Etikett</translation>
+    </message>
+    <message>
+        <location filename="src/qphotorec.cpp" line="491"/>
+        <source>File System type</source>
+        <translation>Typ av filsystem</translation>
+    </message>
+    <message>
+        <location filename="src/qphotorec.cpp" line="492"/>
+        <source>ext2/ext3/ext4 filesystem</source>
+        <translation>ext2/ext3/ext4-filsystem</translation>
+    </message>
+    <message>
+        <location filename="src/qphotorec.cpp" line="493"/>
+        <source>FAT/NTFS/HFS+/ReiserFS/...</source>
+        <translation>FAT/NTFS/HFS+/ReiserFS/...</translation>
+    </message>
+    <message>
+        <location filename="src/qphotorec.cpp" line="497"/>
+        <source>Free: Scan for file from unallocated space only</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location filename="src/qphotorec.cpp" line="498"/>
+        <source>Whole: Extract files from whole partition</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location filename="src/qphotorec.cpp" line="525"/>
+        <source>&amp;Browse</source>
+        <translation>&amp;Bläddra</translation>
+    </message>
+    <message>
+        <location filename="src/qphotorec.cpp" line="541"/>
+        <source>&amp;Search</source>
+        <translation>&amp;Sök</translation>
+    </message>
+    <message>
+        <location filename="src/qphotorec.cpp" line="544"/>
+        <source>&amp;About</source>
+        <translation>&amp;Om</translation>
+    </message>
+    <message>
+        <location filename="src/qphotorec.cpp" line="545"/>
+        <source>&amp;File Formats</source>
+        <translation>&amp;Filformat</translation>
+    </message>
+    <message>
+        <location filename="src/qphotorec.cpp" line="645"/>
+        <source>Destination:</source>
+        <translation>Plats:</translation>
+    </message>
+    <message>
+        <location filename="src/qphotorec.cpp" line="650"/>
+        <source>Recovery completed</source>
+        <translation>Återställningen slutfördes</translation>
+    </message>
+    <message>
+        <location filename="src/qphotorec.cpp" line="655"/>
+        <source>Bruteforce %1 sectors remaining (test %2)</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location filename="src/qphotorec.cpp" line="663"/>
+        <source>Pass %1 - Reading sector %2/%3</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location filename="src/qphotorec.cpp" line="668"/>
+        <source>%1/10 headers found</source>
+        <translation>%1/10 huvuden hittades</translation>
+    </message>
+    <message>
+        <location filename="src/qphotorec.cpp" line="670"/>
+        <source>%1 files found</source>
+        <translation>%1 filer hittades</translation>
+    </message>
+    <message>
+        <location filename="src/qphotorec.cpp" line="753"/>
+        <source>File family</source>
+        <translation>Fil-familj</translation>
+    </message>
+    <message>
+        <location filename="src/qphotorec.cpp" line="543"/>
+        <location filename="src/qphotorec.cpp" line="765"/>
+        <source>&amp;Quit</source>
+        <translation>&amp;Avsluta</translation>
+    </message>
+    <message>
+        <location filename="src/qphotorec.cpp" line="754"/>
+        <source>Number of files recovered</source>
+        <translation>Antalet återställda filer</translation>
+    </message>
+    <message>
+        <location filename="src/qphotorec.cpp" line="837"/>
+        <source>QPhotoRec: Failed to create file!</source>
+        <translation>QPhotoRec: Misslyckades med att skapa filen!</translation>
+    </message>
+    <message>
+        <location filename="src/qphotorec.cpp" line="838"/>
+        <source>Failed to create file! Please choose another destination</source>
+        <translation>Misslyckades med att skapa filen! Vänligen välj en annan plats</translation>
+    </message>
+    <message>
+        <location filename="src/qphotorec.cpp" line="857"/>
+        <source>QPhotoRec: Not enough space!</source>
+        <translation>QPhotoRec: Inte tillräckligt utrymme!</translation>
+    </message>
+    <message>
+        <location filename="src/qphotorec.cpp" line="858"/>
+        <source>There is not enough space left! Please free disk space and/or choose another destination</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location filename="src/qphotorec.cpp" line="927"/>
+        <source>QPhotoRec is is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 2 of the License, or (at your option) any later version.
+
+QPhotoRec is is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with QPhotoRec.  If not, see &lt;http://www.gnu.org/licenses/&gt;.</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location filename="src/qphotorec.cpp" line="928"/>
+        <source>QPhotoRec: About</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location filename="src/qphotorec.cpp" line="957"/>
+        <source>File Formats</source>
+        <translation>Filformat</translation>
+    </message>
+    <message>
+        <location filename="src/qphotorec.cpp" line="960"/>
+        <source>&amp;Reset</source>
+        <translation>&amp;Rensa</translation>
+    </message>
+    <message>
+        <location filename="src/qphotorec.cpp" line="961"/>
+        <source>Res&amp;tore</source>
+        <translation>Åter%ställ</translation>
+    </message>
+</context>
+</TS>
---
+++ src/lang/qphotorec.zh_CN.ts
@@ -0,0 +1,257 @@
+<?xml version="1.0" ?><!DOCTYPE TS><TS language="zh_CN" version="2.1">
+<context>
+    <name>QPhotorec</name>
+    <message>
+        <location filename="src/qphotorec.cpp" line="125"/>
+        <source>QPhotoRec</source>
+        <translation>QPhotoRec</translation>
+    </message>
+    <message>
+        <location filename="src/qphotorec.cpp" line="156"/>
+        <location filename="src/qphotorec.cpp" line="521"/>
+        <source>Please select a destination to save the recovered files to.</source>
+        <translation>请选择一个目的地来保存恢复的文件。</translation>
+    </message>
+    <message>
+        <location filename="src/qphotorec.cpp" line="170"/>
+        <source>Please select a raw file</source>
+        <translation>请选择一个 raw 原始文件</translation>
+    </message>
+    <message>
+        <location filename="src/qphotorec.cpp" line="172"/>
+        <source>Raw Files (*.dd *.raw *.img)</source>
+        <translation>Raw 文件 (*.dd *.raw *.img)</translation>
+    </message>
+    <message>
+        <location filename="src/qphotorec.cpp" line="259"/>
+        <source>Sys=</source>
+        <translation>Sys=</translation>
+    </message>
+    <message>
+        <location filename="src/qphotorec.cpp" line="261"/>
+        <source>Unknown</source>
+        <translation>未知</translation>
+    </message>
+    <message>
+        <location filename="src/qphotorec.cpp" line="377"/>
+        <source>No harddisk found</source>
+        <translation>未发现硬盘 </translation>
+    </message>
+    <message>
+        <location filename="src/qphotorec.cpp" line="379"/>
+        <source>No harddisk found
+You need to be administrator to use this program.
+Under Win9x, use the DOS version instead.
+Under Vista or later, select this program, right-click and choose &quot;Run as administrator&quot;.</source>
+        <translation>没有找到硬盘
+您需要是管理员才能使用此程序。
+在 Win9x 下，请使用 DOS 版本。
+在 Vista 或更高版本下，选择此程序，右键单击并选择“以管理员身份运行”。</translation>
+    </message>
+    <message>
+        <location filename="src/qphotorec.cpp" line="388"/>
+        <source>No harddisk found
+You need to be root to use PhotoRec.</source>
+        <translation>没有找到硬盘
+您需要是 root 用户才能使用 PhotoRec。</translation>
+    </message>
+    <message>
+        <location filename="src/qphotorec.cpp" line="393"/>
+        <source>No Disk!</source>
+        <translation>没有硬盘！</translation>
+    </message>
+    <message>
+        <location filename="src/qphotorec.cpp" line="455"/>
+        <source>Add a raw disk image...</source>
+        <translation>添加 raw 原始磁盘映像...</translation>
+    </message>
+    <message>
+        <location filename="src/qphotorec.cpp" line="461"/>
+        <source>PhotoRec is free software, and comes with ABSOLUTELY NO WARRANTY.</source>
+        <translation>PhotoRec 是自由软件，并不提供任何担保。</translation>
+    </message>
+    <message>
+        <location filename="src/qphotorec.cpp" line="462"/>
+        <source>Please select a media to recover from</source>
+        <translation>请选择需要恢复的媒体</translation>
+    </message>
+    <message>
+        <location filename="src/qphotorec.cpp" line="465"/>
+        <source>Disk capacity must be correctly detected for a successful recovery.
+If a disk listed above has an incorrect size, check HD jumper settings and BIOS
+detection, and install the latest OS patches and disk drivers.</source>
+        <translation>要成功恢复，必须正确检测磁盘容量。
+如果上面列出的磁盘大小不正确，请检查硬盘跳线设置和 BIOS 检测系统，
+并安装最新的操作系统修补程序和磁盘驱动程序。</translation>
+    </message>
+    <message>
+        <location filename="src/qphotorec.cpp" line="472"/>
+        <source>Flags</source>
+        <translation>标识</translation>
+    </message>
+    <message>
+        <location filename="src/qphotorec.cpp" line="473"/>
+        <source>Type</source>
+        <translation>类型</translation>
+    </message>
+    <message>
+        <location filename="src/qphotorec.cpp" line="474"/>
+        <source>File System</source>
+        <translation>文件系统</translation>
+    </message>
+    <message>
+        <location filename="src/qphotorec.cpp" line="475"/>
+        <source>Size</source>
+        <translation>大小</translation>
+    </message>
+    <message>
+        <location filename="src/qphotorec.cpp" line="476"/>
+        <source>Label</source>
+        <translation>标签</translation>
+    </message>
+    <message>
+        <location filename="src/qphotorec.cpp" line="491"/>
+        <source>File System type</source>
+        <translation>文件系统类型</translation>
+    </message>
+    <message>
+        <location filename="src/qphotorec.cpp" line="492"/>
+        <source>ext2/ext3/ext4 filesystem</source>
+        <translation>ext2/ext3/ext4 文件系统</translation>
+    </message>
+    <message>
+        <location filename="src/qphotorec.cpp" line="493"/>
+        <source>FAT/NTFS/HFS+/ReiserFS/...</source>
+        <translation>FAT/NTFS/HFS+/ReiserFS/...</translation>
+    </message>
+    <message>
+        <location filename="src/qphotorec.cpp" line="497"/>
+        <source>Free: Scan for file from unallocated space only</source>
+        <translation>闲置:仅扫描未分配空间文件</translation>
+    </message>
+    <message>
+        <location filename="src/qphotorec.cpp" line="498"/>
+        <source>Whole: Extract files from whole partition</source>
+        <translation>完整：从整个分区中提取文件 </translation>
+    </message>
+    <message>
+        <location filename="src/qphotorec.cpp" line="525"/>
+        <source>&amp;Browse</source>
+        <translation>浏览(&amp;B)</translation>
+    </message>
+    <message>
+        <location filename="src/qphotorec.cpp" line="541"/>
+        <source>&amp;Search</source>
+        <translation>搜索(&amp;S)</translation>
+    </message>
+    <message>
+        <location filename="src/qphotorec.cpp" line="544"/>
+        <source>&amp;About</source>
+        <translation>关于(&amp;A)</translation>
+    </message>
+    <message>
+        <location filename="src/qphotorec.cpp" line="545"/>
+        <source>&amp;File Formats</source>
+        <translation>文件格式(&amp;F)</translation>
+    </message>
+    <message>
+        <location filename="src/qphotorec.cpp" line="645"/>
+        <source>Destination:</source>
+        <translation>目的地:</translation>
+    </message>
+    <message>
+        <location filename="src/qphotorec.cpp" line="650"/>
+        <source>Recovery completed</source>
+        <translation>恢复完成</translation>
+    </message>
+    <message>
+        <location filename="src/qphotorec.cpp" line="655"/>
+        <source>Bruteforce %1 sectors remaining (test %2)</source>
+        <translation>暴力破解剩余 %1 个扇区（测试 %2） </translation>
+    </message>
+    <message>
+        <location filename="src/qphotorec.cpp" line="663"/>
+        <source>Pass %1 - Reading sector %2/%3</source>
+        <translation>进度 %1 - 正在读取扇区 %2/%3</translation>
+    </message>
+    <message>
+        <location filename="src/qphotorec.cpp" line="668"/>
+        <source>%1/10 headers found</source>
+        <translation>发现　%1/10 个磁头</translation>
+    </message>
+    <message>
+        <location filename="src/qphotorec.cpp" line="670"/>
+        <source>%1 files found</source>
+        <translation>发现 %2 个文件</translation>
+    </message>
+    <message>
+        <location filename="src/qphotorec.cpp" line="753"/>
+        <source>File family</source>
+        <translation>文件族</translation>
+    </message>
+    <message>
+        <location filename="src/qphotorec.cpp" line="543"/>
+        <location filename="src/qphotorec.cpp" line="765"/>
+        <source>&amp;Quit</source>
+        <translation>退出(&amp;Q)</translation>
+    </message>
+    <message>
+        <location filename="src/qphotorec.cpp" line="754"/>
+        <source>Number of files recovered</source>
+        <translation>恢复的文件数</translation>
+    </message>
+    <message>
+        <location filename="src/qphotorec.cpp" line="837"/>
+        <source>QPhotoRec: Failed to create file!</source>
+        <translation>QPhotoRec: 创建文件失败!</translation>
+    </message>
+    <message>
+        <location filename="src/qphotorec.cpp" line="838"/>
+        <source>Failed to create file! Please choose another destination</source>
+        <translation>文件创建失败! 请选择其他目的地 </translation>
+    </message>
+    <message>
+        <location filename="src/qphotorec.cpp" line="857"/>
+        <source>QPhotoRec: Not enough space!</source>
+        <translation>QPhotoRec: 空间不足!</translation>
+    </message>
+    <message>
+        <location filename="src/qphotorec.cpp" line="858"/>
+        <source>There is not enough space left! Please free disk space and/or choose another destination</source>
+        <translation>剩余空间不足! 请释放磁盘空间和/或选择另一个目的地 </translation>
+    </message>
+    <message>
+        <location filename="src/qphotorec.cpp" line="927"/>
+        <source>QPhotoRec is is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 2 of the License, or (at your option) any later version.
+
+QPhotoRec is is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with QPhotoRec.  If not, see &lt;http://www.gnu.org/licenses/&gt;.</source>
+        <translation>QPhotoRec 是自由软件：您可以根据自由软件基金会发布的 GNU 通用公共许可证 第 2 版或（由您选择）任何更高版本）的条款重新分发和/或修改它。
+
+发布 QPhotoRec，希望它是有助于您的，但不能提供任何保证；任何保证包括适合销售或适合某个特定目的。
+
+您应该已经收到一份 QPhotoRec 的 GNU 通用公共许可证副本。如果没有，请参阅 &lt;http://www.gnu.org/licenses/&gt;。 </translation>
+    </message>
+    <message>
+        <location filename="src/qphotorec.cpp" line="928"/>
+        <source>QPhotoRec: About</source>
+        <translation>QPhotoRec: 关于</translation>
+    </message>
+    <message>
+        <location filename="src/qphotorec.cpp" line="957"/>
+        <source>File Formats</source>
+        <translation>文件格式</translation>
+    </message>
+    <message>
+        <location filename="src/qphotorec.cpp" line="960"/>
+        <source>&amp;Reset</source>
+        <translation>重置(&amp;R)</translation>
+    </message>
+    <message>
+        <location filename="src/qphotorec.cpp" line="961"/>
+        <source>Res&amp;tore</source>
+        <translation>恢复(&amp;T)</translation>
+    </message>
+</context>
+</TS>
