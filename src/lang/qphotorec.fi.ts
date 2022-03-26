f599c961a03c52c1f2a5fcab47ca8103317302cb
Christophe Grenier <grenier@cgsecurity.org>
2022-03-26 12:03:31 +0100

    QPhotoRec: add Finnish translation from https://www.transifex.com/cgsecurity/testdisk/qphotorects/

---
+++ src/lang/qphotorec.fi.ts
@@ -0,0 +1,269 @@
+<?xml version="1.0" ?><!DOCTYPE TS><TS version="2.1" language="fi">
+<context>
+    <name>QPhotorec</name>
+    <message>
+        <location filename="src/qphotorec.cpp" line="125"/>
+        <source>QPhotoRec</source>
+        <translation>QPhotoRec -kuvapalautusohjelma</translation>
+    </message>
+    <message>
+        <location filename="src/qphotorec.cpp" line="156"/>
+        <location filename="src/qphotorec.cpp" line="521"/>
+        <source>Please select a destination to save the recovered files to.</source>
+        <translation>Valitse kohde johon palautetut tiedostot tallennetaan.</translation>
+    </message>
+    <message>
+        <location filename="src/qphotorec.cpp" line="170"/>
+        <source>Please select a raw file</source>
+        <translation>Valitse raakatiedosto</translation>
+    </message>
+    <message>
+        <location filename="src/qphotorec.cpp" line="172"/>
+        <source>Raw Files (*.dd *.raw *.img)</source>
+        <translation>Raakatiedostot (*.dd *.raw *.img)</translation>
+    </message>
+    <message>
+        <location filename="src/qphotorec.cpp" line="259"/>
+        <source>Sys=</source>
+        <translation>Sys=</translation>
+    </message>
+    <message>
+        <location filename="src/qphotorec.cpp" line="261"/>
+        <source>Unknown</source>
+        <translation>Tuntematon</translation>
+    </message>
+    <message>
+        <location filename="src/qphotorec.cpp" line="377"/>
+        <source>No hard disk found</source>
+        <oldsource>No harddisk found</oldsource>
+        <translation>Kiintolevyä ei löytynyt</translation>
+    </message>
+    <message>
+        <location filename="src/qphotorec.cpp" line="379"/>
+        <source>No hard disk found
+You need to be administrator to use this program.
+Under Win9x, use the DOS version instead.
+Under Vista or later, select this program, right-click and choose &quot;Run as administrator&quot;.</source>
+        <oldsource>No harddisk found
+You need to be administrator to use this program.
+Under Win9x, use the DOS version instead.
+Under Vista or later, select this program, right-click and choose &quot;Run as administrator&quot;.</oldsource>
+        <translation>Kiintolevyä ei löytynyt
+Sinulla tulee olla ylläpitäjän oikeudet käyttääksesi tätä ohjelmaa.
+Jos käytössä on Windows 9x, käytä ohjelman DOS-versiota.
+Mikäli käytössä on Vista taikka myöhempi, kuten Windows 7, napsauta hiiren oikealla napilla ja valitse &quot;Suorita järjestelmänvalvojana&quot;.</translation>
+    </message>
+    <message>
+        <location filename="src/qphotorec.cpp" line="388"/>
+        <source>No hard disk found
+You need to be root to use PhotoRec.</source>
+        <oldsource>No harddisk found
+You need to be root to use PhotoRec.</oldsource>
+        <translation>Kiintolevyä ei löytynyt
+Sinulla tulee olla juurikäyttäjän (root) / järjestelmänvalvojan oikeudet käyttääksesi PhotoRec:iä.</translation>
+    </message>
+    <message>
+        <location filename="src/qphotorec.cpp" line="393"/>
+        <source>No Disk!</source>
+        <translation>Ei levyä!</translation>
+    </message>
+    <message>
+        <location filename="src/qphotorec.cpp" line="455"/>
+        <source>Add a raw disk image...</source>
+        <translation>Lisää raakalevykuva...</translation>
+    </message>
+    <message>
+        <location filename="src/qphotorec.cpp" line="461"/>
+        <source>PhotoRec is free software, and comes with ABSOLUTELY NO WARRANTY.</source>
+        <translation>PhotoRec on vapaa ohjelmisto eikä sitä toimiteta MINKÄÄN TAKUUN KANSSA.</translation>
+    </message>
+    <message>
+        <location filename="src/qphotorec.cpp" line="462"/>
+        <source>Please select a media to recover from</source>
+        <translation>Valitse tallennusväline jolta pyrit palauttamaan tiedostoja</translation>
+    </message>
+    <message>
+        <location filename="src/qphotorec.cpp" line="465"/>
+        <source>Disk capacity must be correctly detected for a successful recovery.
+If a disk listed above has an incorrect size, check HD jumper settings and BIOS
+detection, and install the latest OS patches and disk drivers.</source>
+        <translation>Levyn säilöntätilan pitää tunnistua oikein jotta onnistunut palautus saavutetaan.
+Mikäli ylläoleva levy näyttää väärän koon, tarkista kiintolevyn jumpperiasetukset sekä, että BIOS
+havaitsee tuon levyn, ja asenna viimeisimmät käyttöjärjestelmäpaikkaukset ja levyajurit.</translation>
+    </message>
+    <message>
+        <location filename="src/qphotorec.cpp" line="472"/>
+        <source>Flags</source>
+        <translation>Liput</translation>
+    </message>
+    <message>
+        <location filename="src/qphotorec.cpp" line="473"/>
+        <source>Type</source>
+        <translation>Tyyppi</translation>
+    </message>
+    <message>
+        <location filename="src/qphotorec.cpp" line="474"/>
+        <source>File System</source>
+        <translation>Tiedostojärjestelmä</translation>
+    </message>
+    <message>
+        <location filename="src/qphotorec.cpp" line="475"/>
+        <source>Size</source>
+        <translation>Koko</translation>
+    </message>
+    <message>
+        <location filename="src/qphotorec.cpp" line="476"/>
+        <source>Label</source>
+        <translation>Nimike</translation>
+    </message>
+    <message>
+        <location filename="src/qphotorec.cpp" line="491"/>
+        <source>File System type</source>
+        <translation>Tiedostojärjestelmän tyyppi</translation>
+    </message>
+    <message>
+        <location filename="src/qphotorec.cpp" line="492"/>
+        <source>ext2/ext3/ext4 filesystem</source>
+        <translation>ext2/ext3/ext4 tiedostojärjestelmä</translation>
+    </message>
+    <message>
+        <location filename="src/qphotorec.cpp" line="493"/>
+        <source>FAT/NTFS/HFS+/ReiserFS/...</source>
+        <translation>FAT/NTFS/HFS+/ReiserFS/...</translation>
+    </message>
+    <message>
+        <location filename="src/qphotorec.cpp" line="497"/>
+        <source>Free: Scan for file from unallocated space only</source>
+        <translation>Vapaa: luotaa läpi etsien tiedostoa ainoastaan levyn varaamattomasta osasta </translation>
+    </message>
+    <message>
+        <location filename="src/qphotorec.cpp" line="498"/>
+        <source>Whole: Extract files from whole partition</source>
+        <translation>Täysi: erittele tiedostot koko osiosta</translation>
+    </message>
+    <message>
+        <location filename="src/qphotorec.cpp" line="525"/>
+        <source>&amp;Browse</source>
+        <translation>&amp;Selaa</translation>
+    </message>
+    <message>
+        <location filename="src/qphotorec.cpp" line="541"/>
+        <source>&amp;Search</source>
+        <translation>&amp;Etsi</translation>
+    </message>
+    <message>
+        <location filename="src/qphotorec.cpp" line="544"/>
+        <source>&amp;About</source>
+        <translation>&amp;Tietoja</translation>
+    </message>
+    <message>
+        <location filename="src/qphotorec.cpp" line="545"/>
+        <source>&amp;File Formats</source>
+        <translation>&amp;Tiedostomuodot</translation>
+    </message>
+    <message>
+        <location filename="src/qphotorec.cpp" line="645"/>
+        <source>Destination:</source>
+        <translation>Kohde:</translation>
+    </message>
+    <message>
+        <location filename="src/qphotorec.cpp" line="650"/>
+        <source>Recovery completed</source>
+        <translation>Palautus on suoritettu</translation>
+    </message>
+    <message>
+        <location filename="src/qphotorec.cpp" line="655"/>
+        <source>Bruteforce %1 sectors remaining (test %2)</source>
+        <translation>Väkisinsuoritus %1 lohkoa jäljellä (koestus %2)</translation>
+    </message>
+    <message>
+        <location filename="src/qphotorec.cpp" line="663"/>
+        <source>Pass %1 - Reading sector %2/%3</source>
+        <translation>Ohitus %1 - Luetaan lohkoa %2/%3</translation>
+    </message>
+    <message>
+        <location filename="src/qphotorec.cpp" line="668"/>
+        <source>%1/10 headers found</source>
+        <translation>%1/10 ylätunnistetta löydetty</translation>
+    </message>
+    <message>
+        <location filename="src/qphotorec.cpp" line="670"/>
+        <source>%1 files found</source>
+        <translation>%1 tiedostoa löydetty</translation>
+    </message>
+    <message>
+        <location filename="src/qphotorec.cpp" line="753"/>
+        <source>File family</source>
+        <translation>Tiedostoperhe</translation>
+    </message>
+    <message>
+        <location filename="src/qphotorec.cpp" line="543"/>
+        <location filename="src/qphotorec.cpp" line="765"/>
+        <source>&amp;Quit</source>
+        <translation>&amp;Lopeta</translation>
+    </message>
+    <message>
+        <location filename="src/qphotorec.cpp" line="754"/>
+        <source>Number of files recovered</source>
+        <translation>Palautettujen tiedostojen lukumäärä</translation>
+    </message>
+    <message>
+        <location filename="src/qphotorec.cpp" line="837"/>
+        <source>QPhotoRec: Failed to create file!</source>
+        <translation>QPhotoRec: tiedoston luominen epäonnistui!</translation>
+    </message>
+    <message>
+        <location filename="src/qphotorec.cpp" line="838"/>
+        <source>Failed to create file! Please choose another destination</source>
+        <translation>Tiedoston luominen epäonnistui! Valitse jokin toinen kohde</translation>
+    </message>
+    <message>
+        <location filename="src/qphotorec.cpp" line="857"/>
+        <source>QPhotoRec: Not enough space!</source>
+        <translation>QPhotoRec: tilaa ei ole riittävästi!</translation>
+    </message>
+    <message>
+        <location filename="src/qphotorec.cpp" line="858"/>
+        <source>There is not enough space left! Please free disk space and/or choose another destination</source>
+        <translation>Levyllä ei ole tilaa riittävästi jäljellä! Vapauta levytilaa ja/tai valitse jokin toinen kohde</translation>
+    </message>
+    <message>
+        <location filename="src/qphotorec.cpp" line="927"/>
+        <source>QPhotoRec is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 2 of the License, or (at your option) any later version.
+
+QPhotoRec is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with QPhotoRec.  If not, see &lt;http://www.gnu.org/licenses/&gt;.</source>
+        <oldsource>QPhotoRec is is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 2 of the License, or (at your option) any later version.
+
+QPhotoRec is is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with QPhotoRec.  If not, see &lt;http://www.gnu.org/licenses/&gt;.</oldsource>
+        <translation>QPhotoRec on vapaa ohjelmisto: voit jaella sitä ja/tai muokata sitä julkisen yleisluvan (GNU General Public License) ehdoilla kuten sen Free Software Foundation on määrittänyt, joko luvan versio 2, tai (valintasi mukaan) mikä tahansa myöhempi versio käy.
+
+QPhotoRec toimitetaan siinä toivossa että se olisi hyödyllinen, mutta MITÄÄN TAKUUTA SE EI SISÄLLÄ; sama pätee myös ohjelman KAUPPAMYYNNIN tai sen SOVELTUVUUDEN SUHTEEN MIHINKÄÄN TIETTYYN KÄYTTÖTARKOITUKSEEN. Lue GNU:n yleislupakirjoitus (GNU General Public License) yksityiskohtaisempia lisätietoja varten.
+
+QPhotoRec:in mukana tulisi olla sinulle toimitettuna tuo GNU-yleisluvan selonteko. Mikäli näin ei kuitenkaan ole, lue &lt;http://www.gnu.org/licenses/&gt;.</translation>
+    </message>
+    <message>
+        <location filename="src/qphotorec.cpp" line="928"/>
+        <source>QPhotoRec: About</source>
+        <translation>QPhotoRec: tietoja ohjelmasta</translation>
+    </message>
+    <message>
+        <location filename="src/qphotorec.cpp" line="957"/>
+        <source>File Formats</source>
+        <translation>Tiedostomuodot</translation>
+    </message>
+    <message>
+        <location filename="src/qphotorec.cpp" line="960"/>
+        <source>&amp;Reset</source>
+        <translation>&amp;Nollaa</translation>
+    </message>
+    <message>
+        <location filename="src/qphotorec.cpp" line="961"/>
+        <source>Res&amp;tore</source>
+        <translation>Palau&amp;ta</translation>
+    </message>
+</context>
+</TS>
