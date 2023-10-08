7430e4da4b713807a45eb5ca97a80e299f8d6208
Christophe Grenier <grenier@cgsecurity.org>
2023-10-08 14:25:33 +0200

    src/addpart.h: improve Frama-C annotations for add_partition_cli()

--- src/addpart.h
+++ src/addpart.h
@@ -24,11 +24,11 @@
 #define _ADDPART_H
 /*@
   @ requires \valid(disk);
-  @ requires list_part == \null || \valid_read(list_part);
+  @ requires valid_disk(disk);
+  @ requires valid_list_part(list_part);
   @ requires \valid(current_cmd);
   @ requires valid_read_string(*current_cmd);
   @ requires separation: \separated(disk, list_part, current_cmd);
-  @ ensures  valid_read_string(*current_cmd);
   @*/
 list_part_t *add_partition_cli(disk_t *disk, list_part_t *list_part, char **current_cmd);
 #endif
