50d4181e76ec215c6b6d21c8c9563807e2325d65
Christophe Grenier <grenier@cgsecurity.org>
2023-12-27 10:58:33 +0100

    src/file_flac.c: add more frama-c annotations

--- src/file_flac.c
+++ src/file_flac.c
@@ -50,6 +50,7 @@ const file_hint_t file_hint_flac= {
 /*@
   @ requires file_recovery->data_check==&data_check_flac_metadata;
   @ requires valid_data_check_param(buffer, buffer_size, file_recovery);
+  @ terminates \true;
   @ ensures  valid_data_check_result(\result, file_recovery);
   @ assigns  file_recovery->calculated_file_size, file_recovery->data_check;
   @*/
@@ -57,7 +58,10 @@ static data_check_t data_check_flac_metadata(const unsigned char *buffer, const 
 {
   /*@ assert file_recovery->calculated_file_size <= PHOTOREC_MAX_FILE_SIZE; */
   /*@ assert file_recovery->file_size <= PHOTOREC_MAX_FILE_SIZE; */
-  /*@ loop assigns file_recovery->calculated_file_size, file_recovery->data_check; */
+  /*@
+    @ loop assigns file_recovery->calculated_file_size, file_recovery->data_check;
+    @ loop variant file_recovery->file_size + buffer_size/2 - (file_recovery->calculated_file_size + 4);
+    @*/
   while(file_recovery->calculated_file_size + buffer_size/2  >= file_recovery->file_size &&
       file_recovery->calculated_file_size + 4 < file_recovery->file_size + buffer_size/2)
   {
@@ -85,6 +89,7 @@ static data_check_t data_check_flac_metadata(const unsigned char *buffer, const 
   @ requires buffer_size >= 8;
   @ requires separation: \separated(&file_hint_flac, buffer+(..), file_recovery, file_recovery_new);
   @ requires valid_header_check_param(buffer, buffer_size, safe_header_only, file_recovery, file_recovery_new);
+  @ terminates \true;
   @ ensures  valid_header_check_result(\result, file_recovery_new);
   @ assigns  *file_recovery_new;
   @*/
