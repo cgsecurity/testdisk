0287bc3643a221bbaa2e8a7bd64659fd0802828f
Christophe Grenier <grenier@cgsecurity.org>
2023-12-27 10:48:57 +0100

    src/file_bac.c: more frama-c annotations

--- src/file_bac.c
+++ src/file_bac.c
@@ -71,6 +71,7 @@ static data_check_t data_check_bac(const unsigned char *buffer, const unsigned i
   /*@ assert file_recovery->file_size <= PHOTOREC_MAX_FILE_SIZE; */
   /*@
     @ loop assigns file_recovery->calculated_file_size;
+    @ loop variant file_recovery->file_size + buffer_size/2 - (file_recovery->calculated_file_size + 0x18);
     @*/
   while(file_recovery->calculated_file_size + buffer_size/2  >= file_recovery->file_size &&
       file_recovery->calculated_file_size + 0x18 < file_recovery->file_size + buffer_size/2)
@@ -106,6 +107,7 @@ static data_check_t data_check_bac(const unsigned char *buffer, const unsigned i
   @ requires buffer_size >= sizeof(struct block_header);
   @ requires separation: \separated(&file_hint_bac, buffer, file_recovery, file_recovery_new);
   @ requires valid_header_check_param(buffer, buffer_size, safe_header_only, file_recovery, file_recovery_new);
+  @ terminates \true;
   @ ensures  valid_header_check_result(\result, file_recovery_new);
   @ ensures (\result == 1) ==> (file_recovery_new->time == 0);
   @ ensures (\result == 1) ==> (file_recovery_new->file_size == 0);
